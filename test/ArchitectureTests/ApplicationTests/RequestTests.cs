02c0e5b80586dbbb2bf496887256e21d553588de
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2024-06-02 20:22:44 +0100

    CFODEV-472 throw an exception if not authorize attribute exists (or we do not explicitly allow anonymous).
    Tests added to break the build if these are detected at run time.
    
    (breaks the build as we have not yet added the authorise test to any command)

---
+++ test/ArchitectureTests/ApplicationTests/RequestTests.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Cfo.Cats.Application.Common.Interfaces.Caching;
+using Cfo.Cats.Application.Common.Security;
+using Cfo.Cats.Domain.Common;
+using FluentAssertions;
+using MediatR;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
+using NetArchTest.Rules;
+using NUnit.Framework;
+
+namespace Cfo.Cats.Domain.ArchitectureTests.ApplicationTests;
+
+public class RequestTests
+{
+    private static readonly Assembly ApplicationAssembly = typeof(Application.DependencyInjection).Assembly;
+
+    [Test]
+    public void Commands_Should_HaveAuthorizeAttribute()
+    {
+        var result = Types.InAssembly(ApplicationAssembly)
+            .That()
+            .ImplementInterface(typeof(IRequest<>))
+            .Or()
+            .ImplementInterface(typeof(ICacheableRequest<>))
+            .Or()
+            .ImplementInterface(typeof(ICacheInvalidatorRequest<>))
+            .Should()
+            .HaveCustomAttribute(typeof(AuthorAttribute))
+            .Or()
+            .HaveCustomAttribute(typeof(AllowAnonymousAttribute))
+            .GetResult();
+
+        var failedTypes = result.FailingTypes?.Select(t => t.FullName).ToList();
+        
+        var formattedFailedTypes = failedTypes == null ? "None" : string.Join("\n", failedTypes);
+        
+        result.IsSuccessful
+            .Should()
+            .BeTrue($"The following types failed the test:\n {formattedFailedTypes}");
+
+    }
+
+
+}
