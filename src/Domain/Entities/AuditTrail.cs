9a16fd35016346ecd1c5dac961a966d66042837f
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2024-06-02 16:59:59 +0100

    CFODEV-473 : fix tests after merge.
    Also remove the exclusion for audittrail

--- src/Domain/Entities/AuditTrail.cs
+++ src/Domain/Entities/AuditTrail.cs
@@ -10,23 +10,44 @@ namespace Cfo.Cats.Domain.Entities;
 public class AuditTrail : IEntity<int>
 {
 
-    public int Id { get; set; }
-    public int? UserId { get; set; }
-    public virtual ApplicationUser? Owner { get; set; }
-    public AuditType AuditType { get; set; }
-    public string? TableName { get; set; }
-    public DateTime DateTime { get; set; }
-    public Dictionary<string, object?>? OldValues { get; set; }
-    public Dictionary<string, object?>? NewValues { get; set; }
-    public List<string>? AffectedColumns { get; set; }
-    public Dictionary<string, object> PrimaryKey { get; set; } = new();
-    public List<PropertyEntry> TemporaryProperties { get; } = new();
+    private AuditTrail()
+    {
+    }
+
+    public static AuditTrail Create(string tableName, int? userId, AuditType auditType)
+    {
+        return new AuditTrail()
+        {
+            TableName = tableName,
+            UserId = userId,
+            DateTime = DateTime.UtcNow,
+            AuditType = auditType,
+            AffectedColumns = new List<string>(),
+            NewValues = new Dictionary<string, object?>(),
+            OldValues = new Dictionary<string, object?>()
+        };
+    }
+
+
+    public int Id { get; private set; }
+    public int? UserId { get; private set; }
+    public virtual ApplicationUser? Owner { get; private set; }
+    public AuditType AuditType { get; private set; }
+    public string? TableName { get; private set; }
+    public DateTime DateTime { get; private set; }
+    public Dictionary<string, object?>? OldValues { get; private set; }
+    public Dictionary<string, object?>? NewValues { get; private set; }
+    public List<string>? AffectedColumns { get; private set; }
+    public Dictionary<string, object> PrimaryKey { get; private set; } = new();
+    public List<PropertyEntry> TemporaryProperties { get; private set; } = new();
     public bool HasTemporaryProperties => TemporaryProperties.Any();
 
     [NotMapped]
     public IReadOnlyCollection<DomainEvent> DomainEvents => new List<DomainEvent>().AsReadOnly();
 
     public void ClearDomainEvents() { }
+
+    
 }
 
 public enum AuditType
