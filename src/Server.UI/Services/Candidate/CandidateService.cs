6c6e9f99ceaf0be324a0d6ea60f011f7b38bf287
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-06-26 20:44:46 +0100

    Candidate Service

---
+++ src/Server.UI/Services/Candidate/CandidateService.cs
@@ -0,0 +1,26 @@
+﻿using Cfo.Cats.Application.Features.Candidates.DTOs;
+using Cfo.Cats.Application.Features.Candidates.Queries.Search;
+
+namespace Cfo.Cats.Server.UI.Services.Candidate;
+
+public class CandidateService(HttpClient client)
+{
+    public async Task<IEnumerable<SearchResult>?> SearchAsync(CandidateSearchQuery searchQuery)
+    {
+        var queryParams = new[]
+        {
+            $"identifier={searchQuery.ExternalIdentifier}",
+            $"lastName={searchQuery.LastName}",
+            $"dateOfBirth={searchQuery.DateOfBirth.Value.ToString("yyyy-MM-dd")}"
+        };
+
+        return await client.GetFromJsonAsync<IEnumerable<SearchResult>>($"/search?{string.Join('&', queryParams)}");
+    }
+
+    public async Task<CandidateDto?> GetByUpciAsync(string upci)
+    {
+        return await client.GetFromJsonAsync<CandidateDto>($"clustering/{upci}/Aggregate");
+    }
+}
+
+public record SearchResult(string Upci, int Precedence);
