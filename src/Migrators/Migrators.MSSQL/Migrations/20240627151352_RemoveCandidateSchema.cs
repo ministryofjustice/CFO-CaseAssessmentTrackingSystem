bed41f7ae1f5b0a044015db4cced4186f6466495
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-06-27 16:15:10 +0100

    Database migration to drop database schema removed under CFODEV-506

---
+++ src/Migrators/Migrators.MSSQL/Migrations/20240627151352_RemoveCandidateSchema.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cfo.Cats.Migrators.MSSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveCandidateSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CandidateIdentifier");
+
+            migrationBuilder.DropTable(
+                name: "Candidate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Candidate",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(9)", maxLength: 9, nullable: false),
+                    CurrentLocationId = table.Column<int>(type: "int", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    MiddleName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidate", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Candidate_Location_CurrentLocationId",
+                        column: x => x.CurrentLocationId,
+                        principalTable: "Location",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CandidateIdentifier",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CandidateId = table.Column<string>(type: "nvarchar(9)", nullable: false),
+                    IdentifierType = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    IdentifierValue = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CandidateIdentifier", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CandidateIdentifier_Candidate_CandidateId",
+                        column: x => x.CandidateId,
+                        principalTable: "Candidate",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Candidate_CurrentLocationId",
+                table: "Candidate",
+                column: "CurrentLocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CandidateIdentifier_CandidateId",
+                table: "CandidateIdentifier",
+                column: "CandidateId");
+        }
+    }
+}
