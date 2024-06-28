3de2468ca95df5332070319c7391cb6126d3fe1e
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-06-28 12:39:39 +0100

    Tenant Domain creation/deletion screens and functionality

---
+++ src/Application/Features/Tenants/Commands/AddDomainCommand.cs
@@ -0,0 +1,79 @@
+﻿using Cfo.Cats.Application.Common.Interfaces.MultiTenant;
+using Cfo.Cats.Application.Common.Security;
+using Cfo.Cats.Application.SecurityConstants;
+using Cfo.Cats.Domain.Entities.Administration;
+using Cfo.Cats.Domain.ValueObjects;
+
+namespace Cfo.Cats.Application.Features.Tenants.Commands;
+
+[RequestAuthorize(Roles = "Admin")]
+public static class AddDomainCommand
+{
+    [RequestAuthorize(Policy = PolicyNames.SystemFunctionsWrite)]
+    public class Command : IRequest<Result>
+    {
+        [Description("Tenant Id")]
+        public string? TenantId { get; set; }
+
+        [Description("Domain")]
+        public string? Domain { get; set; }
+
+        private class Mapping : Profile
+        {
+            public Mapping()
+            {
+                CreateMap<Command, TenantDomain>();
+            }
+        }
+    }
+
+    internal class Handler(IApplicationDbContext context, IMapper mapper, ITenantService tenantService) : IRequestHandler<Command, Result>
+    {
+        public async Task<Result> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var tenant = await context.Tenants.FindAsync(request.TenantId);
+
+            if (tenant is null)
+            {
+                throw new NotFoundException(nameof(Tenant), request.TenantId!);
+            }
+
+            var model = mapper.Map<TenantDomain>(request);
+
+            tenant.AddDomain(model);
+
+            await context.SaveChangesAsync(cancellationToken);
+
+            tenantService.Refresh();
+
+            return Result.Success();
+        }
+    }
+
+    public class Validator : AbstractValidator<Command>
+    {
+        public Validator()
+        {
+            RuleFor(v => v.Domain)
+                .MaximumLength(255)
+                .WithMessage("Domain must be no more than 255 characters")
+                .NotEmpty()
+                .WithMessage("Domain is required");
+
+            /*
+             * Match "domain" patterns, case sensitive. Examples:
+             * @example.com
+             * @test.example.com
+             * @my-domain.com
+            */
+            RuleFor(v => v.Domain)
+                .Matches(@"^@[a-z0-9]+(?:[-]?[a-z0-9]+)*(?:\.[a-z0-9]+(?:[-]?[a-z0-9]+)*)+$")
+                .WithMessage("Must be in the format '@example.com'");
+
+            RuleFor(v => v.TenantId)
+                .NotEmpty()
+                .WithMessage("Tenant Id is required");
+        }
+    }
+
+}
