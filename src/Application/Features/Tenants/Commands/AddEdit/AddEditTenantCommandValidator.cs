5a318f15d374b1728aba24246877ce52b61e66ac
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-06-28 12:21:39 +0100

    Bug fix for tenant creation/deletion.

--- src/Application/Features/Tenants/Commands/AddEdit/AddEditTenantCommandValidator.cs
+++ src/Application/Features/Tenants/Commands/AddEdit/AddEditTenantCommandValidator.cs
@@ -5,5 +5,13 @@ public class AddEditTenantCommandValidator : AbstractValidator<AddEditTenantComm
     public AddEditTenantCommandValidator()
     {
         RuleFor(v => v.Name).MaximumLength(256).NotEmpty();
+
+        RuleFor(v => v.Description).MaximumLength(150).NotEmpty();
+
+        RuleFor(v => v.Id).MaximumLength(200).NotEmpty();
+
+        RuleFor(v => v.Id)
+            .Matches("^[0-9]+(?:\\.[0-9]+)*\\.$")
+            .WithMessage("Must start with a number and end with a period");
     }
 }
