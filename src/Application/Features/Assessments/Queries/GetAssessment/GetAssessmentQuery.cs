d548f2a553e9dfb2cdeba857204146e65a21e9a2
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2024-06-28 20:19:46 +0100

    (WIP) get ui to build with assessment page

--- src/Application/Features/Assessments/Queries/GetAssessment/GetAssessmentQuery.cs
+++ src/Application/Features/Assessments/Queries/GetAssessment/GetAssessmentQuery.cs
@@ -1,10 +1,11 @@
 ﻿using Cfo.Cats.Application.Common.Security;
 using Cfo.Cats.Application.Features.Assessments.Caching;
 using Cfo.Cats.Application.Features.Assessments.DTOs;
+using Cfo.Cats.Application.SecurityConstants;
 
 namespace Cfo.Cats.Application.Features.Assessments.Queries.GetAssessment;
 
-[RequestAuthorize(Roles = "Admin, Basic")]
+[RequestAuthorize(Policy = PolicyNames.AllowEnrol)]
 public class GetAssessmentQuery : ICacheableRequest<Result<AssessmentDto>>
 {
     public string CacheKey 
