b3f99fc6c9ca8237d213bb62b0cd09b38acaf29f
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2024-07-01 21:01:59 +0100

    Participant screen

--- src/Application/Features/Assessments/PostAssessmentCommand/PostAssessmentCommand.cs
+++
@@ -1,7 +0,0 @@
-﻿using Cfo.Cats.Application.Common.Security;
-using Cfo.Cats.Application.Features.Assessments.DTOs;
-
-namespace Cfo.Cats.Application.Features.Assessments.PostAssessmentCommand;
-
-[RequestAuthorize(Roles = "Admin, Basic")]
-public record PostAssessmentCommand(AssessmentDto assessment) : IRequest<bool>;
