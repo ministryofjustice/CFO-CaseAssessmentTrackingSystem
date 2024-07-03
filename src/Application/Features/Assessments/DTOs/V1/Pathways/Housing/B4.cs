32a17edb468d16586e6d4b145cc0d9666ec6563e
VS <vinay.saini@justice.gov.uk>
2024-07-03 10:48:43 +0100

    Housing Questions - Answers replaced with constant strings

--- src/Application/Features/Assessments/DTOs/V1/Pathways/Housing/B4.cs
+++ src/Application/Features/Assessments/DTOs/V1/Pathways/Housing/B4.cs
@@ -4,9 +4,16 @@ public class B4() : SingleChoiceQuestion(
     "How satisfied are you with your current housing?",
     "If you’re in prison, then think about your expected housing on release.",
     [
-        "Very dissatisfied",
-        "Slightly dissatisfied",
-        "Neither satisfied or dissatisfied",
-        "Fairly satisfied",
-        "Very satisfied",
-    ]);
+        VeryDissatisfiedHousing,
+        SlightlySatisfiedHousing,
+        NeitherSatisfiedOrDissatisfiedHousing,
+        FairlySatisfiedHousing,
+        VerySatisfiedHousing,
+    ])
+{
+    public const string VeryDissatisfiedHousing = "Very dissatisfied";
+    public const string SlightlySatisfiedHousing = "Slightly dissatisfied";
+    public const string NeitherSatisfiedOrDissatisfiedHousing = "Neither satisfied or dissatisfied";
+    public const string FairlySatisfiedHousing = "Fairly satisfied";
+    public const string VerySatisfiedHousing = "Very satisfied";
+}
--- src/Application/Features/Assessments/DTOs/V1/Pathways/Housing/B5.cs
+++ src/Application/Features/Assessments/DTOs/V1/Pathways/Housing/B5.cs
@@ -3,8 +3,14 @@ namespace Cfo.Cats.Application.Features.Assessments.DTOs.V1.Pathways.Housing;
 public class B5() : SingleChoiceQuestion(
     "How safe do/would you feel walking alone in your local area after dark?",
     [
-        "Very unsafe",
-        "A bit unsafe",
-        "Fairly safe",
-        "Very safe",
-    ]);
+        VeryUnsafeToWalkAloneAfterDarkInLocalArea,
+        ABitUnsafeToWalkAloneAfterDarkInLocalArea,
+        FairlySafeToWalkAloneAfterDarkInLocalArea,
+        VerySafeToWalkAloneAfterDarkInLocalArea,
+    ])
+{
+    public const string VeryUnsafeToWalkAloneAfterDarkInLocalArea = "Very unsafe";
+    public const string ABitUnsafeToWalkAloneAfterDarkInLocalArea = "A bit unsafe";
+    public const string FairlySafeToWalkAloneAfterDarkInLocalArea = "Fairly safe";
+    public const string VerySafeToWalkAloneAfterDarkInLocalArea = "Very safe";
+}
