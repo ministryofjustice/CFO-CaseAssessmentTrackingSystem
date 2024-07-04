810273a1e11fa064ef20b0fb1dc505225586c69f
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2024-07-04 08:33:26 +0100

    Relationship scoring

--- src/Application/Features/Assessments/DTOs/V1/Pathways/Relationships/F6.cs
+++ src/Application/Features/Assessments/DTOs/V1/Pathways/Relationships/F6.cs
@@ -4,14 +4,14 @@ public class F6() : SingleChoiceQuestion("On a scale 1 to 5, in general how much
     [
         NotAtAll,
         ALittle,
-        SomeWhat,
+        Somewhat,
         Mostly,
         Completely,
     ])
 {
     public const string NotAtAll = "1 Not at all";
     public const string ALittle = "2 A little";
-    public const string SomeWhat = "3 Somewhat";
+    public const string Somewhat = "3 Somewhat";
     public const string Mostly = "4 Mostly";
     public const string Completely = "5 Completely";
 }
