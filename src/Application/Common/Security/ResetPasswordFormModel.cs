a97af6457653ad11ef40d96d4fadfbb9e59905c0
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-07-08 12:51:06 +0100

    Password reset functionality for users

--- src/Application/Common/Security/ResetPasswordFormModel.cs
+++ src/Application/Common/Security/ResetPasswordFormModel.cs
@@ -2,10 +2,6 @@
 
 public class ResetPasswordFormModel
 {
-    public string? Id { get; set; }
-    public string? UserName { get; set; }
-    public string? DisplayName { get; set; }
-    public string? ProfilePictureDataUrl { get; set; }
+    public string? UserId { get; set; }
     public string? Password { get; set; }
-    public string? ConfirmPassword { get; set; }
 }
