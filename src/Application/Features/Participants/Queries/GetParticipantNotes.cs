689c3cd576b8fafe9b1a46cf477217605b6aa99f
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-07-12 15:09:17 +0100

    GetParticipantNotes query

---
+++ src/Application/Features/Participants/Queries/GetParticipantNotes.cs
@@ -0,0 +1,30 @@
+﻿using Cfo.Cats.Application.Common.Security;
+using Cfo.Cats.Application.Features.Participants.DTOs;
+using Cfo.Cats.Application.SecurityConstants;
+
+namespace Cfo.Cats.Application.Features.Participants.Queries;
+
+public static class GetParticipantNotes
+{
+    [RequestAuthorize(Policy = PolicyNames.AuthorizedUser)]
+    public class Query : IRequest<Result<ParticipantNoteDto[]>>
+    {
+        public required string ParticipantId { get; set; }
+    }
+
+    public class Handler(IApplicationDbContext context, IMapper mapper) : IRequestHandler<Query, Result<ParticipantNoteDto[]>>
+    {
+        public async Task<Result<ParticipantNoteDto[]>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var query = context.Participants
+                .Where(p => p.Id == request.ParticipantId)
+                .SelectMany(p => p.Notes);
+
+            var notes = await query.ProjectTo<ParticipantNoteDto>(mapper.ConfigurationProvider)
+                .ToArrayAsync(cancellationToken) ?? [];
+
+            return await Result<ParticipantNoteDto[]>.SuccessAsync(notes);
+        }
+    }
+
+}
