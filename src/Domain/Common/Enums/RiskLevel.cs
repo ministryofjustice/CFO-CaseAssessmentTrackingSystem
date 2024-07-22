a25b51eca201f8d52f2661f1e2a4c38489fcade5
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-07-22 15:00:25 +0100

    WIP: Risk Form

---
+++ src/Domain/Common/Enums/RiskLevel.cs
@@ -0,0 +1,14 @@
+﻿using Ardalis.SmartEnum;
+
+namespace Cfo.Cats.Domain.Common.Enums;
+
+public sealed class RiskLevel : SmartEnum<RiskLevel>
+{
+    public static readonly RiskLevel Low = new(nameof(Low), 0);
+    public static readonly RiskLevel Medium = new(nameof(Medium), 1);
+    public static readonly RiskLevel High = new(nameof(High), 2);
+    public static readonly RiskLevel VeryHigh = new(nameof(VeryHigh), 3);
+
+    public RiskLevel(string name, int value)
+        : base(name, value) { }
+}
---
+++ src/Domain/Entities/Participants/Risk.cs
@@ -0,0 +1,37 @@
+﻿using Cfo.Cats.Domain.Common.Entities;
+using Cfo.Cats.Domain.Common.Enums;
+using Cfo.Cats.Domain.ValueObjects;
+
+namespace Cfo.Cats.Domain.Entities.Participants;
+
+public class Risk : BaseAuditableEntity<int>
+{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    private Risk()
+    {
+    }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+    private readonly List<Note> _notes = new();
+
+    public IReadOnlyCollection<Note> Notes => _notes.AsReadOnly();
+
+    public string ParticipantId { get; private set; }
+    public RiskLevel? RiskToChildren { get; private set; }
+    public RiskLevel? RiskToPublic { get; private set; }
+    public RiskLevel? RiskToKnownAdult { get; private set; }
+    public RiskLevel? RiskToStaff { get; private set; }
+    public RiskLevel? RiskToOtherPrisoners { get; private set; }
+    public RiskLevel? RiskToSelf { get; private set; }
+
+    public Risk AddNote(Note note)
+    {
+        if (_notes.Contains(note) is false)
+        {
+            _notes.Add(note);
+        }
+
+        return this;
+    }
+
+}
