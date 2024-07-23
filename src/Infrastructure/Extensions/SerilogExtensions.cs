54160074c85a8d2594ffae04a4bb5709d64e352a
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2024-07-23 09:55:18 +0100

    Adjust the customerror component to hide exception
    - Hide error and stack trace if not in Development Environment
    - Adjust launch settings so we can fire off a different environment from
      VS
    - Adjust appsettings.config to comment out Sentry, as running in none
      dev environment attempts to use an invalid url due to user secrets not
      loading under this profile

--- src/Infrastructure/Extensions/SerilogExtensions.cs
+++ src/Infrastructure/Extensions/SerilogExtensions.cs
@@ -1,10 +1,5 @@
-using System.Configuration;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
 using Serilog;
-using Serilog.Configuration;
-using Serilog.Core;
-using Serilog.Events;
 
 namespace Cfo.Cats.Infrastructure.Extensions;
 
