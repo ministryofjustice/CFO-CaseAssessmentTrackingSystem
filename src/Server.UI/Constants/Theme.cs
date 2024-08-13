4839329cc0f03d8a492bfa32b1986311d4e858c5
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2024-08-13 10:24:40 +0100

    Allow override of primary theme.
    Override the primary colour to allow different environments to be obvious

--- src/Server.UI/Constants/Theme.cs
+++ src/Server.UI/Constants/Theme.cs
@@ -2,34 +2,39 @@
 
 public static class Theme
 {
-    public static MudTheme ApplicationTheme()
+    /// <summary>
+    /// The default primary colour for the site.
+    /// </summary>
+    public const string DefaultPrimaryColour = "#722660";
+
+    public static MudTheme ApplicationTheme(string primaryColour)
     {
         var theme = new MudTheme()
         {
             PaletteLight = new PaletteLight
             {
-                Primary = "#722660", // the default CFO Purple
+                Primary = primaryColour, // the default CFO Purple
                 Secondary = "#494f56",
                 Success = "#62ae2f",
                 Info = "#0c6980",
                 Black = "#000000",
-                TableLines = "#722660",
+                TableLines = primaryColour,
                 Tertiary = "#FFFFFF",
-                AppbarBackground = "#722660",
+                AppbarBackground = primaryColour,
                 AppbarText = "#FFFFFF",
                 Error="#CC0033",
                 White = "#FFFFFF",
             },
             PaletteDark = new PaletteDark
             {
-                Primary = "#AA3C85", // Adjusted CFO Purple for dark mode
+                Primary = primaryColour, // Adjusted CFO Purple for dark mode
                 Secondary = "#666B73", // Lighter than #494f56 for better contrast on dark backgrounds
                 Success = "#76c043", // Slightly brighter than #62ae2f for visibility
                 Info = "#1493A3", // Brighter than #0c6980 for better contrast
                 Black = "#FFFFFF", // White to contrast dark backgrounds
-                TableLines = "#AA3C85", // Same as Primary to maintain consistency
+                TableLines = primaryColour, // Same as Primary to maintain consistency
                 Tertiary = "#FFFFFF", // Very dark grey for background contrast
-                AppbarBackground = "#AA3C85", // Matching Tertiary for a unified dark theme
+                AppbarBackground = primaryColour, // Matching Tertiary for a unified dark theme
                 AppbarText = "#FFFFFF",
                 Error = "#FF4D4D", // Brighter red than #CC0033 for better visibility,
                 White = "#FFFFFF",
