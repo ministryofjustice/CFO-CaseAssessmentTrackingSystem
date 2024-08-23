713011c6d49d25d53ed3abb10480781418bdef2a
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2024-08-23 13:09:09 +0100

    fix build

--- src/Application/Features/QualityAssurance/Commands/SubmitPqaResponse.cs
+++ src/Application/Features/QualityAssurance/Commands/SubmitPqaResponse.cs
@@ -30,7 +30,7 @@ public static class SubmitPqaResponse
             }
             
             entry.Complete(request.Accept.GetValueOrDefault(), request.Message);
-            EnrolmentStatus transitionTo = request.Accept ? EnrolmentStatus.SubmittedToAuthorityStatus : EnrolmentStatus.PendingStatus;
+            EnrolmentStatus transitionTo = request.Accept.GetValueOrDefault() ? EnrolmentStatus.SubmittedToAuthorityStatus : EnrolmentStatus.PendingStatus;
             entry.Participant!.TransitionTo(transitionTo);
                         
             return Result.Success();
