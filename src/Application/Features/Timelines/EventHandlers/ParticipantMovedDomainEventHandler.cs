25622c474e028f9653d00dda3b1fd799a6153891
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-08-28 14:54:27 +0100

    Quartz - Job schedule for retrieving participant information

---
+++ src/Application/Features/Timelines/EventHandlers/ParticipantMovedDomainEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using Cfo.Cats.Domain.Events;
+
+namespace Cfo.Cats.Application.Features.Timelines.EventHandlers;
+
+public class ParticipantMovedDomainEventHandler(
+    ICurrentUserService currentUserService, 
+    IUnitOfWork unitOfWork) : TimelineNotificationHandler<ParticipantMovedDomainEvent>(currentUserService, unitOfWork)
+{
+    protected override TimelineEventType GetEventType() => TimelineEventType.PathwayPlan;
+
+    protected override string GetLine1(ParticipantMovedDomainEvent notification) => "Participant moved locations";
+
+    protected override string? GetLine2(ParticipantMovedDomainEvent notification) => $"To {notification.To.Name}";
+    protected override string? GetLine3(ParticipantMovedDomainEvent notification) => $"From {notification.From.Name}";
+
+    protected override string GetParticipantId(ParticipantMovedDomainEvent notification) => notification.Item.Id;
+}
