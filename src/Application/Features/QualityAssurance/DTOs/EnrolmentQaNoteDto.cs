e4ececb49ee79ddb1d713c26deb904193d6212e8
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2024-08-30 10:56:17 +0100

    Record and display notes as part of the process

---
+++ src/Application/Features/QualityAssurance/DTOs/EnrolmentQaNoteDto.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cfo.Cats.Domain.ValueObjects;
+
+namespace Cfo.Cats.Application.Features.QualityAssurance.DTOs
+{
+    #nullable disable
+    public class EnrolmentQaNoteDto
+    {
+        public required DateTime Created {get;set;}
+        public required string Message { get;set; }
+        public required string CreatedBy { get;set; }
+
+        private class Mapper : Profile
+        {
+            
+            public Mapper()
+            {
+                CreateMap<Note, EnrolmentQaNoteDto>()
+                    .ForMember(target => target.CreatedBy, options => options.MapFrom(source=>source.CreatedByUser.DisplayName))
+                    .ForMember(target => target.Message, options => options.MapFrom(source => source.Message))
+                    .ForMember(target => target.Created, options => options.MapFrom(source => source.Created));
+            }
+
+
+        }
+
+
+    }
+}
