842d597fbf593a450c125c272c407ba98b86b383
VS <vinay.saini@justice.gov.uk>
2024-09-03 10:52:43 +0100

    Added Documents tab on participant dashboard, it lists all the documents assciated with the case. From the list, user can open and view each documents

---
+++ src/Application/Features/Documents/Queries/GetByParticipantId.cs
@@ -0,0 +1,47 @@
+﻿using Cfo.Cats.Application.Common.Security;
+using Cfo.Cats.Application.Common.Validators;
+using Cfo.Cats.Application.Features.Documents.DTOs;
+using Cfo.Cats.Application.Features.Participants.DTOs;
+using Cfo.Cats.Application.SecurityConstants;
+using DocumentFormat.OpenXml.InkML;
+using System.Threading.Tasks;
+
+namespace Cfo.Cats.Application.Features.Documents.Queries;
+public static class GetByParticipantId
+{
+    [RequestAuthorize(Policy = SecurityPolicies.AuthorizedUser)]
+
+    public class Query : IRequest<Result<DocumentDto[]>>
+    {
+        public required string ParticipantId { get; set; }
+    }
+
+    public class Handler(IUnitOfWork unitOfWork, IMapper mapper) : IRequestHandler<Query, Result<DocumentDto[]>>
+    {
+        public async Task<Result<DocumentDto[]>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            string likeCriteria = string.Format(@"Files/{0}%", request.ParticipantId);
+            var query = unitOfWork.DbContext.Documents
+                .Where(d => EF.Functions.Like(d.URL, likeCriteria));
+
+            var documents = await query.ProjectTo<DocumentDto>(mapper.ConfigurationProvider)
+                .ToArrayAsync(cancellationToken) ?? [];
+
+            return Result<DocumentDto[]>.Success(documents);
+        }
+    }
+    public class Validator : AbstractValidator<Query>
+    {
+        public Validator()
+        {
+            RuleFor(x => x.ParticipantId.ToString())
+                .NotEmpty()
+                .MinimumLength(9)
+                .MaximumLength(9)
+                .Matches(ValidationConstants.AlphaNumeric)
+                .WithMessage(string.Format(ValidationConstants.AlphaNumericMessage, "Participant Id"));
+
+        }
+    }
+}
+
