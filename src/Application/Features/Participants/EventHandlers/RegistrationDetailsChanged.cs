70cc100b0c88d2e11d2ba95032da6a0a2e7a77f5
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-09-05 12:41:44 +0100

    Add event handler for registration details update (currently does nothing)

---
+++ src/Application/Features/Participants/EventHandlers/RegistrationDetailsChanged.cs
@@ -0,0 +1,20 @@
+﻿using Cfo.Cats.Domain.Events;
+
+namespace Cfo.Cats.Application.Features.Participants.EventHandlers;
+
+public class RegistrationDetailsChanged(IUnitOfWork unitOfWork) : INotificationHandler<ParticipantRegistrationDetailsChangedDomainEvent>
+{
+    public async Task Handle(ParticipantRegistrationDetailsChangedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        var risk = await unitOfWork.DbContext.Risks
+            .Where(risk => risk.ParticipantId == notification.Item.Id)
+            .OrderBy(risk => risk.Created)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if(risk is not null)
+        {
+            //risk.SetDue(DateTime.UtcNow);
+        }
+
+    }
+}
