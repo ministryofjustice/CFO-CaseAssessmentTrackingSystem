8c15851ec025a607172d73600c78c7a210e9fbf0
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-09-06 15:51:35 +0100

    Feedback for risk
    Removes declaration step, and renames "referrer" on the front end

--- src/Migrators/Migrators.MSSQL/Migrations/20240906143957_Risk_RemoveDeclaration.Designer.cs
+++ src/Migrators/Migrators.MSSQL/Migrations/20240906143957_Risk_RemoveDeclaration.Designer.cs
@@ -4,6 +4,7 @@ using Cfo.Cats.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cfo.Cats.Migrators.MSSQL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240906143957_Risk_RemoveDeclaration")]
+    partial class Risk_RemoveDeclaration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -1152,9 +1155,6 @@ namespace Cfo.Cats.Migrators.MSSQL.Migrations
 
                     b.Property<bool>("IsLive")
                         .HasColumnType("bit");
-
-                    b.Property<DateTime?>("LastLogin")
-                        .HasColumnType("datetime2");
 
                     b.Property<DateTime?>("LastModified")
                         .HasColumnType("datetime2");
