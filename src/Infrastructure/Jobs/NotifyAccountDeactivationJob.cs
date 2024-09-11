4cc8c050630b4dda4a294f80eb13c02b197a2fe7
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-09-11 09:54:24 +0100

    Send account deactivation reminders

---
+++ src/Infrastructure/Jobs/NotifyAccountDeactivationJob.cs
@@ -0,0 +1,51 @@
+﻿using Cfo.Cats.Domain.Identity;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Quartz;
+
+namespace Cfo.Cats.Infrastructure.Jobs;
+
+public class NotifyAccountDeactivationJob(
+    ILogger<NotifyAccountDeactivationJob> logger,
+    UserManager<ApplicationUser> userManager,
+    ICommunicationsService communicationsService) : IJob
+{
+    public static readonly JobKey Key = new JobKey(name: nameof(NotifyAccountDeactivationJob));
+    public static readonly string Description = "A job to notify accounts that are due to deactivate.";
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        using (logger.BeginScope(Key))
+
+        if (context.RefireCount > 3)
+        {
+            logger.LogWarning($"Failed to complete within 3 tries, aborting...");
+            return;
+        }
+
+        try
+        {
+            logger.LogInformation("Notifying accounts that will be deactivated soon");
+
+            DateTime sevenDaysFromDeactivationDate = DateTime.Now
+                .AddDays(-30) // 30 days ago
+                .AddDays(7); // 7 days before
+
+            var users = await userManager.Users
+                .IgnoreAutoIncludes()
+                .Where(user => user.IsActive)
+                .Where(user => user.LastLogin < sevenDaysFromDeactivationDate
+                    || (user.LastLogin == null && user.Created < sevenDaysFromDeactivationDate))
+                .ToListAsync();
+
+            users.ForEach(Notify);
+
+            async void Notify(ApplicationUser user) => await communicationsService.SendAccountDeactivationEmail(user.Email!);
+        }
+        catch(Exception ex)
+        {
+            throw new JobExecutionException(msg: $"An unexpected error occurred executing job", refireImmediately: true, cause: ex);
+        }
+
+    }
+
+}
--- src/Infrastructure/Services/CommunicationsService.cs
+++ src/Infrastructure/Services/CommunicationsService.cs
@@ -10,7 +10,7 @@ public class CommunicationsService(IOptions<NotifyOptions> options, ILogger<Comm
         {
             var client = Client();
             var response = await client.SendSmsAsync(mobileNumber: mobileNumber,
-            templateId: options.Value.SmsTemplate,
+            templateId: options.Value.GetTemplate("TwoFactorCode")!.SmsTemplateId,
             personalisation: new Dictionary<string, dynamic>()
             {
                 {
@@ -30,7 +30,7 @@ public class CommunicationsService(IOptions<NotifyOptions> options, ILogger<Comm
         {
             var client = Client();
             var response = await client.SendEmailAsync(emailAddress: email,
-            templateId: options.Value.EmailTemplate,
+            templateId: options.Value.GetTemplate("TwoFactorCode")!.EmailTemplateId,
             personalisation: new Dictionary<string, dynamic>() {
                 {
                     "subject",
@@ -46,14 +46,45 @@ public class CommunicationsService(IOptions<NotifyOptions> options, ILogger<Comm
             logger.LogError("Failed to send Email code. {e}", e);
         }
     }
-    
+ 
+    public async Task SendAccountDeactivationEmail(string email)
+    {
+        try
+        {
+            var client = Client();
+            var response = await client.SendEmailAsync(emailAddress: email,
+            templateId: options.Value.GetTemplate("AccountDeactivationReminder")!.EmailTemplateId,
+            personalisation: new Dictionary<string, dynamic>() {
+                {
+                    "subject",
+                    "Your account will be deactivated."
+                },
+                {
+                    "body", 
+                    "Your account will be deactivated if you do not login soon"
+                }
+            });
+        }
+        catch (Exception e)
+        {
+            logger.LogError("Failed to send Email. {e}", e);
+        }
+    }
+
     private NotificationClient Client() => new(options.Value.ApiKey);
 }
 
 public class NotifyOptions
 {
     public const string Notify = "Notify";
-    public string ApiKey { get; set; } = default!;
-    public string SmsTemplate { get; set; } = default!;
-    public string EmailTemplate { get; set; } = default!;
+    public required string ApiKey { get; set; }
+    public IEnumerable<Template> Templates { get; set; } = [];
+    public Template? GetTemplate(string key) => Templates.FirstOrDefault(template => template.Key == key);
+}
+
+public class Template
+{
+    public required string Key { get; set; }
+    public string? EmailTemplateId { get; set; }
+    public string? SmsTemplateId { get; set; }
 }
