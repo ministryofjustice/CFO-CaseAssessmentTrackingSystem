e5e0b861bf5dfbbb10ed59a6681af18fdd3fdc61
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2024-09-15 16:32:01 +0100

    Add filter

---
+++ src/Application/Features/Notifications/Queries/NotificationsWithPaginationQuery.cs
@@ -0,0 +1,30 @@
+using Cfo.Cats.Application.Common.Security;
+using Cfo.Cats.Application.Features.Notifications.DTOs;
+using Cfo.Cats.Application.Features.Notifications.Specifications;
+using Cfo.Cats.Application.SecurityConstants;
+using Cfo.Cats.Domain.Entities.Notifications;
+
+namespace Cfo.Cats.Application.Features.Notifications.Queries;
+
+public static class NotificationsWithPaginationQuery
+{
+    [RequestAuthorize(Policy = SecurityPolicies.AuthorizedUser)]
+    public class Query : NotificationsAdvancedFilter, IRequest<PaginatedData<NotificationDto>>
+    {
+        public NotificationAdvancedSpecification Specification => new(this);
+    }
+
+    public class Handler(IUnitOfWork unitOfWork, IMapper mapper)
+        : IRequestHandler<Query, PaginatedData<NotificationDto>>
+    {
+        public async Task<PaginatedData<NotificationDto>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var data = await unitOfWork.DbContext.Notifications
+                .OrderBy($"{request.OrderBy} {request.SortDirection}")
+                .ProjectToPaginatedDataAsync<Notification, NotificationDto>(request.Specification, request.PageNumber,
+                    request.PageSize, mapper.ConfigurationProvider, cancellationToken);
+
+            return data;
+        }
+    }
+}
