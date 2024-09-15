d42174b943bf5d12c562d12cab672c52ffd497eb
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2024-09-15 16:32:01 +0100

    Add filter

---
+++ src/Application/Features/Notifications/DTOs/NotificationDto.cs
@@ -0,0 +1,25 @@
+using Cfo.Cats.Application.Features.Identity.DTOs;
+using Cfo.Cats.Domain.Entities.Notifications;
+using DocumentFormat.OpenXml.Drawing.Charts;
+
+namespace Cfo.Cats.Application.Features.Notifications.DTOs;
+
+public class NotificationDto
+{
+    public string Heading { get; set; } = default!;
+    public string Details { get; set; } = default!;
+    
+    public DateTime NotificationDate { get; set; }
+
+    private class Mapper : Profile
+    {
+        public Mapper()
+        {
+            CreateMap<Notification, NotificationDto>(MemberList.Destination)
+                .ForMember(t => t.Heading, opt => opt.MapFrom(src => src.Heading))
+                .ForMember(t => t.Details, opt => opt.MapFrom(src => src.Details))
+                .ForMember(t => t.NotificationDate, opt => opt.MapFrom(src => src.Created));
+        }
+    }
+
+}
