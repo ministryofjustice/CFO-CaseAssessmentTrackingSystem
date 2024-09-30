beaffe0bd5a44fa53367718338c18b9367e82dda
VS <vinay.saini@justice.gov.uk>
2024-09-30 09:31:17 +0100

    Risk due date is now reset to 70 days when user reviews the Risk with options ‘No risk information available’ or ‘No change’

---
+++ src/Application/Features/Participants/EventHandlers/RiskInformationReviewed.cs
@@ -0,0 +1,20 @@
+﻿using Cfo.Cats.Domain.Events;
+
+namespace Cfo.Cats.Application.Features.Participants.EventHandlers;
+
+public class RiskInformationReviewed(IUnitOfWork unitOfWork) : INotificationHandler<RiskInformationReviewedDomainEvent>
+{
+    public async Task Handle(RiskInformationReviewedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        var participant = await unitOfWork.DbContext.Participants
+            .Where(p => p.Id == notification.Item.ParticipantId)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (participant is not null)
+        {
+            participant.SetRiskDue(DateTime.UtcNow.AddDays(70));
+            unitOfWork.DbContext.Participants.Update(participant);
+        }
+
+    }
+}
