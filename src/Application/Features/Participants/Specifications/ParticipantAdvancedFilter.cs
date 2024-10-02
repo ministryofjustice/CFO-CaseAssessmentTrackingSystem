6bd89432696fa10daa6264a9fe6223265cff757d
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2024-10-02 13:40:15 +0100

    CFODEV-764: Display other peoples participants.
    - User can select to view all Participants
    - User can filter by one or more locations

--- src/Application/Features/Participants/Specifications/ParticipantAdvancedFilter.cs
+++ src/Application/Features/Participants/Specifications/ParticipantAdvancedFilter.cs
@@ -5,8 +5,27 @@ namespace Cfo.Cats.Application.Features.Participants.Specifications;
 public class ParticipantAdvancedFilter
     : PaginationFilter
 {
+    /// <summary>
+    /// The filter for the list (based on the status)
+    /// </summary>
     public ParticipantListView ListView { get; set; } = ParticipantListView.Default;
+
+    /// <summary>
+    /// The currently logged in user
+    /// </summary>
     public UserProfile? CurrentUser { get; set; }
+
+    /// <summary>
+    /// Flag to indicate that you only want to see your own cases.
+    /// </summary>
+    [Description("Just My Cases")]
+    public bool JustMyCases { get; set; } = true;
+
+    /// <summary>
+    /// The current location of the participant
+    /// </summary>
+    public int[] Locations { get; set; } = [];
+
 }
 
 public enum ParticipantListView
