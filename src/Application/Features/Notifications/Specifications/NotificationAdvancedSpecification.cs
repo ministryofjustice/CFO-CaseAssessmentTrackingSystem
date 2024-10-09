8d15dcfe92282d0fbc3983c6a21162d20a53e1a4
VS <vinay.saini@justice.gov.uk>
2024-10-09 12:28:08 +0100

    added return notifications and notifications screen

--- src/Application/Features/Notifications/Specifications/NotificationAdvancedSpecification.cs
+++ src/Application/Features/Notifications/Specifications/NotificationAdvancedSpecification.cs
@@ -6,7 +6,7 @@ public sealed class NotificationAdvancedSpecification : Specification<Notificati
 {
     public NotificationAdvancedSpecification(NotificationsAdvancedFilter filter)
     {
-        Query.Where(r => r.ReadDate == null, filter.IncludeUnreadNotifications == false)
+        Query.Where(r => r.ReadDate == null, filter.IncludeReadNotifications == false)
             .Where(n => n.OwnerId == filter.CurrentUser!.UserId);
     }
 }
