986d34b283dfdad6c2495b4c87d8f1a6a9e1862e
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-10-11 14:55:17 +0100

    SyncParticipantsJob - update nationality

---
+++ src/Application/Features/Timelines/EventHandlers/ParticipantNationalityChangedDomainEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using Cfo.Cats.Domain.Events;
+
+namespace Cfo.Cats.Application.Features.Timelines.EventHandlers;
+
+public class ParticipantNationalityChangedDomainEventHandler(
+    ICurrentUserService currentUserService,
+    IUnitOfWork unitOfWork) : TimelineNotificationHandler<ParticipantNationalityChangedDomainEvent>(currentUserService, unitOfWork)
+{
+    protected override TimelineEventType GetEventType() => TimelineEventType.Dms;
+
+    protected override string GetLine1(ParticipantNationalityChangedDomainEvent notification) => "Participant nationality updated";
+
+    protected override string? GetLine2(ParticipantNationalityChangedDomainEvent notification) => $"To {notification.To}";
+    protected override string? GetLine3(ParticipantNationalityChangedDomainEvent notification) => $"From {notification.From}";
+
+    protected override string GetParticipantId(ParticipantNationalityChangedDomainEvent notification) => notification.Item.Id;
+}
