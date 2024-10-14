4f66941ed7055e71b76a6320792ba6958bbf0545
PaulCooperWorkJustice <paul.cooper5@justice.gov.uk>
2024-10-14 15:24:57 +0100

    CFODEV-776 -  Last Login is now updated either 2fa and non 2fa logins.  Removed old last login update on custom sign in manager so its only in one place.

--- src/Infrastructure/Services/Identity/CustomSigninManager.cs
+++ src/Infrastructure/Services/Identity/CustomSigninManager.cs
@@ -8,8 +8,7 @@ namespace Cfo.Cats.Infrastructure.Services.Identity;
 
 public class CustomSigninManager(UserManager<ApplicationUser> userManager, IHttpContextAccessor contextAccessor, IUserClaimsPrincipalFactory<ApplicationUser> claimsFactory, IOptions<IdentityOptions> optionsAccessor, ILogger<SignInManager<ApplicationUser>> logger, IAuthenticationSchemeProvider schemes, IUserConfirmation<ApplicationUser> confirmation, IOptions<AllowlistOptions> allowlistOptions, INetworkIpProvider networkIpAccessor)
     : SignInManager<ApplicationUser>(userManager, contextAccessor, claimsFactory, optionsAccessor, logger, schemes, confirmation)
-{
-    
+{    
     public override async Task<SignInResult> PasswordSignInAsync(string userName, string password, bool isPersistent, bool lockoutOnFailure)
     {
         var user = await UserManager.FindByNameAsync(userName);
@@ -42,17 +41,9 @@ public class CustomSigninManager(UserManager<ApplicationUser> userManager, IHttp
         {
             await SignInAsync(user, isPersistent);
             return passwordCheckResult;
-        }
-        
-        var signInResult = await base.PasswordSignInAsync(userName, password, isPersistent, lockoutOnFailure);
+        }        
 
-        if(signInResult.Succeeded)
-        {
-            user.LastLogin = DateTime.UtcNow;
-            await UserManager.UpdateAsync(user);
-        }
-
-        return signInResult;
+        return await base.PasswordSignInAsync(userName, password, isPersistent, lockoutOnFailure); 
     }
 
     private bool PasswordCheckSucceededAndTwoFactorDisabledForIpRange(SignInResult passwordCheckResult, string? ipAddress)
@@ -79,6 +70,7 @@ public class CustomSigninManager(UserManager<ApplicationUser> userManager, IHttp
 
         return false;
     }
+
     private static bool PasswordChecksOutAndRequiresPasswordReset(SignInResult passwordCheckResult, ApplicationUser user) => passwordCheckResult.Succeeded && user.RequiresPasswordReset;
 
     public class CustomSignInResult : SignInResult
@@ -88,6 +80,5 @@ public class CustomSigninManager(UserManager<ApplicationUser> userManager, IHttp
 
         public static CustomSignInResult PasswordResetRequired => new CustomSignInResult { RequiresPasswordReset = true };
         public static CustomSignInResult Inactive => new CustomSignInResult { IsInactive = true };
-    }
-    
+    }    
 }
