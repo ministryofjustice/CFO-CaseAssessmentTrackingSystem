424fbab2dc748c503a961efdeb243a2b7b1d4ec8
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2024-10-22 14:52:18 +0100

    Adjustments
    Some changes to the way notifications are raised and displayed.
    - only one per event while one is open

---
+++ src/Application/Features/Notifications/EventHandlers/NotifyOwnerParticipantHasBeenReturnedEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using Cfo.Cats.Domain.Entities.Notifications;
+using Cfo.Cats.Domain.Events;
+
+namespace Cfo.Cats.Application.Features.Notifications.EventHandlers;
+
+public class NotifyOwnerParticipantHasBeenReturnedEventHandler(IUnitOfWork unitOfWork) : INotificationHandler<ParticipantTransitionedDomainEvent>
+{
+    public async Task Handle(ParticipantTransitionedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        if (notification.From == EnrolmentStatus.SubmittedToProviderStatus && notification.To == EnrolmentStatus.EnrollingStatus)
+        {
+            const string heading = "Enrolment returned";
+
+            string details = "You have enrolments that have been returned by PQA";
+            
+            if(unitOfWork.DbContext.Notifications.Any(n => 
+                   n.Heading == heading 
+                   && n.OwnerId == notification.Item.OwnerId
+                   && n.ReadDate == null
+                   ) == false)
+            {
+                var n = Notification.Create(heading, details, notification.Item.OwnerId!);
+                n.SetLink($"/pages/participants/");
+                await unitOfWork.DbContext.Notifications.AddAsync(n, cancellationToken);
+            }
+        }
+    }
+}
+
+
+
