a884d223604ba5002a7da08e3c7c1fea4a521c49
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2024-11-04 10:09:48 +0000

    Add performance manager role

--- src/Application/SecurityConstants/RoleNames.cs
+++ src/Application/SecurityConstants/RoleNames.cs
@@ -2,19 +2,11 @@
 
 public static class RoleNames
 {
-    /// <summary>
-    /// A provider role, perform QA and finance services for a provider
-    /// </summary>
     public const string QAFinance = "QA + Finance";
-    
-    /// <summary>
-    /// A CFO QA Officer
-    /// </summary>
     public const string QAOfficer = "CFO QA Officer";
-    
-    
     public const string QASupportManager = "CFO QA Support Manager";
     public const string QAManager = "CFO QA Manager";
+    public const string PerformanceManager = "Performance Manager";
     public const string SMT = "SMT";
     public const string SystemSupport = "System Support";
 }
