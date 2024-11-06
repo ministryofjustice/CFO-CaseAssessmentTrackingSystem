ecee23509134a69c8c1fa0aeb040abb2bc14da99
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-11-06 14:16:14 +0000

    CFODEV-824: (WIP) Participant Transfer queue base entity & transfer type

---
+++ src/Domain/Common/Enums/TransferLocationType.cs
@@ -0,0 +1,26 @@
+﻿using Ardalis.SmartEnum;
+using Cfo.Cats.Domain.Entities.Administration;
+
+namespace Cfo.Cats.Domain.Common.Enums;
+
+public class TransferLocationType(string name, int value) : SmartEnum<TransferLocationType>(name, value)
+{
+    public static readonly TransferLocationType CommunityToCommunity = new("Community to Community", 0);
+    public static readonly TransferLocationType CommunityToCustody = new("Community to Custody", 1);
+    public static readonly TransferLocationType CustodyToCommunity = new("Custody to Community", 2);
+    public static readonly TransferLocationType CustodyToCustody = new("Custody to Custody", 3);
+
+    public static TransferLocationType DetermineFromLocationTypes(Location from, Location to)
+    {
+        // IsFromCustody: false == Community
+        return (IsFromCustody: from.LocationType.IsCustody, IsToCustody: to.LocationType.IsCustody) 
+            switch
+            {
+                (IsFromCustody: false, IsToCustody: false) => CommunityToCommunity,
+                (IsFromCustody: false, IsToCustody: true) => CommunityToCustody,
+                (IsFromCustody: true, IsToCustody: false) => CustodyToCommunity,
+                (IsFromCustody: true, IsToCustody: true) => CustodyToCustody
+            };
+    }
+
+}
---
+++ src/Domain/Entities/Participants/ParticipantOutgoingTransferQueueEntry.cs
@@ -0,0 +1,23 @@
+﻿using Cfo.Cats.Domain.Entities.Administration;
+
+namespace Cfo.Cats.Domain.Entities.Participants;
+
+public class ParticipantOutgoingTransferQueueEntry : ParticipantTransferQueueEntry
+{
+    private ParticipantOutgoingTransferQueueEntry(
+        string participantId,
+        Location fromLocation,
+        Location toLocation,
+        Contract? fromContract,
+        Contract? toContract,
+        DateTime moveOccured)
+    : base(participantId, fromLocation, toLocation, fromContract, toContract, moveOccured) { }
+
+    public static ParticipantOutgoingTransferQueueEntry Create(
+        string participantId,
+        Location fromLocation,
+        Location toLocation,
+        Contract? fromContract,
+        Contract? toContract,
+        DateTime moveOccured) => new(participantId, fromLocation, toLocation, fromContract, toContract, moveOccured);
+}
