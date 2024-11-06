a7b89cca7e0ad79303c16da8d04f6e4274423c68
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-11-06 14:16:14 +0000

    CFODEV-824: (WIP) Participant Transfer queue base entity & transfer type

---
+++ src/Domain/Entities/Participants/ParticipantIncomingTransferQueueEntry.cs
@@ -0,0 +1,36 @@
+﻿using Cfo.Cats.Domain.Entities.Administration;
+
+namespace Cfo.Cats.Domain.Entities.Participants;
+
+public class ParticipantIncomingTransferQueueEntry : ParticipantTransferQueueEntry
+{
+    private ParticipantIncomingTransferQueueEntry(
+        string participantId, 
+        Location fromLocation, 
+        Location toLocation, 
+        Contract? fromContract, 
+        Contract? toContract, 
+        DateTime moveOccured)
+    : base(participantId, fromLocation, toLocation, fromContract, toContract, moveOccured) { }
+
+    public static ParticipantIncomingTransferQueueEntry Create(
+        string participantId, 
+        Location fromLocation, 
+        Location toLocation, 
+        Contract? fromContract, 
+        Contract? toContract, 
+        DateTime moveOccured) => new(participantId, fromLocation, toLocation, fromContract, toContract, moveOccured);
+
+    /// <summary>
+    /// The completion status of the transfer.
+    /// </summary>
+    public bool Completed { get; private set; }
+
+    public ParticipantIncomingTransferQueueEntry Complete()
+    {
+        Completed = true;
+        //AddDomainEvent(new IncomingTransferCompletedDomainEvent());
+        return this;
+    }
+
+}
