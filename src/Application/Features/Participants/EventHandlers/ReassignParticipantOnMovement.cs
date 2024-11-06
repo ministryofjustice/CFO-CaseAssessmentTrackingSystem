2ccfaaead0786e717bcf85fdb048123922e8d828
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-11-06 09:20:37 +0000

    CFODEV-823: Calculate new assignee on transfer

---
+++ src/Application/Features/Participants/EventHandlers/ReassignParticipantOnMovement.cs
@@ -0,0 +1,51 @@
+﻿using Cfo.Cats.Application.Common.Interfaces.Locations;
+using Cfo.Cats.Domain.Entities.Administration;
+using Cfo.Cats.Domain.Events;
+
+namespace Cfo.Cats.Application.Features.Participants.EventHandlers;
+
+public class ReassignParticipantOnMovement(
+    IUnitOfWork unitOfWork,
+    ILocationService locationService) : INotificationHandler<ParticipantMovedDomainEvent>
+{
+    public async Task Handle(ParticipantMovedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        string? newAssignee = await GetNewAssignee(notification);
+        notification.Item.AssignTo(newAssignee);
+    }
+
+    private async Task<string?> GetNewAssignee(ParticipantMovedDomainEvent notification)
+    {
+        var newAssignee = notification.Item.OwnerId;
+
+        // Todo: Get PRI Assignee
+        if (notification.Item.HasActivePRI())
+        {
+            newAssignee = string.Empty;
+        }
+
+        if (await AssigneeHasAccessToNewLocation(newAssignee, notification.To) is false)
+        {
+            newAssignee = null;
+        }
+
+        return newAssignee;
+    }
+
+    private async Task<bool> AssigneeHasAccessToNewLocation(string? assigneeId, Location to)
+    {
+        // Can we load the Owner instead of fetching from db?
+        var owner = await unitOfWork.DbContext.Users
+            .FindAsync(assigneeId);
+
+        if (owner is null)
+        {
+            return false;
+        }
+
+        return locationService
+            .GetVisibleLocations(owner.TenantId!)
+            .Any(location => location.Id == to.Id);
+    }
+
+}
