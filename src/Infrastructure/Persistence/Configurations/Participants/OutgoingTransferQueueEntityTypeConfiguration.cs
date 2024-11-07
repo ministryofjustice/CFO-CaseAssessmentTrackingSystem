d2b74d80fb2b4c5d297970592a82048bab677306
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-11-07 12:22:24 +0000

    CFODEV-824: Queue EF configurations/migrations

---
+++ src/Infrastructure/Persistence/Configurations/Participants/OutgoingTransferQueueEntityTypeConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Cfo.Cats.Domain.Common.Enums;
+using Cfo.Cats.Domain.Entities.Participants;
+using Cfo.Cats.Infrastructure.Constants.Database;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Cfo.Cats.Infrastructure.Persistence.Configurations.Participants;
+
+public class OutgoingTransferQueueEntityTypeConfiguration : IEntityTypeConfiguration<ParticipantOutgoingTransferQueueEntry>
+{
+    public void Configure(EntityTypeBuilder<ParticipantOutgoingTransferQueueEntry> builder)
+    {
+        builder.ToTable(
+            DatabaseConstants.Tables.OutgoingTransferQueue,
+            DatabaseConstants.Schemas.Participant);
+
+        builder.Property(q => q.ParticipantId)
+            .IsRequired()
+            .HasMaxLength(DatabaseConstants.FieldLengths.ParticipantId);
+
+        builder.HasOne(q => q.FromContract)
+            .WithMany();
+
+        builder.HasOne(q => q.ToContract)
+            .WithMany();
+
+        builder.HasOne(q => q.FromLocation)
+            .WithMany();
+
+        builder.HasOne(q => q.ToLocation)
+            .WithMany();
+
+        builder.Property(q => q.TransferType)
+            .IsRequired()
+            .HasConversion(
+                tlt => tlt!.Value,
+                tlt => TransferLocationType.FromValue(tlt)
+            );
+    }
+}
