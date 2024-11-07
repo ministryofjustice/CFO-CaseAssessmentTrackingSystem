61a50a714ab91a043bee1f1b0b6b589459456eb1
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-11-07 15:06:16 +0000

    Transfer Queue handlers, queries and DTO's

---
+++ src/Application/Features/Transfers/Queries/GetIncomingTransfers.cs
@@ -0,0 +1,47 @@
+﻿using Cfo.Cats.Application.Common.Interfaces.Locations;
+using Cfo.Cats.Application.Common.Security;
+using Cfo.Cats.Application.Features.Transfers.DTOs;
+using Cfo.Cats.Application.SecurityConstants;
+
+namespace Cfo.Cats.Application.Features.Transfers.Queries;
+
+public static class GetIncomingTransfers
+{
+    [RequestAuthorize(Policy = SecurityPolicies.AuthorizedUser)]
+    public class Query : IRequest<Result<IEnumerable<IncomingTransferDto>>>
+    {
+        public string? TenantId { get; set; }
+    }
+
+    class Handler(
+        ICurrentUserService currentUserService,
+        IUnitOfWork unitOfWork,
+        ILocationService locationService,
+        IMapper mapper) : IRequestHandler<Query, Result<IEnumerable<IncomingTransferDto>>>
+    {
+        public async Task<Result<IEnumerable<IncomingTransferDto>>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            List<IncomingTransferDto> transfers = [];
+
+            var locations = locationService.GetVisibleLocations(request.TenantId ?? currentUserService.TenantId!)
+                .Select(location => location.Id);
+
+            if (locations.Any())
+            {
+                transfers = await unitOfWork.DbContext.ParticipantIncomingTransferQueue
+                    .Where(q => locations.Contains(q.ToLocation.Id))
+                    .Where(q => q.Completed == false) // Specification?
+                    .ProjectTo<IncomingTransferDto>(mapper.ConfigurationProvider) // ProjectToPaginatedDataAsync
+                    .ToListAsync(cancellationToken);
+            }
+
+            return transfers;
+        }
+    }
+
+    class Validator : AbstractValidator<Query>
+    {
+
+    }
+
+}
---
+++ src/Application/Features/Transfers/Queries/GetOutgoingTransfers.cs
@@ -0,0 +1,47 @@
+﻿using Cfo.Cats.Application.Common.Interfaces.Locations;
+using Cfo.Cats.Application.Common.Security;
+using Cfo.Cats.Application.Features.Transfers.DTOs;
+using Cfo.Cats.Application.SecurityConstants;
+
+namespace Cfo.Cats.Application.Features.Transfers.Queries;
+
+public static class GetOutgoingTransfers
+{
+    [RequestAuthorize(Policy = SecurityPolicies.AuthorizedUser)]
+    public class Query : IRequest<Result<IEnumerable<OutgoingTransferDto>>>
+    {
+        public string? TenantId { get; set; }
+    }
+
+    class Handler(
+        ICurrentUserService currentUserService,
+        IUnitOfWork unitOfWork,
+        ILocationService locationService,
+        IMapper mapper) : IRequestHandler<Query, Result<IEnumerable<OutgoingTransferDto>>>
+    {
+        public async Task<Result<IEnumerable<OutgoingTransferDto>>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            List<OutgoingTransferDto> transfers = [];
+
+            var locations = locationService.GetVisibleLocations(request.TenantId ?? currentUserService.TenantId!)
+                .Select(location => location.Id);
+
+            if (locations.Any())
+            {
+                transfers = await unitOfWork.DbContext.ParticipantOutgoingTransferQueue
+                    .Where(q => locations.Contains(q.FromLocation.Id))
+                    .Where(q => q.MoveOccured > DateTime.UtcNow.AddDays(-60)) // Specification?
+                    .ProjectTo<OutgoingTransferDto>(mapper.ConfigurationProvider) // ProjectToPaginatedDataAsync
+                    .ToListAsync(cancellationToken);
+            }
+
+            return transfers;
+        }
+    }
+
+    class Validator : AbstractValidator<Query>
+    {
+
+    }
+
+}
