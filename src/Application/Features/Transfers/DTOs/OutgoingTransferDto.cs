f0178773d9b3269486de21a5ba2ccddeb2631a50
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-11-07 15:06:16 +0000

    Transfer Queue handlers, queries and DTO's

---
+++ src/Application/Features/Transfers/DTOs/OutgoingTransferDto.cs
@@ -0,0 +1,23 @@
+﻿using Cfo.Cats.Application.Features.Locations.DTOs;
+using Cfo.Cats.Domain.Entities.Participants;
+
+namespace Cfo.Cats.Application.Features.Transfers.DTOs;
+
+public class OutgoingTransferDto
+{
+    public required string ParticipantId { get; set; }
+    public string? FromContractId { get; set; }
+    public string? ToContractId { get; set; }
+    public required LocationDto FromLocation { get; set; }
+    public required LocationDto ToLocation { get; set; }
+    public required DateTime MoveOccured { get; set; }
+    public required TransferLocationType TransferType { get; set; }
+
+    class Mapper : Profile
+    {
+        public Mapper()
+        {
+            CreateMap<ParticipantOutgoingTransferQueueEntry, OutgoingTransferDto>();
+        }
+    }
+}
