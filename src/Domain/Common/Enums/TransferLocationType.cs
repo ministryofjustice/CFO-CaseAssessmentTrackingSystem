7a9ee69dd4cdf6808919b5200949f5af354abc90
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-11-08 12:21:49 +0000

    CFODEV-827: Determine type (custody/community) of unmapped locations

--- src/Domain/Common/Enums/TransferLocationType.cs
+++ src/Domain/Common/Enums/TransferLocationType.cs
@@ -9,18 +9,62 @@ public class TransferLocationType(string name, int value) : SmartEnum<TransferLo
     public static readonly TransferLocationType CommunityToCustody = new("Community to Custody", 1);
     public static readonly TransferLocationType CustodyToCommunity = new("Custody to Community", 2);
     public static readonly TransferLocationType CustodyToCustody = new("Custody to Custody", 3);
+    public static readonly TransferLocationType CustodyToUnmapped = new("Custody to Unmapped", 4);
+    public static readonly TransferLocationType CommunityToUnmapped = new("Community to Unmapped", 5);
+    public static readonly TransferLocationType UnmappedToCustody = new("Unmapped to Custody", 6);
+    public static readonly TransferLocationType UnmappedToCommunity = new("Unmapped to Community", 7);
+    public static readonly TransferLocationType UnmappedToUnmapped = new("Unmapped to Unmapped", 8);
 
     public static TransferLocationType DetermineFromLocationTypes(Location from, Location to)
     {
-        // IsFromCustody: false == Community
-        return (IsFromCustody: from.LocationType.IsCustody, IsToCustody: to.LocationType.IsCustody) 
-            switch
+        if(from.LocationType == LocationType.UnmappedCustody)
+        {
+            if (to.LocationType.IsCustody)
             {
-                (IsFromCustody: false, IsToCustody: false) => CommunityToCommunity,
-                (IsFromCustody: false, IsToCustody: true) => CommunityToCustody,
-                (IsFromCustody: true, IsToCustody: false) => CustodyToCommunity,
-                (IsFromCustody: true, IsToCustody: true) => CustodyToCustody
-            };
+                return UnmappedToCustody;
+            }
+            else if(to.LocationType.IsCommunity)
+            {
+                return UnmappedToCommunity;
+            }
+        }
+        else if (to.LocationType == LocationType.UnmappedCustody)
+        {
+            if (from.LocationType.IsCustody)
+            {
+                return CustodyToUnmapped;
+            }
+            else if (from.LocationType.IsCommunity)
+            {
+                return CommunityToUnmapped;
+            }
+
+        }
+        else if (to.LocationType.IsCommunity)
+        {
+            if (from.LocationType.IsCustody)
+            {
+                return CustodyToCommunity;
+            }
+            else if (from.LocationType.IsCommunity)
+            {
+                return CommunityToCommunity;
+            }
+
+        }
+        else if (to.LocationType.IsCustody)
+        {
+            if (from.LocationType.IsCustody)
+            {
+                return CustodyToCustody;
+            }
+            else if (from.LocationType.IsCommunity)
+            {
+                return CommunityToCustody;
+            }
+        }
+
+        return UnmappedToUnmapped;
     }
 
 }
