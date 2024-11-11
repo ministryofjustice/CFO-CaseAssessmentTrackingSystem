e75a291c46157365217596287839afefbf0c174c
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-11-11 11:44:33 +0000

    CFODEV-824: fix for missing locations on transfer

--- src/Application/Features/Participants/EventHandlers/QueueTransferOnMovement.cs
+++ src/Application/Features/Participants/EventHandlers/QueueTransferOnMovement.cs
@@ -1,4 +1,4 @@
-﻿using Cfo.Cats.Domain.Entities.Participants;
+using Cfo.Cats.Domain.Entities.Participants;
 using Cfo.Cats.Domain.Events;
 
 namespace Cfo.Cats.Application.Features.Participants.EventHandlers;
@@ -8,30 +8,37 @@ public class QueueTransferOnMovement(IUnitOfWork unitOfWork) : INotificationHand
     public async Task Handle(ParticipantMovedDomainEvent notification, CancellationToken cancellationToken)
     {
         var now = DateTime.UtcNow;
+        var locations = await unitOfWork.DbContext.Locations
+            .Where(l => new[] { notification.From.Id, notification.To.Id }.Contains(l.Id))
+            .Include(l => l.Contract)
+            .ToListAsync(cancellationToken);
+
+        var from = locations.First(l => l.Id == notification.From.Id);
+        var to = locations.First(l => l.Id == notification.To.Id);
 
         // Only publish outgoing transfer if (source/original) contract exists
-        if (notification.From.Contract is not null)
+        if (from is { Contract: not null })
         {
             var outgoingTransfer = ParticipantOutgoingTransferQueueEntry.Create(
                 notification.Item.Id,
-                notification.From,
-                notification.To,
-                notification.From.Contract,
-                notification.To.Contract,
+                from,
+                to,
+                from.Contract,
+                to.Contract,
                 now);
 
             await unitOfWork.DbContext.ParticipantOutgoingTransferQueue.AddAsync(outgoingTransfer, cancellationToken);
         }
 
         // Only publish incoming transfer if (destination/target) contract exists
-        if (notification.To.Contract is not null)
+        if (to is { Contract: not null })
         {
             var incomingTransfer = ParticipantIncomingTransferQueueEntry.Create(
                 notification.Item.Id,
-                notification.From,
-                notification.To,
-                notification.From.Contract,
-                notification.To.Contract,
+                from,
+                to,
+                from.Contract,
+                to.Contract,
                 now);
 
             await unitOfWork.DbContext.ParticipantIncomingTransferQueue.AddAsync(incomingTransfer, cancellationToken);
