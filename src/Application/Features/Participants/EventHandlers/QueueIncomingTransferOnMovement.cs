753c47d761222e7cf9bc5e6f202645b3949be86d
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-11-14 17:29:32 +0000

    Added 'Replaced' status to outgoing queue items

---
+++ src/Application/Features/Participants/EventHandlers/QueueIncomingTransferOnMovement.cs
@@ -0,0 +1,41 @@
+﻿using Cfo.Cats.Domain.Entities.Participants;
+using Cfo.Cats.Domain.Events;
+
+namespace Cfo.Cats.Application.Features.Participants.EventHandlers;
+
+public class QueueIncomingTransferOnMovement(IUnitOfWork unitOfWork) : INotificationHandler<ParticipantMovedDomainEvent>
+{
+    public async Task Handle(ParticipantMovedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        var locations = await unitOfWork.DbContext.Locations
+            .Where(l => new[] { notification.From.Id, notification.To.Id }.Contains(l.Id))
+            .Include(l => l.Contract)
+            .ToDictionaryAsync(l => l.Id, cancellationToken);
+
+        var (from, to) = (
+            locations[notification.From.Id],
+            locations[notification.To.Id]
+        );
+
+        var incomingActiveTransfer = await unitOfWork.DbContext.ParticipantIncomingTransferQueue
+            .Where(q => q.ParticipantId == notification.Item.Id && q.Completed == false)
+            .SingleOrDefaultAsync(cancellationToken);
+
+        // Complete active transfer
+        incomingActiveTransfer?.Complete();
+
+        // Only publish incoming transfer if (destination/target) contract exists
+        if (to.Contract is not null)
+        {
+            var incomingTransfer = ParticipantIncomingTransferQueueEntry.Create(
+                notification.Item.Id,
+                from,
+                to,
+                from.Contract,
+                to.Contract,
+                DateTime.UtcNow);
+
+            await unitOfWork.DbContext.ParticipantIncomingTransferQueue.AddAsync(incomingTransfer, cancellationToken);
+        }
+    }
+}
---
+++ src/Application/Features/Participants/EventHandlers/QueueOutgoingTransferOnMovement.cs
@@ -0,0 +1,53 @@
+﻿using Cfo.Cats.Domain.Entities.Participants;
+using Cfo.Cats.Domain.Events;
+
+namespace Cfo.Cats.Application.Features.Participants.EventHandlers;
+
+public class QueueOutgoingTransferOnMovement(IUnitOfWork unitOfWork) : INotificationHandler<ParticipantMovedDomainEvent>
+{
+    public async Task Handle(ParticipantMovedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        var locations = await unitOfWork.DbContext.Locations
+            .Where(l => new[] { notification.From.Id, notification.To.Id }.Contains(l.Id))
+            .Include(l => l.Contract)
+            .ToDictionaryAsync(l => l.Id, cancellationToken);
+
+        var (from, to) = (
+            locations[notification.From.Id],
+            locations[notification.To.Id]
+        );
+
+        // Find any outgoing transfers which have now become stale. These tranfers are either:
+        // Leaving a contract they have left in the past
+        // OR
+        // Returning to a contract they have left in the past
+        var staleOutgoingTransfers = await unitOfWork.DbContext.ParticipantOutgoingTransferQueue
+            .Where(q => q.IsReplaced == false)
+            .Where(q => q.ParticipantId == notification.Item.Id)
+            //.Include(q => q.FromContract)
+            .Where(q => q.FromContract == from.Contract || q.FromContract == to.Contract)
+            .ToListAsync(cancellationToken);
+
+        foreach(var transfer in staleOutgoingTransfers)
+        {
+            transfer.MarkAsReplaced();
+        }
+
+        // Only publish an outgoing transfer if:
+        // The source/original contract exists
+        // AND...
+        // This is a cross-contract transfer
+        if (from.Contract is not null && from.Contract != to.Contract)
+        {
+            var outgoingTransfer = ParticipantOutgoingTransferQueueEntry.Create(
+                notification.Item.Id,
+                from,
+                to,
+                from.Contract,
+                to.Contract,
+                DateTime.UtcNow);
+
+            await unitOfWork.DbContext.ParticipantOutgoingTransferQueue.AddAsync(outgoingTransfer, cancellationToken);
+        }
+    }
+}
