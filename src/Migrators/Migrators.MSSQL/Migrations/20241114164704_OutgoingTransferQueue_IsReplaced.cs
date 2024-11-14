4165574d08a1322eccfae2a6c5e3b8716f2bdfc4
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-11-14 17:30:03 +0000

    EF migration for outgoing queue replaced status

---
+++ src/Migrators/Migrators.MSSQL/Migrations/20241114164704_OutgoingTransferQueue_IsReplaced.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cfo.Cats.Migrators.MSSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class OutgoingTransferQueue_IsReplaced : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsReplaced",
+                schema: "Participant",
+                table: "OutgoingTransferQueue",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OutgoingTransferQueue_IsReplaced",
+                schema: "Participant",
+                table: "OutgoingTransferQueue",
+                column: "IsReplaced");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_OutgoingTransferQueue_IsReplaced",
+                schema: "Participant",
+                table: "OutgoingTransferQueue");
+
+            migrationBuilder.DropColumn(
+                name: "IsReplaced",
+                schema: "Participant",
+                table: "OutgoingTransferQueue");
+        }
+    }
+}
