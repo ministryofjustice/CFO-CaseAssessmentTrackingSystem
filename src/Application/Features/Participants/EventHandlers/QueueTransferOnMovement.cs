ef83ee213e744fec55fa31578200f6b5d676907a
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-11-19 13:49:56 +0000

    Refactor transfer queue handler
    Fix to only publish outgoing if incoming was completed

--- src/Application/Features/Participants/EventHandlers/QueueTransferOnMovement.cs
+++ src/Application/Features/Participants/EventHandlers/QueueTransferOnMovement.cs
@@ -1,102 +1,95 @@
-﻿using Cfo.Cats.Application.Common.Interfaces.Locations;
+﻿using Cfo.Cats.Domain.Entities.Administration;
 using Cfo.Cats.Domain.Entities.Participants;
 using Cfo.Cats.Domain.Events;
 
 namespace Cfo.Cats.Application.Features.Participants.EventHandlers;
 
-public class QueueTransferOnMovement(IUnitOfWork unitOfWork,        
-      ILocationService locationService) //: INotificationHandler<ParticipantMovedDomainEvent>
+public class QueueTransferOnMovement(IUnitOfWork unitOfWork) : INotificationHandler<ParticipantMovedDomainEvent>
 {
     public async Task Handle(ParticipantMovedDomainEvent notification, CancellationToken cancellationToken)
     {
-        var now = DateTime.UtcNow;
-        var createOutGoingQueue = false;
-
-        //Get all items off Incoming Queue for the participant
-        var allIncomingQueueEntriesForParticipant = await unitOfWork.DbContext.ParticipantIncomingTransferQueue
-            .Where(q => q.ParticipantId == notification.Item.Id)
-            .ToListAsync(cancellationToken);
-
-        //Are there any items on queue ?
-        if (allIncomingQueueEntriesForParticipant.Any())
-        {
-            //Are there already open items on queue for participant ?
-            var openItem = allIncomingQueueEntriesForParticipant?.Where(q => q.Completed == false).FirstOrDefault();
-            if (openItem != null)
-            {
-                //get list of owners
-                var previouslyOwned = await unitOfWork.DbContext.ParticipantOwnershipHistories
-                    .Where(x => x.ParticipantId == notification.Item.Id
-                    && x.From > DateTime.UtcNow.AddDays(-90))
-                    .ToListAsync(cancellationToken);               
-
-                //are there previous owners
-                if (previouslyOwned.Any())
-                {
-                    foreach (var previousOwer in previouslyOwned)
-                    {
-                        var ownedLocations = locationService.GetVisibleLocations(previousOwer.TenantId).Select(location => location.Id);
-
-                        //Dont add to Outgoing queue unless particiapnt has been assigned to them in last 3 months. (ownership table)
-                        if (ownedLocations.Any())
-                        {
-                            createOutGoingQueue = true;
-                        }
-                    }
-                }
-
-                //close open item on queue                
-                openItem?.Complete();
-            }
-            //inputqueue item on queue but been closed / assigned
-            else
-            {
-                createOutGoingQueue = true;
-            }
-        }
-        //Effectively first time transferred.
-        else
-        {
-            createOutGoingQueue = true;
-        }
+        string participantId = notification.Item.Id;
 
         var locations = await unitOfWork.DbContext.Locations
             .Where(l => new[] { notification.From.Id, notification.To.Id }.Contains(l.Id))
             .Include(l => l.Contract)
-            .ToListAsync(cancellationToken);
+            .ToDictionaryAsync(l => l.Id, cancellationToken);
 
-        var from = locations.First(l => l.Id == notification.From.Id);
-        var to = locations.First(l => l.Id == notification.To.Id);
+        var (from, to) = (locations[notification.From.Id], locations[notification.To.Id]);
 
-        if (createOutGoingQueue)
+        await CloseStaleOutgoingTransfers(participantId, from, to, cancellationToken);
+
+        var latestIncomingTransfer = await unitOfWork.DbContext.ParticipantIncomingTransferQueue
+            .Where(q => q.ParticipantId == participantId)
+            .OrderByDescending(q => q.Created)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (latestIncomingTransfer is { Completed: true })
         {
-            // Only publish outgoing transfer if (source/original) contract exists
-            if (from is { Contract: not null })
-            {
-                var outgoingTransfer = ParticipantOutgoingTransferQueueEntry.Create(
-                    notification.Item.Id,
-                    from,
-                    to,
-                    from.Contract,
-                    to.Contract,
-                    now);
-
-                await unitOfWork.DbContext.ParticipantOutgoingTransferQueue.AddAsync(outgoingTransfer, cancellationToken);
-            }
+            await PublishOutgoingTransfer(participantId, from, to, cancellationToken);
+        }
+        else
+        {
+            latestIncomingTransfer?.Complete();
         }
 
-        // Only publish incoming transfer if (destination/target) contract exists
-        if (to is { Contract: not null })
+        await PublishIncomingTransfer(participantId, from, to, cancellationToken);
+
+    }
+
+    async Task PublishOutgoingTransfer(string participantId, Location from, Location to, CancellationToken cancellationToken)
+    {
+        // Only publish an outgoing transfer if:
+        // The source/original contract exists
+        // AND...
+        // This is a cross-contract transfer
+        if (from.Contract is not null && from.Contract != to.Contract)
         {
-            var incomingTransfer = ParticipantIncomingTransferQueueEntry.Create(
-                notification.Item.Id,
+            var outgoingTransfer = ParticipantOutgoingTransferQueueEntry.Create(
+                participantId,
                 from,
                 to,
                 from.Contract,
                 to.Contract,
-                now);
+                DateTime.UtcNow);
+
+            await unitOfWork.DbContext.ParticipantOutgoingTransferQueue.AddAsync(outgoingTransfer, cancellationToken);
+        }
+    }
+
+    async Task PublishIncomingTransfer(string participantId, Location from, Location to, CancellationToken cancellationToken)
+    {
+        // Only publish incoming transfer if (destination/target) contract exists
+        if (to.Contract is not null)
+        {
+            var incomingTransfer = ParticipantIncomingTransferQueueEntry.Create(
+                participantId,
+                from,
+                to,
+                from.Contract,
+                to.Contract,
+                DateTime.UtcNow);
 
             await unitOfWork.DbContext.ParticipantIncomingTransferQueue.AddAsync(incomingTransfer, cancellationToken);
         }
     }
+
+    async Task CloseStaleOutgoingTransfers(string participantId, Location from, Location to, CancellationToken cancellationToken)
+    {
+        // Find any outgoing transfers which have now become stale. These tranfers are either:
+        // Leaving a contract they have left in the past
+        // OR
+        // Returning to a contract they have left in the past
+        var staleOutgoingTransfers = await unitOfWork.DbContext.ParticipantOutgoingTransferQueue
+            .Where(q => q.IsReplaced == false)
+            .Where(q => q.ParticipantId == participantId)
+            .Where(q => q.FromContract == from.Contract || q.FromContract == to.Contract)
+            .ToListAsync(cancellationToken);
+
+        foreach (var transfer in staleOutgoingTransfers)
+        {
+            transfer.MarkAsReplaced();
+        }
+    }
+
 }
