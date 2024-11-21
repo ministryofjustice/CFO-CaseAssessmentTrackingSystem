76dd48b49ed3aab67a13781117c09ad5cacb0507
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-11-21 16:59:55 +0000

    Participant Ownership History - 'To' date EF migration

---
+++ src/Migrators/Migrators.MSSQL/Migrations/20241121161229_OwnershipHistory_ToDate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cfo.Cats.Migrators.MSSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class OwnershipHistory_ToDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "To",
+                schema: "Participant",
+                table: "OwnershipHistory",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "To",
+                schema: "Participant",
+                table: "OwnershipHistory");
+        }
+    }
+}
