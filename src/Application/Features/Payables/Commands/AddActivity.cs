4e69838f67cd5298f06e0d6f96e5bfabf9d4adf5
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-11-22 13:47:10 +0000

    CFODEV-865: Activity Dialog & placeholder command

---
+++ src/Application/Features/Payables/Commands/AddActivity.cs
@@ -0,0 +1,30 @@
+﻿
+using Cfo.Cats.Application.Features.Locations.DTOs;
+
+namespace Cfo.Cats.Application.Features.Payables.Commands;
+
+public static class AddActivity
+{
+    public class Command : IRequest<Result<bool>>
+    {
+        public LocationDto? Location { get; set; }
+    }
+
+    class Handler : IRequestHandler<Command, Result<bool>>
+    {
+        public async Task<Result<bool>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(true);
+        }
+    }
+
+    public class Validator : AbstractValidator<Command>
+    {
+        public Validator()
+        {
+            RuleFor(c => c.Location)
+                .NotNull()
+                .WithMessage("You must choose a location");
+        }
+    }
+}
