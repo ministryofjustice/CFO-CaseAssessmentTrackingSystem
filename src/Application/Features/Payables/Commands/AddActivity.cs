7d8b35e6da5fa2effaf1988e650d26cf3fc459ae
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-11-27 10:20:38 +0000

    Disallow selection of hub locations without inductions + small refactor

--- src/Application/Features/Payables/Commands/AddActivity.cs
+++ src/Application/Features/Payables/Commands/AddActivity.cs
@@ -10,6 +10,7 @@ public static class AddActivity
     [RequestAuthorize(Policy = SecurityPolicies.Enrol)]
     public class Command : IRequest<Result<bool>>
     {
+        public required string ParticipantId { get; set; }
         public LocationDto? Location { get; set; }
         public ActivityDto? Activity { get; set; }
     }
@@ -18,17 +19,32 @@ public static class AddActivity
     {
         public async Task<Result<bool>> Handle(Command request, CancellationToken cancellationToken)
         {
+            // TODO: record activity
             return await Task.FromResult(true);
         }
     }
 
     public class Validator : AbstractValidator<Command>
     {
-        public Validator()
+        readonly IUnitOfWork unitOfWork;
+
+        public Validator(IUnitOfWork unitOfWork)
         {
+            this.unitOfWork = unitOfWork;
+
+            RuleFor(c => c.ParticipantId)
+                .NotNull()
+                .Length(9);
+
             RuleFor(c => c.Location)
                 .NotNull()
                 .WithMessage("You must choose a location");
+
+            // Require an induction if the activity took place in a hub
+            RuleFor(c => c.Location)
+                .MustAsync(async (command, location, token) => await HaveAHubInduction(command.ParticipantId, location!, token))
+                .When(c => c.Location is not null && c.Location.LocationType.IsHub)
+                .WithMessage("A hub induction is required for the selected location");
 
             When(c => c.Location is not null, () =>
             {
@@ -36,6 +52,12 @@ public static class AddActivity
                     .NotNull()
                     .WithMessage("You must choose an Activity/ETE");
             });
+        }
+
+        async Task<bool> HaveAHubInduction(string participantId, LocationDto location, CancellationToken cancellationToken)
+        {
+            return await unitOfWork.DbContext.HubInductions.AnyAsync(induction => 
+                induction.ParticipantId == participantId && induction.LocationId == location.Id, cancellationToken);
         }
     }
 }
