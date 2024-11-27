a09617e5032d3981b24102387c9ba27fe83d3d76
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-11-27 11:35:36 +0000

    Rename for 'Human Citizenship' activity type

--- src/Domain/Common/Enums/ActivityType.cs
+++ src/Domain/Common/Enums/ActivityType.cs
@@ -9,7 +9,7 @@ public class ActivityType(
     public static readonly ActivityType CommunityAndSocial = new("Community and Social", 0);
     public static readonly ActivityType EducationAndTraining = new("Education and Training", 1);
     public static readonly ActivityType Employment = new("Employment", 2);
-    public static readonly ActivityType HumanAndCitizenship = new("Human and Citizenship", 3);
+    public static readonly ActivityType HumanCitizenship = new("Human Citizenship", 3);
     public static readonly ActivityType InterventionsAndServicesWraparoundSupport = new("Interventions And Services Wraparound Support", 4);
     public static readonly ActivityType SupportWork = new("Support Work", 5);
 }
