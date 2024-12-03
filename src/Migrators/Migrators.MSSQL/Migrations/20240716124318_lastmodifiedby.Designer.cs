0dc7abe8d8c64e3b6ae7740c47f71dc30b93bb41
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2024-12-03 09:30:57 +0000

    Remove warnings for migrations

--- src/Migrators/Migrators.MSSQL/Migrations/20240716124318_lastmodifiedby.Designer.cs
+++ src/Migrators/Migrators.MSSQL/Migrations/20240716124318_lastmodifiedby.Designer.cs
@@ -13,7 +13,9 @@ namespace Cfo.Cats.Migrators.MSSQL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
     [Migration("20240716124318_lastmodifiedby")]
+#pragma warning disable CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
     partial class lastmodifiedby
+#pragma warning restore CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
