c4e30e270fbc193aabf114ea0b69d0568298d6a4
VS <vinay.saini@justice.gov.uk>
2024-12-05 09:51:11 +0000

    added command and form for Education and training, need more work

---
+++ src/Application/Features/Payables/Commands/AddEducationTraining.cs
@@ -0,0 +1,78 @@
+﻿using Cfo.Cats.Application.Common.Security;
+using Cfo.Cats.Application.Common.Validators;
+using Cfo.Cats.Application.Features.Payables.DTOs;
+using Cfo.Cats.Application.SecurityConstants;
+using Humanizer.Bytes;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cfo.Cats.Application.Features.Payables.Commands;
+
+public static class AddEducationTraining
+{
+    [RequestAuthorize(Policy = SecurityPolicies.Enrol)]
+    public class Command : IRequest<Result<bool>>
+    {
+        [Description("Course Title")]
+        public string? CourseTitle { get; set; }
+
+        [Description("Course Hyperlink")]
+        public string? CourseHyperlink { get; set; }
+
+        [Description("Course Level")]
+        public string? CourseLevel { get; set; }
+
+        [Description("Course Commenced Date")]
+        public DateTime? CourseCommencedDate { get; set; }
+
+        [Description("Course Completed Date")]
+        public DateTime? CourseCompletedDate { get; set; }
+
+        [Description("Passed")]
+        public string? Passed { get; set; }
+
+        [Description("Additional Information")]
+        public string? AdditionalInformation { get; set; }
+    }
+
+    class Handler: IRequestHandler<Command, Result<bool>>
+    {
+        public async Task<Result<bool>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            // TODO: record activity
+            return await Task.FromResult(true);
+        }
+    }
+
+    public class Validator : AbstractValidator<Command>
+    {
+        readonly IUnitOfWork unitOfWork;
+        public Validator(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+
+            RuleFor(c => c.CourseTitle)
+                .NotNull()
+                .MaximumLength(100)
+                .WithMessage("You must enter a Course Title");
+            
+            RuleFor(c => c.CourseLevel)
+                .NotNull()
+                .WithMessage("You must choose a Course Level");
+
+            RuleFor(c => c.CourseCommencedDate)
+                .NotNull()
+                .WithMessage("You must enter Course Commenced Date"); 
+            
+            RuleFor(c => c.Passed)
+                .NotNull()
+                .WithMessage("You must choose a value for Passed");
+
+        }
+
+    }
+}
