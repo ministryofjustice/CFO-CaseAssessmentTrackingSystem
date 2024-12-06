0e55c4ab8e4e6df6647c2ebe3e05ab15520f8e8a
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-12-06 12:43:47 +0000

    Non-ISW, ISW, Employment, EducationTraining activity configurations

---
+++ src/Infrastructure/Persistence/Configurations/Payables/EmploymentActivityEntityTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Cfo.Cats.Domain.Entities.Payables;
+using Cfo.Cats.Infrastructure.Constants.Database;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Cfo.Cats.Infrastructure.Persistence.Configurations.Payables;
+
+public class EmploymentActivityEntityTypeConfiguration : IEntityTypeConfiguration<EmploymentActivity>
+{
+    public void Configure(EntityTypeBuilder<EmploymentActivity> builder)
+    {
+        builder.ToTable(
+            DatabaseConstants.Tables.EmploymentActivities,
+            DatabaseConstants.Schemas.Participant);
+
+        builder.HasOne(a => a.Document)
+            .WithMany()
+            .IsRequired(true);
+    }
+}
