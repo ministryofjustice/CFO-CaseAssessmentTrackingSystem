8851c029818fda2b8d46f9fa95edc8b23627b9ba
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-12-06 12:58:04 +0000

    Flatten NonISW configuration (relies on base type)

--- src/Infrastructure/Persistence/Configurations/Payables/NonISWActivityEntityTypeConfiguration.cs
+++ src/Infrastructure/Persistence/Configurations/Payables/NonISWActivityEntityTypeConfiguration.cs
@@ -14,55 +14,5 @@ public class NonISWActivityEntityTypeConfiguration : IEntityTypeConfiguration<No
         builder.ToTable(
             DatabaseConstants.Tables.NonISWActivities, 
             DatabaseConstants.Schemas.Participant);
-
-        builder.HasKey(a => a.Id);
-
-        builder.HasOne<Participant>()
-            .WithMany()
-            .HasForeignKey(a => a.ParticipantId);
-
-        builder.Property(a => a.Definition)
-            .IsRequired()
-            .HasConversion(
-                d => d!.Value,
-                d => ActivityDefinition.FromValue(d)
-            );
-
-        builder.HasOne(a => a.TookPlaceAtLocation)
-            .WithMany()
-            .OnDelete(DeleteBehavior.Restrict);
-
-        builder.HasOne(a => a.TookPlaceAtContract)
-            .WithMany()
-            .OnDelete(DeleteBehavior.Restrict);
-
-        builder.HasOne(a => a.ParticipantCurrentLocation)
-            .WithMany()
-            .OnDelete(DeleteBehavior.Restrict);
-
-        builder.HasOne(a => a.ParticipantCurrentContract)
-            .WithMany()
-            .OnDelete(DeleteBehavior.Restrict);
-
-        builder.Property(a => a.ParticipantStatus)
-            .IsRequired()
-            .HasConversion(
-                ps => ps!.Value,
-                ps => EnrolmentStatus.FromValue(ps)
-            );
-
-        builder.Property(a => a.AdditionalInformation)
-            .HasMaxLength(1000);
-
-        builder.HasOne<Tenant>()
-            .WithMany()
-            .HasForeignKey(a => a.TenantId);
-
-        builder.Property(a => a.Status)
-            .IsRequired()
-            .HasConversion(
-                s => s!.Value,
-                s => ActivityStatus.FromValue(s)
-            );
     }
 }
