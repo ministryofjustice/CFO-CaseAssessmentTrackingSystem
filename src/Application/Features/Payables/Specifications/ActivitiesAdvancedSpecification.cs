7b8e3956f56f1f4515e8188ad8f296ba0c9ea6d6
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-12-11 12:02:31 +0000

    Activity completed range filtering

--- src/Application/Features/Payables/Specifications/ActivitiesAdvancedSpecification.cs
+++ src/Application/Features/Payables/Specifications/ActivitiesAdvancedSpecification.cs
@@ -10,6 +10,8 @@ public class ActivitiesAdvancedSpecification : Specification<Activity>
              .Where(a => a.TaskId == filter.TaskId, filter.TaskId is not null)
              .Where(a => a.ObjectiveId == filter.ObjectiveId, filter.ObjectiveId is not null)
              .Where(a => a.TookPlaceAtLocation.Id == filter.Location!.Id, filter.Location is not null)
-             .Where(a => filter.IncludeTypes!.Contains(a.Type), filter.IncludeTypes is { Count: > 0 });
+             .Where(a => filter.IncludeTypes!.Contains(a.Type), filter.IncludeTypes is { Count: > 0 })
+             .Where(a => a.Completed >= filter.CompletedStart, filter.CompletedStart is not null)
+             .Where(a => a.Completed <= filter.CompletedEnd, filter.CompletedEnd is not null);
     }
 }
