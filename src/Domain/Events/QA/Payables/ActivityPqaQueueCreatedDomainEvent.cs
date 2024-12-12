1f3268e5dbf7fac3a92c3123f8e1b491f7452036
PaulCooperWorkJustice <paul.cooper5@justice.gov.uk>
2024-12-12 11:11:44 +0000

    Added Queries and Event Handlers for PQA queue

--- src/Domain/Events/QA/Payables/ActivityPqaQueueCreatedDomainEvent.cs
+++ src/Domain/Events/QA/Payables/ActivityPqaQueueCreatedDomainEvent.cs
@@ -1,10 +1,5 @@
 ﻿using Cfo.Cats.Domain.Common.Events;
 using Cfo.Cats.Domain.Entities.Payables;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Cfo.Cats.Domain.Events.QA.Payables
 {
