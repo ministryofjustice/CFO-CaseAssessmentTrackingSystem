1eb3aacc09d76bd674f8ecd23208b968d5579519
PaulCooperWorkJustice <paul.cooper5@justice.gov.uk>
2024-12-12 11:11:44 +0000

    Added Queries and Event Handlers for PQA queue

---
+++ src/Application/Features/Notifications/EventHandlers/NotifyPqaActivityRejectedEventHandler.cs
@@ -0,0 +1,40 @@
+﻿using Cfo.Cats.Domain.Entities.Notifications;
+using Cfo.Cats.Domain.Events;
+
+namespace Cfo.Cats.Application.Features.Notifications.EventHandlers
+{    
+    public class NotifyPqaActivityRejectedEventHandler(IUnitOfWork unitOfWork) : INotificationHandler<ActivityTransitionedDomainEvent>
+    {
+        public async Task Handle(ActivityTransitionedDomainEvent notification, CancellationToken cancellationToken)
+        {
+            if (notification.From == ActivityStatus.SubmittedToAuthorityStatus && notification.To == ActivityStatus.SubmittedToProviderStatus)
+            {
+                const string heading = "Activity returned from authority";
+                string details = "You have Activities that have been returned";
+
+                // who did the PQA?
+                var qaUser = await unitOfWork.DbContext
+                    .ActivityPqaQueue.AsNoTracking()
+                    .Where(pqa =>
+                        pqa.ActivityId == notification.Item.Id && pqa.IsAccepted == true && pqa.IsCompleted == true)
+                    .Select(pqa => pqa.LastModifiedBy!)
+                    .FirstOrDefaultAsync(cancellationToken);
+
+                Notification? previous = unitOfWork.DbContext.Notifications.FirstOrDefault(
+                    n => n.Heading == heading
+                    && n.OwnerId == qaUser
+                    && n.ReadDate == null
+                );
+
+                previous?.ResetNotificationDate();
+
+                if (previous == null)
+                {
+                    var n = Notification.Create(heading, details, qaUser!);
+                    n.SetLink($"pages/qa/activities/pqa/");
+                    await unitOfWork.DbContext.Notifications.AddAsync(n, cancellationToken);
+                }
+            }
+        }
+    }
+}
