1bb108ccf2bd52655f7a2b22fbde2f491a92279c
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-12-13 09:00:08 +0000

    Activity status transition safe-guarding

---
+++ src/Domain/Common/Exceptions/InvalidActivityTransition.cs
@@ -0,0 +1,6 @@
+﻿using Cfo.Cats.Domain.Common.Enums;
+
+namespace Cfo.Cats.Domain.Common.Exceptions;
+
+public class InvalidActivityTransition(ActivityStatus from, ActivityStatus to)
+    : Exception($"Activities cannot transition from {from.Name} to {to.Name}");
