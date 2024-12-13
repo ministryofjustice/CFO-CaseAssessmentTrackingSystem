d34ffefd85d1c65a0531c070a3d8f2c05d702fd0
PaulCooperWorkJustice <paul.cooper5@justice.gov.uk>
2024-12-13 14:52:15 +0000

    Added creation of QA1 queues

---
+++ src/Application/Features/Payables/EventHandlers/SubmittedToQa/CreateActivityQa1QueueEntry.cs
@@ -0,0 +1,18 @@
+﻿using Cfo.Cats.Domain.Entities.Payables;
+using Cfo.Cats.Domain.Events;
+
+namespace Cfo.Cats.Application.Features.Payables.EventHandlers.SubmittedToQa
+{
+    public class CreateActivityQa1QueueEntry(IUnitOfWork unitOfWork) : INotificationHandler<ActivityTransitionedDomainEvent>
+    {
+        public async Task Handle(ActivityTransitionedDomainEvent notification, CancellationToken cancellationToken)
+        {
+            if (notification.To == ActivityStatus.SubmittedToAuthorityStatus)
+            {
+                var queueEntry = ActivityQa1QueueEntry.Create(notification.Item.Id);
+                queueEntry.TenantId = notification.Item.Owner!.TenantId!;
+                await unitOfWork.DbContext.ActivityQa1Queue.AddAsync(queueEntry, cancellationToken);
+            }
+        }
+    }
+}
