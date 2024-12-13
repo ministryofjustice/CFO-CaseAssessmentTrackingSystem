235ac7b806c4496329a3691ea34ec091c7d3b28d
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-12-13 10:56:34 +0000

    User friendly course status + vertical spacing fixes

--- src/Domain/Common/Enums/CourseCompletionStatus.cs
+++ src/Domain/Common/Enums/CourseCompletionStatus.cs
@@ -4,7 +4,7 @@ namespace Cfo.Cats.Domain.Common.Enums;
 
 public class CourseCompletionStatus(string name, int value) : SmartEnum<CourseCompletionStatus>(name, value)
 {
-    public static readonly CourseCompletionStatus NotApplicable = new(nameof(NotApplicable), -1);
+    public static readonly CourseCompletionStatus NotApplicable = new("Not Applicable", -1);
     public static readonly CourseCompletionStatus No = new(nameof(No), 0);
     public static readonly CourseCompletionStatus Yes = new(nameof(Yes), 1);
 }
