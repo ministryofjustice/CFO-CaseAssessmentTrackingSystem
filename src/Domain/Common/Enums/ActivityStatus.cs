75822fce72d967604f76a61b25df27e052004197
samgibsonmoj <sam.gibson@justice.gov.uk>
2024-12-13 11:54:30 +0000

    Rename 'Creating' to 'Pending' status

--- src/Domain/Common/Enums/ActivityStatus.cs
+++ src/Domain/Common/Enums/ActivityStatus.cs
@@ -6,16 +6,19 @@ namespace Cfo.Cats.Domain.Common.Enums;
 
 public abstract class ActivityStatus : SmartEnum<ActivityStatus>
 {
-    public static readonly ActivityStatus CreatingStatus = new Creating();
+    public static readonly ActivityStatus PendingStatus = new Pending();
     public static readonly ActivityStatus SubmittedToProviderStatus = new SubmittedToProvider();
     public static readonly ActivityStatus SubmittedToAuthorityStatus = new SubmittedToAuthority();
     public static readonly ActivityStatus ApprovedStatus = new Approved();
     public static readonly ActivityStatus AbandonedStatus = new Abandoned();
 
-    private ActivityStatus(string name, int value)
+    private ActivityStatus(string name, int value, bool allowsChanges = false)
     : base(name, value)
     {
+        AllowsChanges = allowsChanges;
     }
+
+    public bool AllowsChanges { get; }
 
     /// <summary>
     ///     Statuses that we can transition to.
@@ -23,20 +26,20 @@ public abstract class ActivityStatus : SmartEnum<ActivityStatus>
     protected abstract ActivityStatus[] GetAllowedTransitions();
 
     public bool AllowSubmitToPqa() =>
-    this == CreatingStatus;
+        this == PendingStatus;
 
     public bool CanTransitionTo(ActivityStatus next) =>
         GetAllowedTransitions()
             .Any(e => next == e);
 
-    private sealed class Creating() : ActivityStatus("Creating", 0)
+    private sealed class Pending() : ActivityStatus("Pending", 0, allowsChanges: true)
     {
         protected override ActivityStatus[] GetAllowedTransitions() => [AbandonedStatus, SubmittedToProviderStatus, ApprovedStatus];
     }
 
     private sealed class SubmittedToProvider() : ActivityStatus("Submitted to Provider", 1)
     {
-        protected override ActivityStatus[] GetAllowedTransitions() => [AbandonedStatus, CreatingStatus, SubmittedToAuthorityStatus];
+        protected override ActivityStatus[] GetAllowedTransitions() => [AbandonedStatus, PendingStatus, SubmittedToAuthorityStatus];
     }
 
     private sealed class SubmittedToAuthority() : ActivityStatus("Submitted to Authority", 2)
