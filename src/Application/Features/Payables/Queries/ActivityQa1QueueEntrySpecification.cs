4f1adb8fc317e08e3b841e5a3f036b9127cd7c42
PaulCooperWorkJustice <paul.cooper5@justice.gov.uk>
2024-12-13 14:52:15 +0000

    Added creation of QA1 queues

---
+++ src/Application/Features/Payables/Queries/ActivityQa1QueueEntrySpecification.cs
@@ -0,0 +1,16 @@
+﻿using Cfo.Cats.Application.Features.QualityAssurance.Queries;
+using Cfo.Cats.Domain.Entities.Payables;
+
+namespace Cfo.Cats.Application.Features.Payables.Queries
+{
+    public class ActivityQa1QueueEntrySpecification : Specification<ActivityQa1QueueEntry>
+    {
+        public ActivityQa1QueueEntrySpecification(QueueEntryFilter filter)
+        {
+            Query.Where(e => e.TenantId
+                    .StartsWith(filter.CurrentUser!.TenantId!))
+                .Where(e => e.IsCompleted == false);
+        }
+    }
+
+}
---
+++ src/Application/Features/Payables/Queries/ActivityQa1WithPagination.cs
@@ -0,0 +1,99 @@
+﻿using Cfo.Cats.Application.Common.Security;
+using Cfo.Cats.Application.Common.Validators;
+using Cfo.Cats.Application.Features.Payables.DTOs;
+using Cfo.Cats.Application.Features.QualityAssurance.Queries;
+using Cfo.Cats.Application.SecurityConstants;
+using Cfo.Cats.Domain.Entities.Payables;
+
+namespace Cfo.Cats.Application.Features.Payables.Queries
+{    
+    public static class ActivityQa1WithPagination
+    {
+        [RequestAuthorize(Roles = $"{RoleNames.QAOfficer}, {RoleNames.QAManager}, {RoleNames.QASupportManager}, {RoleNames.SMT}, {RoleNames.SystemSupport}")]
+        public class Query : QueueEntryFilter, IRequest<PaginatedData<ActivityQueueEntryDto>>
+        {
+            public Query()
+            {
+                SortDirection = "Desc";
+                OrderBy = "Created";
+            }
+            public ActivityQa1QueueEntrySpecification Specification => new(this);
+        }
+
+        public class Handler(IUnitOfWork unitOfWork, IMapper mapper) : IRequestHandler<Query, PaginatedData<ActivityQueueEntryDto>>
+        {
+            public async Task<PaginatedData<ActivityQueueEntryDto>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var query = unitOfWork.DbContext
+                    .ActivityQa1Queue
+                    .AsNoTracking();
+
+                var sortExpression = GetSortExpression(request);
+
+                var ordered = request.SortDirection.Equals("Descending", StringComparison.CurrentCultureIgnoreCase)
+                    ? query.OrderByDescending(sortExpression)
+                    : query.OrderBy(sortExpression);
+
+                var data = await ordered
+                    .ProjectToPaginatedDataAsync<ActivityQa1QueueEntry, ActivityQueueEntryDto>(request.Specification, request.PageNumber, request.PageSize, mapper.ConfigurationProvider, cancellationToken);
+
+                return data;
+            }
+
+            private Expression<Func<ActivityQa1QueueEntry, object?>> GetSortExpression(Query request)
+            {
+                Expression<Func<ActivityQa1QueueEntry, object?>> sortExpression;
+                switch (request.OrderBy)
+                {
+                    case "ParticipantId":
+                        sortExpression = (x => x.Participant!.FirstName + ' ' + x.Participant.LastName);
+                        break;
+                    case "TenantId":
+                        sortExpression = (x => x.TenantId);
+                        break;
+                    case "Created":
+                        sortExpression = (x => x.Created!);
+                        break;
+                    case "SupportWorker":
+                        sortExpression = (x => x.Participant!.Owner!.DisplayName!);
+                        break;
+                    case "AssignedTo":
+                        sortExpression = (x => x.OwnerId == null ? null : x.Owner!.DisplayName);
+                        break;
+                    default:
+                        sortExpression = (x => x.Created!);
+                        break;
+                }
+
+                return sortExpression;
+            }
+        }
+        public class Validator : AbstractValidator<Query>
+        {
+            public Validator()
+            {
+                RuleFor(r => r.Keyword)
+                    .Matches(ValidationConstants.Keyword)
+                    .WithMessage(string.Format(ValidationConstants.KeywordMessage, "Search Keyword"));
+
+                RuleFor(r => r.PageNumber)
+                    .GreaterThan(0)
+                    .WithMessage(string.Format(ValidationConstants.PositiveNumberMessage, "Page Number"));
+
+                RuleFor(r => r.PageSize)
+                    .GreaterThan(0)
+                    .LessThanOrEqualTo(1000)
+                    .WithMessage(string.Format(ValidationConstants.MaximumPageSizeMessage, "Page Size"));
+
+                RuleFor(r => r.SortDirection)
+                    .Matches(ValidationConstants.SortDirection)
+                    .WithMessage(ValidationConstants.SortDirectionMessage);
+
+                //May be at some point in future validate against columns of query result dataset
+                RuleFor(r => r.OrderBy)
+                    .Matches(ValidationConstants.AlphaNumeric)
+                    .WithMessage(string.Format(ValidationConstants.AlphaNumericMessage, "OrderBy"));
+            }
+        }
+    }
+}
