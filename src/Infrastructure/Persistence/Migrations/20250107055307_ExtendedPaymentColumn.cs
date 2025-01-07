a078fe5e92cfba151e2e8cba5ffd4c3f92551731
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2025-01-07 07:22:35 +0000

    hotfix: Increase length of column

---
+++ src/Infrastructure/Persistence/Migrations/20250107055307_ExtendedPaymentColumn.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cfo.Cats.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExtendedPaymentColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ActivityCategory",
+                schema: "Mi",
+                table: "ActivityPayment",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ActivityCategory",
+                schema: "Mi",
+                table: "ActivityPayment",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
