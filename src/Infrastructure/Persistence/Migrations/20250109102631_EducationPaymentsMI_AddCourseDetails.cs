1ae8aa335833bade9fe9e0c9d62f9b2a20024b53
PaulCooperWorkJustice <paul.cooper5@justice.gov.uk>
2025-01-09 12:30:58 +0000

    Add course title and level to MI Education payment table

---
+++ src/Infrastructure/Persistence/Migrations/20250109102631_EducationPaymentsMI_AddCourseDetails.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cfo.Cats.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class EducationPaymentsMI_AddCourseDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CourseLevel",
+                schema: "Mi",
+                table: "EducationPayment",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CourseTitle",
+                schema: "Mi",
+                table: "EducationPayment",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CourseLevel",
+                schema: "Mi",
+                table: "EducationPayment");
+
+            migrationBuilder.DropColumn(
+                name: "CourseTitle",
+                schema: "Mi",
+                table: "EducationPayment");
+        }
+    }
+}
