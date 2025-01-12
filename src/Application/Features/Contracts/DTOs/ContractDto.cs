3ba566e02f04cc615e09d8e05e3be3f8e86d7043
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2025-01-12 15:41:33 +0000

    Display graphs + paid
    This adds graphs and a summary of paid achievements by contract.

---
+++ src/Application/Features/Contracts/DTOs/ContractDto.cs
@@ -0,0 +1,20 @@
+﻿using Cfo.Cats.Domain.Entities.Administration;
+
+namespace Cfo.Cats.Application.Features.Contracts.DTOs
+{
+    public class ContractDto
+    {
+        public required string Id { get; set; }
+        public required string Name { get; set; }
+        public required string TenantId { get; set; }
+
+        private class Mapper : Profile
+        {
+            public Mapper()
+            {
+                CreateMap<Contract, ContractDto>()
+                    .ForMember(target => target.Name, options => options.MapFrom(source => source.Description));
+            }
+        }
+    }
+}
