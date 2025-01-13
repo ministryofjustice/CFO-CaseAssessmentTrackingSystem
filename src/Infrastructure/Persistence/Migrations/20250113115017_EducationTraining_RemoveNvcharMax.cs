f0ec0c4b376a22ed2c4d32a50d6277a14782b4e2
PaulCooperWorkJustice <paul.cooper5@justice.gov.uk>
2025-01-13 11:56:40 +0000

    Change Education activity  so doesnt use nvarchar max columns

---
+++ src/Infrastructure/Persistence/Migrations/20250113115017_EducationTraining_RemoveNvcharMax.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cfo.Cats.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class EducationTraining_RemoveNvcharMax : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CourseUrl",
+                schema: "Activities",
+                table: "EducationTrainingActivity",
+                type: "nvarchar(2000)",
+                maxLength: 2000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CourseTitle",
+                schema: "Activities",
+                table: "EducationTrainingActivity",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CourseLevel",
+                schema: "Activities",
+                table: "EducationTrainingActivity",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CourseUrl",
+                schema: "Activities",
+                table: "EducationTrainingActivity",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2000)",
+                oldMaxLength: 2000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CourseTitle",
+                schema: "Activities",
+                table: "EducationTrainingActivity",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CourseLevel",
+                schema: "Activities",
+                table: "EducationTrainingActivity",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
