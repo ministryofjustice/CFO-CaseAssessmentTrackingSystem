9e3097016ba30e9794946cc8bb3a812015ee7f84
PaulCooperWorkJustice <paul.cooper5@justice.gov.uk>
2025-01-13 12:11:42 +0000

    Alter columns on mi education table to match activity table

--- src/Infrastructure/Persistence/Migrations/20250113120823_EducationTrainingMI_RemoveNvcharMax.Designer.cs
+++ src/Infrastructure/Persistence/Migrations/20250113120823_EducationTrainingMI_RemoveNvcharMax.Designer.cs
@@ -4,6 +4,7 @@ using Cfo.Cats.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cfo.Cats.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250113120823_EducationTrainingMI_RemoveNvcharMax")]
+    partial class EducationTrainingMI_RemoveNvcharMax
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -2670,17 +2673,14 @@ namespace Cfo.Cats.Infrastructure.Persistence.Migrations
 
                     b.Property<string>("CourseLevel")
                         .IsRequired()
-                        .HasMaxLength(100)
-                        .HasColumnType("nvarchar(100)");
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("CourseTitle")
                         .IsRequired()
-                        .HasMaxLength(200)
-                        .HasColumnType("nvarchar(200)");
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("CourseUrl")
-                        .HasMaxLength(2000)
-                        .HasColumnType("nvarchar(2000)");
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<Guid>("DocumentId")
                         .HasColumnType("uniqueidentifier");
