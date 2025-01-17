816a088da3598d4253f1c05dab37e77a6974e38b
samgibsonmoj <sam.gibson@justice.gov.uk>
2025-01-17 11:01:54 +0000

    Automatically create pathway plan when a participant is created

---
+++ src/Application/Features/Participants/EventHandlers/AddPathwayPlanOnParticipantCreated.cs
@@ -0,0 +1,13 @@
+﻿using Cfo.Cats.Domain.Entities.Participants;
+using Cfo.Cats.Domain.Events;
+
+namespace Cfo.Cats.Application.Features.Participants.EventHandlers;
+
+public class AddPathwayPlanOnParticipantCreated(IUnitOfWork unitOfWork) : INotificationHandler<ParticipantCreatedDomainEvent>
+{
+    public async Task Handle(ParticipantCreatedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        var pathwayPlan = PathwayPlan.Create(notification.Item.Id);
+        await unitOfWork.DbContext.PathwayPlans.AddAsync(pathwayPlan, cancellationToken);
+    }
+}
