c44504260aabeaa60a94a38cf1bc9003b66e26fd
VS <vinay.saini@justice.gov.uk>
2025-01-23 11:07:20 +0000

    added query to get Community SW based on participant and the code

--- src/Application/Features/Participants/DTOs/PriCodeDto.cs
+++ src/Application/Features/Participants/DTOs/PriCodeDto.cs
@@ -1,6 +1,9 @@
 ﻿
 using Cfo.Cats.Application.Common.Validators;
+using Cfo.Cats.Application.Features.Identity.DTOs;
 using Cfo.Cats.Domain.Entities.Participants;
+using Cfo.Cats.Domain.Identity;
+using Cfo.Cats.Domain.ValueObjects;
 
 namespace Cfo.Cats.Application.Features.Participants.DTOs;
 
@@ -11,5 +14,17 @@ public class PriCodeDto
 
     [Description("PRI Code")]
     public int? Code { get; set; }
+
+    [Description("Community Support Worker")]
+    public string? CommunitySupportWorker { get;}
+
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<PriCode, PriCodeDto>(MemberList.None)
+                .ForMember(x => x.CommunitySupportWorker, s => s.MapFrom(y => y.CreatedBy));
+        }
+    }
 
 }
