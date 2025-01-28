08dda9de7b00d94121768511bc09a22e2834126c
VS <vinay.saini@justice.gov.uk>
2025-01-28 07:54:18 +0000

    added functionality to notify Community support worker when a PRI is asssigned to them, no notification in case of self assignment.

---
+++ src/Application/Features/Notifications/EventHandlers/NotifyCommunitySupportWorkerPriAssignedEventHandler.cs
@@ -0,0 +1,35 @@
+﻿using Cfo.Cats.Domain.Entities.Notifications;
+using Cfo.Cats.Domain.Entities.PRIs;
+using Cfo.Cats.Domain.Events;
+using DocumentFormat.OpenXml.Bibliography;
+
+namespace Cfo.Cats.Application.Features.Notifications.EventHandlers;
+
+public class NotifyCommunitySupportWorkerPriAssignedEventHandler(IUnitOfWork unitOfWork) : INotificationHandler<PRIAssignedDomainEvent>
+{
+    public async Task Handle(PRIAssignedDomainEvent pri, CancellationToken cancellationToken)
+    {
+        if (pri.Entity.AssignedTo!.Length > 0 && pri.Entity.CreatedBy != pri.Entity.AssignedTo)
+        {
+            const string heading = "PRI assigned";
+
+            string details = "You have been assigned a PRI";
+
+            Notification? previous = unitOfWork.DbContext.Notifications.FirstOrDefault(
+                n => n.Heading == heading
+                && n.OwnerId == pri.Entity.AssignedTo
+                && n.ReadDate == null
+            );
+
+            previous?.ResetNotificationDate();
+
+            if (previous is null)
+            {
+                var n = Notification.Create(heading, details, pri.Entity.AssignedTo);
+                //n.SetLink($"/pages/participants/{pri.Entity.ParticipantId}");
+                n.SetLink($"pages/participants/pre-release-inventory");
+                await unitOfWork.DbContext.Notifications.AddAsync(n, cancellationToken);
+            }
+        }
+    }
+}
