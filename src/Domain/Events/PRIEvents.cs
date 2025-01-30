859342221d5b53974f398ecb83f5b6ee01d7de64
PaulCooperWorkJustice <paul.cooper5@justice.gov.uk>
2025-01-30 14:57:31 +0000

    PRI Changes to include new Abandon fields and have a reason

--- src/Domain/Events/PRIEvents.cs
+++ src/Domain/Events/PRIEvents.cs
@@ -4,8 +4,19 @@ using Cfo.Cats.Domain.Entities.PRIs;
 namespace Cfo.Cats.Domain.Events;
 
 public sealed class PRICreatedDomainEvent(PRI pri) : CreatedDomainEvent<PRI>(pri);
+
 public sealed class PRIAssignedDomainEvent(PRI pri, string? to) : DomainEvent
 {
     public PRI Item { get; set; } = pri;
     public string? To { get; set; } = to;
+}
+
+public sealed class PRICompletedDomainEvent(PRI pri) : DomainEvent
+{
+    public PRI Item { get; set; } = pri;    
+}
+
+public sealed class PRIAbandonedDomainEvent(PRI pri) : DomainEvent
+{
+    public PRI Item { get; set; } = pri;
 }
