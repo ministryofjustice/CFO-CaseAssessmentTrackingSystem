c3b8ca5fa075a011a2147ceb55b40a2d3de64e57
PaulCooperWorkJustice <paul.cooper5@justice.gov.uk>
2025-01-30 14:57:31 +0000

    PRI Changes to include new Abandon fields and have a reason

--- src/Application/Features/PRIs/Commands/AbandonPRI.cs
+++ src/Application/Features/PRIs/Commands/AbandonPRI.cs
@@ -14,7 +14,10 @@ public static class AbandonPRI
         public required string ParticipantId { get; set; }
 
         [Description("Reason Abandoned")]
-        public string? ReasonAbandoned { get; set; }
+        public PriAbandonReason AbandonReason { get; set; } = PriAbandonReason.Other;
+
+        [Description("Abandoned Justification")]
+        public string? AbandonJustification { get; set; }
 
         [Description("Abandoned By")]
         public string? AbandonedBy { get; set; }
@@ -28,13 +31,12 @@ public static class AbandonPRI
                 .SingleOrDefaultAsync(p => p.ParticipantId == request.ParticipantId
                 && p.Status != PriStatus.Abandoned, cancellationToken);
 
-
             if (pri == null)
             {
                 throw new NotFoundException("Cannot find PRI", request.ParticipantId);
-            }            
+            }
 
-            pri.Abandon(request.ReasonAbandoned,request.AbandonedBy);
+            pri.Abandon(request.AbandonReason, request.AbandonJustification, request.AbandonedBy);
 
             return Result.Success();
         }
@@ -53,9 +55,12 @@ public static class AbandonPRI
                     .NotNull()
                     .MinimumLength(36);
 
-            RuleFor(c => c.ReasonAbandoned)
-             .NotEmpty()             
-             .WithMessage("You must provide a reason");
+            RuleFor(c => c.AbandonJustification)
+                .NotEmpty()
+                .When(c => c.AbandonReason.RequiresJustification)
+                .WithMessage("You must provide a justification for the selected Abandoned reason")
+                .Matches(ValidationConstants.Notes)
+                .WithMessage(string.Format(ValidationConstants.NotesMessage, "Justification"));
         }
     }
 }
