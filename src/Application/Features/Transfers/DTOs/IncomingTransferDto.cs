01cfeed8e9db122818fbe14180e0974e26d7e456
samgibsonmoj <sam.gibson@justice.gov.uk>
2025-02-05 14:49:47 +0000

    Display contracts on transfers screen

--- src/Application/Features/Transfers/DTOs/IncomingTransferDto.cs
+++ src/Application/Features/Transfers/DTOs/IncomingTransferDto.cs
@@ -13,8 +13,8 @@ public class IncomingTransferDto
     public required string ParticipantId { get; set; }
 
     //public required ParticipantDto Participant { get; set; }
-    public string? FromContractId { get; set; }
-    public string? ToContractId { get; set; }
+    public string? FromContract { get; set; }
+    public string? ToContract { get; set; }
     public required LocationDto FromLocation { get; set; }
     public required LocationDto ToLocation { get; set; }
     public required DateTime MoveOccured { get; set; }
@@ -27,7 +27,9 @@ public class IncomingTransferDto
         {
             CreateMap<ParticipantIncomingTransferQueueEntry, IncomingTransferDto>()
                 .ForMember(p => p.ParticipantId, options => options.MapFrom(src => src.ParticipantId))
-                .ForMember(p => p.ParticipantFullName, options => options.MapFrom(src => src.Participant!.FirstName + " " + src.Participant!.LastName));
+                .ForMember(p => p.ParticipantFullName, options => options.MapFrom(src => src.Participant!.FirstName + " " + src.Participant!.LastName))
+                .ForMember(p => p.FromContract, options => options.MapFrom(src => src.FromContract == null ? null : src.FromContract.Description))
+                .ForMember(p => p.ToContract, options => options.MapFrom(src => src.ToContract == null ? null : src.ToContract.Description));
         }
     }
 }
