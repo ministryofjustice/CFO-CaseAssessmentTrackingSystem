a4fab30b6d9e19c62c66c1f0c7d5f1657e60b265
samgibsonmoj <sam.gibson@justice.gov.uk>
2025-02-12 14:10:22 +0000

    Refactoring for enrolment approval
    Fixes an issue where RTW checks are not carried out when approved in Escalation queue

---
+++ src/Application/Features/QualityAssurance/EventHandlers/VerifyRightToWorkOnApproval.cs
@@ -0,0 +1,32 @@
+﻿using Cfo.Cats.Domain.Events;
+
+namespace Cfo.Cats.Application.Features.QualityAssurance.EventHandlers;
+
+public class VerifyRightToWorkOnApproval(IUnitOfWork unitOfWork, IRightToWorkSettings rtwSettings) : INotificationHandler<ParticipantEnrolmentApprovedDomainEvent>
+{
+    public async Task Handle(ParticipantEnrolmentApprovedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        bool hasActiveRightToWork = false;
+
+        bool isExempt = rtwSettings.NationalitiesExempted.Any(s => s.Equals(notification.Item!.Nationality!, StringComparison.OrdinalIgnoreCase));
+
+        if (isExempt is false)
+        {
+            hasActiveRightToWork = await unitOfWork.DbContext.Participants
+                .Where(x => x.Id == notification.Item!.Id)
+                .SelectMany(p => p.RightToWorks)
+                .AnyAsync(x => x.Lifetime.EndDate >= DateTime.Now.Date, cancellationToken);
+        }
+
+        bool isRightToWorkRequired = isExempt is false && hasActiveRightToWork is false;
+
+        if (isRightToWorkRequired)
+        {
+            notification.Item.TransitionTo(EnrolmentStatus.SubmittedToProviderStatus);
+        }
+        else
+        {
+            notification.Item.TransitionTo(EnrolmentStatus.ApprovedStatus);
+        }
+    }
+}
