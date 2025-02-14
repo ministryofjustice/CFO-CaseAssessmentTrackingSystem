a5ff538230741441a1e4ab1e79c20877797849bf
samgibsonmoj <sam.gibson@justice.gov.uk>
2025-02-14 15:03:20 +0000

    Refactor and unify handlers

--- src/Application/Features/PRIs/EventHandlers/PreventCompletionOfTaskWithoutRelease.cs
+++ src/Application/Features/PRIs/EventHandlers/PreventCompletionOfTaskWithoutRelease.cs
@@ -10,8 +10,18 @@ internal class PreventCompletionOfTaskWithoutRelease(IUnitOfWork unitOfWork) : I
         {
             var pri = await unitOfWork.DbContext.PRIs
                 .Where(x => x.ObjectiveId == notification.Item.ObjectiveId)
-                .Select(x => new { x.ExpectedReleaseRegionId, x.Created, x.ParticipantId } )
-                .FirstAsync(cancellationToken);
+                .Select(x => new { x.ExpectedReleaseRegionId, x.Created, x.ParticipantId, x.ActualReleaseDate } )
+                .FirstOrDefaultAsync(cancellationToken);
+
+            if(pri is null)
+            {
+                return;
+            }
+
+            if (pri.ActualReleaseDate is null)
+            {
+                throw new ApplicationException("Cannot accept this task as the actual release date is missing from the PRI");
+            }
 
             // Has the participant been in the expected release region after the PRI was created?
             var hasBeenReleasedToLocation = await unitOfWork.DbContext.ParticipantLocationHistories
@@ -22,7 +32,7 @@ internal class PreventCompletionOfTaskWithoutRelease(IUnitOfWork unitOfWork) : I
 
             if (hasBeenReleasedToLocation is false)
             {
-                throw new ApplicationException("Cannot accept this task as notice of the participants' release to the expected release region has not yet been received.");
+                throw new ApplicationException("Cannot accept this task as notice of the participants release to the expected release region has not yet been received.");
             }
         }
     }
