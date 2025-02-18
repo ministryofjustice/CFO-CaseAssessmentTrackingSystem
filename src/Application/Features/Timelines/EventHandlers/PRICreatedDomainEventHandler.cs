f42603fc376ca956aeb472d4fd8661d8df07d2fa
Paul Cooper <62419931+PaulCooperWorkJustice@users.noreply.github.com>
2025-02-18 12:04:10 +0000

    CFODEV-1070: Show PRI creation on timeline (#475)

---
+++ src/Application/Features/Timelines/EventHandlers/PRICreatedDomainEventHandler.cs
@@ -0,0 +1,11 @@
+﻿using Cfo.Cats.Domain.Events;
+
+namespace Cfo.Cats.Application.Features.Timelines.EventHandlers;
+
+public class PRICreatedDomainEventHandler(ICurrentUserService currentUserService, IUnitOfWork unitOfWork) : TimelineNotificationHandler<PRICreatedDomainEvent>(currentUserService, unitOfWork)
+{    
+    protected override string GetLine1(PRICreatedDomainEvent notification) => "PRI created.";
+    protected override TimelineEventType GetEventType() => TimelineEventType.PRI;
+    protected override string GetParticipantId(PRICreatedDomainEvent notification)
+        => notification.Entity.ParticipantId;
+}
