9068b179e55d5a2961eb49344a2a82b91f87f94f
Sam Gibson <140488216+samgibsonmoj@users.noreply.github.com>
2025-02-20 15:48:38 +0000

    (hotfix) CFODEV-1107: Re-issue TTG payment on approval of enrolment (#489)
    * CFODEV-1107: Re-issue TTG payment on approval of enrolment
    * Check for completed mandatory tasks

--- src/Application/Features/ManagementInformation/IntegrationEventHandlers/RaisePaymentsAfterApprovalConsumer.cs
+++ src/Application/Features/ManagementInformation/IntegrationEventHandlers/RaisePaymentsAfterApprovalConsumer.cs
@@ -2,6 +2,7 @@
 using Cfo.Cats.Application.Features.Inductions.IntegrationEvents;
 using Cfo.Cats.Application.Features.Participants.IntegrationEvents;
 using Cfo.Cats.Application.Features.PRIs.IntegrationEvents;
+using Cfo.Cats.Domain.Entities.PRIs;
 using MassTransit;
 
 namespace Cfo.Cats.Application.Features.ManagementInformation.IntegrationEventHandlers;
@@ -38,13 +39,19 @@ public class RaisePaymentsAfterApprovalConsumer(IUnitOfWork unitOfWork) : IConsu
                 .Where(e => e.AssignedTo != null)
                 .Select(e => e)
                 .ToArrayAsync();
-            
+
+            var mandatoryTasks = (await unitOfWork.DbContext.PathwayPlans
+                .AsNoTracking()
+                .SelectMany(p => p.Objectives.Where(o => pris.Select(p => p.ObjectiveId).Contains(o.Id)))
+                .SelectMany(o => o.Tasks.Where(t => t.Completed != null && t.Index == 2))
+                .ToArrayAsync())
+                .Select(task => new { task, PRI = pris.First(p => p.ObjectiveId == task.ObjectiveId) });
+
             events.AddRange(wings.Select(wi => new WingInductionCreatedIntegrationEvent(wi, DateTime.UtcNow)));
             events.AddRange(hubs.Select(hi => new HubInductionCreatedIntegrationEvent(hi, DateTime.UtcNow)));
-            events.AddRange(activities.Select(e =>
-                new ActivityApprovedIntegrationEvent(e.Id, e.ApprovedOn!.Value)));
-
-            events.AddRange(pris.Select(e => new PRIAssignedIntegrationEvent(e.Id, e.MeetingAttendedOn.ToDateTime(TimeOnly.MinValue) ) ));
+            events.AddRange(activities.Select(e => new ActivityApprovedIntegrationEvent(e.Id, e.ApprovedOn!.Value)));
+            events.AddRange(pris.Select(e => new PRIAssignedIntegrationEvent(e.Id, e.MeetingAttendedOn.ToDateTime(TimeOnly.MinValue))));
+            events.AddRange(mandatoryTasks.Select(task => new PRIThroughTheGateCompletedIntegrationEvent(task.PRI.Id)));
 
             await context.PublishBatch(events);
         }
