0dcaf35e83c540ef4e3f130238bbf446fa35cb92
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2025-02-21 18:35:48 +0000

    (WIP): initial commit for command base sync
    Speed up the job by pushing the work into a command and command handler that can be handled concurrently

---
+++ src/Application/Common/MessageBus/ICommand.cs
@@ -0,0 +1,11 @@
+namespace Cfo.Cats.Application.Common.MessageBus;
+
+/// <summary>
+/// Represents a command that will be sent to the message bus.
+///
+/// This differs from an IRequest used by the domain services
+/// </summary>
+public interface ICommand
+{
+    
+}
