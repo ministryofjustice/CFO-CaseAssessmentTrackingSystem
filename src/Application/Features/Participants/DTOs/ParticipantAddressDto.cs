c64be2a4ee5c6c8c6a845076554fe70d41b0fbf5
samgibsonmoj <sam.gibson@justice.gov.uk>
2025-02-26 17:09:32 +0000

    WIP - Ordnance Places API integration

---
+++ src/Application/Features/Participants/DTOs/ParticipantAddressDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Cfo.Cats.Application.Features.Participants.DTOs;
+
+public class ParticipantAddressDto
+{
+    public required string Address { get; set; }
+    public required string UPRN { get; set; }
+    public required double X_COORDINATE { get; set; }
+    public required double Y_COORDINATE { get; set; }
+}
---
+++ src/Infrastructure/Services/Ordnance/AddressLookupService.cs
@@ -0,0 +1,67 @@
+﻿using Cfo.Cats.Application.Features.Participants.DTOs;
+using System.Net.Http.Json;
+
+namespace Cfo.Cats.Infrastructure.Services.Ordnance;
+
+public class AddressLookupService(
+    HttpClient client) : IAddressLookupService
+{
+    public async Task<Result<IEnumerable<ParticipantAddressDto>>> SearchAsync(string query, CancellationToken cancellationToken)
+    {
+        var queryParams = new[]
+        {
+            "lr=EN",
+            $"query={query}",
+            "maxresults=15"
+        };
+
+        var result = await client.GetFromJsonAsync<OrdnanceResponse>($"find?{string.Join('&', queryParams)}", cancellationToken);
+
+        if(result is { Error: not null })
+        {
+            // Error occurred
+            throw new Exception(result.Error.Message);
+        }
+
+        IEnumerable<OrdnanceResult> results = result!.Results ?? [];
+
+        return Result<IEnumerable<ParticipantAddressDto>>.Success(results.Select(x => x.DPA));
+    }
+
+    public async Task<Result<IEnumerable<ParticipantAddressDto>>> SearchByPostCodeAsync(string postCode, CancellationToken cancellationToken)
+    {
+        var queryParams = new[]
+        {
+            "lr=EN",
+            $"postcode={postCode}"
+        };
+
+        var result = await client.GetFromJsonAsync<dynamic>($"/postcode?{string.Join('&', queryParams)}", cancellationToken);
+
+        return Result<IEnumerable<ParticipantAddressDto>>.Success([]);
+    }
+}
+
+public interface IAddressLookupService
+{
+    Task<Result<IEnumerable<ParticipantAddressDto>>> SearchAsync(string query, CancellationToken cancellationToken);
+    Task<Result<IEnumerable<ParticipantAddressDto>>> SearchByPostCodeAsync(string postCode, CancellationToken cancellationToken);
+}
+
+public record class OrdnanceResponse
+{
+    public IEnumerable<OrdnanceResult>? Results { get; set; }
+    public OrdnanceError? Error { get; set; }
+
+}
+
+public record class OrdnanceResult
+{
+    public required ParticipantAddressDto DPA { get; set; }
+}
+
+public record class OrdnanceError
+{
+    public required int StatusCode { get; set; }
+    public required string Message { get; set; }
+}
