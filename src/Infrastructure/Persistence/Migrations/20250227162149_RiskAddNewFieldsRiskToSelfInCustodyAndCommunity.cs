cf4e65d5e0ac13e863833ad1f75c747d36955f32
VS <vinay.saini@justice.gov.uk>
2025-02-27 18:31:12 +0000

    added migration for new fields for risk to self in custody and community

---
+++ src/Infrastructure/Persistence/Migrations/20250227162149_RiskAddNewFieldsRiskToSelfInCustodyAndCommunity.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cfo.Cats.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RiskAddNewFieldsRiskToSelfInCustodyAndCommunity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RiskToSelfInCommunityNew",
+                schema: "Participant",
+                table: "Risk",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RiskToSelfInCustodyNew",
+                schema: "Participant",
+                table: "Risk",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RiskToSelfInCommunityNew",
+                schema: "Participant",
+                table: "Risk");
+
+            migrationBuilder.DropColumn(
+                name: "RiskToSelfInCustodyNew",
+                schema: "Participant",
+                table: "Risk");
+        }
+    }
+}
