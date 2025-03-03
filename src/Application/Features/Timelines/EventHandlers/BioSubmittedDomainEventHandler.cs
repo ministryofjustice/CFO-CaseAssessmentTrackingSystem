504ef6e66b2055c57e1ef7450ac682b44a891f1d
PaulCooperWorkJustice <paul.cooper5@justice.gov.uk>
2025-03-03 14:35:46 +0000

    CFODEV-967:  Add Bio Due functionality.

---
+++ src/Application/Features/Timelines/EventHandlers/BioSubmittedDomainEventHandler.cs
@@ -0,0 +1,11 @@
+﻿using Cfo.Cats.Domain.Events;
+
+namespace Cfo.Cats.Application.Features.Timelines.EventHandlers;
+
+public class BioSubmittedDomainEventHandler(ICurrentUserService currentUserService, IUnitOfWork unitOfWork) : TimelineNotificationHandler<BioSubmittedDomainEvent>(currentUserService, unitOfWork)
+{
+    protected override string GetLine1(BioSubmittedDomainEvent notification) => "Bio Submitted";
+    protected override TimelineEventType GetEventType() => TimelineEventType.Bio;
+    protected override string GetParticipantId(BioSubmittedDomainEvent notification)
+        => notification.Item.ParticipantId;
+}
