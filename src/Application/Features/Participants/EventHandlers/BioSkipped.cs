ee9ab5aa2c059ebc06c991a56255fd35705dfeb6
PaulCooperWorkJustice <paul.cooper5@justice.gov.uk>
2025-03-03 14:35:46 +0000

    CFODEV-967:  Add Bio Due functionality.

---
+++ src/Application/Features/Participants/EventHandlers/BioSkipped.cs
@@ -0,0 +1,19 @@
+﻿using Cfo.Cats.Domain.Events;
+
+namespace Cfo.Cats.Application.Features.Participants.EventHandlers;
+
+public class BioSkipped(IUnitOfWork unitOfWork) : INotificationHandler<BioSkippedDomainEvent>
+{
+    public async Task Handle(BioSkippedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        var participant = await unitOfWork.DbContext.Participants
+            .Where(p => p.Id == notification.Entity.ParticipantId)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (participant is not null)
+        {
+            participant.SetBioDue(DateTime.UtcNow.AddDays(70));
+            unitOfWork.DbContext.Participants.Update(participant);
+        }
+    }
+}
