e15390c8dc01ac75824dd44feaee4f62851519f8
PaulCooperWorkJustice <paul.cooper5@justice.gov.uk>
2025-03-03 14:35:46 +0000

    CFODEV-967:  Add Bio Due functionality.

--- src/Domain/Entities/Bios/ParticipantBio.cs
+++ src/Domain/Entities/Bios/ParticipantBio.cs
@@ -2,14 +2,11 @@ using Cfo.Cats.Domain.Common.Contracts;
 using Cfo.Cats.Domain.Common.Entities;
 using Cfo.Cats.Domain.Common.Enums;
 using Cfo.Cats.Domain.Events;
-using System.Security.Cryptography;
 
 namespace Cfo.Cats.Domain.Entities.Bios
 {
-
     public class ParticipantBio : BaseAuditableEntity<Guid>, IAuditTrial
     {
-
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         private ParticipantBio()
         {
@@ -27,7 +24,14 @@ namespace Cfo.Cats.Domain.Entities.Bios
             ParticipantId = participantId;
             BioJson = bioJson;
             Status = status;
-            AddDomainEvent(new BioCreatedDomainEvent(this));
+            if (status == BioStatus.SkippedForNow)
+            {
+                AddDomainEvent(new BioSkippedDomainEvent(this));
+            }
+            else if (status == BioStatus.InProgress)
+            {
+                AddDomainEvent(new BioCreatedDomainEvent(this));
+            }
         }
 
         public ParticipantBio UpdateJson(string json)
