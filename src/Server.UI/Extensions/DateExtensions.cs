d3242e0bc26e0c676e9a132e9c0c61c29cc7c3ae
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2025-03-06 13:03:49 +0000

    CFODEV-1131: Fix flashing issues with activity QA

---
+++ src/Server.UI/Extensions/DateExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Cfo.Cats.Server.UI.Extensions;
+
+public static class DateExtensions
+{
+    public static string ToShortDateOrEmptyString(this DateTime? datetime)
+    {
+        if (datetime.HasValue is false)
+        {
+            return string.Empty;
+        }
+
+        return datetime.Value.ToShortDateString();
+    }
+}
