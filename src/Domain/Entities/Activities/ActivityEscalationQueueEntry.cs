ca94d6304d144edbe1a79d90f4efa6d9ac24f510
PaulCooperWorkJustice <paul.cooper5@justice.gov.uk>
2025-03-06 12:16:04 +0000

    CFODEV-1101:  Added extra details for Activity QA queues

--- src/Domain/Entities/Activities/ActivityEscalationQueueEntry.cs
+++ src/Domain/Entities/Activities/ActivityEscalationQueueEntry.cs
@@ -1,38 +1,31 @@
-﻿using Cfo.Cats.Domain.Entities.Participants;
-using Cfo.Cats.Domain.Events.QA.Payables;
+﻿using Cfo.Cats.Domain.Events.QA.Payables;
 
-namespace Cfo.Cats.Domain.Entities.Activities
-{    
-    public class ActivityEscalationQueueEntry : ActivityQueueEntry
+namespace Cfo.Cats.Domain.Entities.Activities;
+
+public class ActivityEscalationQueueEntry : ActivityQueueEntry
+{
+    private ActivityEscalationQueueEntry() : base()
     {
-        private ActivityEscalationQueueEntry() : base(Guid.Empty)
-        {
-        }
+    }
+         
+    public ActivityEscalationQueueEntry(Guid activityId, string tenantId, string supportWorkerId, DateTime originalPQASubmissionDate, string participantId)
+      : base(activityId, tenantId, supportWorkerId, originalPQASubmissionDate, participantId)
+    {            
+    }  
 
-        private ActivityEscalationQueueEntry(Guid activityId, string tenantId, Participant participant) : base(activityId)
-        {
-            TenantId = tenantId;
-            Participant = participant;
-            //todo: Add event?
-        }
+    public override ActivityQueueEntry Accept()
+    {
+        IsAccepted = true;
+        IsCompleted = true;
+        AddDomainEvent(new ActivityEscalationEntryCompletedDomainEvent(this));
+        return this;
+    }
 
-        public static ActivityEscalationQueueEntry Create(Guid activityId, string tenantId, Participant participant)
-            => new(activityId, tenantId, participant);
-
-        public override ActivityQueueEntry Accept()
-        {
-            IsAccepted = true;
-            IsCompleted = true;
-            AddDomainEvent(new ActivityEscalationEntryCompletedDomainEvent(this));
-            return this;
-        }
-
-        public override ActivityQueueEntry Return()
-        {
-            IsAccepted = false;
-            IsCompleted = true;
-            AddDomainEvent(new ActivityEscalationEntryCompletedDomainEvent(this));
-            return this;
-        }
+    public override ActivityQueueEntry Return()
+    {
+        IsAccepted = false;
+        IsCompleted = true;
+        AddDomainEvent(new ActivityEscalationEntryCompletedDomainEvent(this));
+        return this;
     }
 }
---
+++ src/Infrastructure/Persistence/Migrations/20250306121429_AddExtraActivityQueueDetails.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cfo.Cats.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExtraActivityQueueDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OriginalPQASubmissionDate",
+                schema: "Activities",
+                table: "ActivityQa2Queue",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "SupportWorkerId",
+                schema: "Activities",
+                table: "ActivityQa2Queue",
+                type: "nvarchar(36)",
+                maxLength: 36,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OriginalPQASubmissionDate",
+                schema: "Activities",
+                table: "ActivityQa1Queue",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "SupportWorkerId",
+                schema: "Activities",
+                table: "ActivityQa1Queue",
+                type: "nvarchar(36)",
+                maxLength: 36,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OriginalPQASubmissionDate",
+                schema: "Activities",
+                table: "ActivityPqaQueue",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "SupportWorkerId",
+                schema: "Activities",
+                table: "ActivityPqaQueue",
+                type: "nvarchar(36)",
+                maxLength: 36,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OriginalPQASubmissionDate",
+                schema: "Activities",
+                table: "ActivityEscalationQueue",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "SupportWorkerId",
+                schema: "Activities",
+                table: "ActivityEscalationQueue",
+                type: "nvarchar(36)",
+                maxLength: 36,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OriginalPQASubmissionDate",
+                schema: "Activities",
+                table: "ActivityQa2Queue");
+
+            migrationBuilder.DropColumn(
+                name: "SupportWorkerId",
+                schema: "Activities",
+                table: "ActivityQa2Queue");
+
+            migrationBuilder.DropColumn(
+                name: "OriginalPQASubmissionDate",
+                schema: "Activities",
+                table: "ActivityQa1Queue");
+
+            migrationBuilder.DropColumn(
+                name: "SupportWorkerId",
+                schema: "Activities",
+                table: "ActivityQa1Queue");
+
+            migrationBuilder.DropColumn(
+                name: "OriginalPQASubmissionDate",
+                schema: "Activities",
+                table: "ActivityPqaQueue");
+
+            migrationBuilder.DropColumn(
+                name: "SupportWorkerId",
+                schema: "Activities",
+                table: "ActivityPqaQueue");
+
+            migrationBuilder.DropColumn(
+                name: "OriginalPQASubmissionDate",
+                schema: "Activities",
+                table: "ActivityEscalationQueue");
+
+            migrationBuilder.DropColumn(
+                name: "SupportWorkerId",
+                schema: "Activities",
+                table: "ActivityEscalationQueue");
+        }
+    }
+}
