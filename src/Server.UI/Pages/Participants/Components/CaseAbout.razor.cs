2a069d9161f3a018361fd83f502d0204a23d6560
samgibsonmoj <sam.gibson@justice.gov.uk>
2025-03-06 17:39:03 +0000

    Ability to add and edit contact information

--- src/Server.UI/Pages/Participants/Components/CaseAbout.razor.cs
+++ src/Server.UI/Pages/Participants/Components/CaseAbout.razor.cs
@@ -1,13 +1,13 @@
-﻿using Cfo.Cats.Application.Features.Activities.Queries;
+﻿using AutoMapper;
 using Cfo.Cats.Application.Features.Participants.Commands;
 using Cfo.Cats.Application.Features.Participants.DTOs;
 using Cfo.Cats.Application.Features.Participants.Queries;
-using Cfo.Cats.Server.UI.Pages.Activities;
-using MassTransit.RabbitMqTransport.Topology;
+using Cfo.Cats.Infrastructure.Constants;
+using Cfo.Cats.Server.UI.Components.Dialogs;
 
 namespace Cfo.Cats.Server.UI.Pages.Participants.Components;
 
-public partial class CaseAbout
+public partial class CaseAbout(IMapper mapper)
 {
     [Parameter, EditorRequired]
     public required string ParticipantId { get; set; }
@@ -34,33 +34,46 @@ public partial class CaseAbout
         });
     }
 
-    async Task AddContact()
+    async Task AddContact() 
+        => await AddOrEditContact(new AddOrEditContactDetail.Command { ParticipantId = ParticipantId }, "Add Contact Details");
+
+    async Task EditContact(ParticipantContactDetailDto contact) =>
+        await AddOrEditContact(mapper.Map<AddOrEditContactDetail.Command>(contact), "Edit Contact Details");
+
+    async Task AddOrEditContact(AddOrEditContactDetail.Command command, string title)
     {
         var parameters = new DialogParameters<AddressDialog>()
         {
-            {
-                x => x.Model, new AddContactDetail.Command()
-                {
-                    ParticipantId = ParticipantId
-                }
-            }
+            { x => x.Model, command }
         };
 
-        var options = new DialogOptions
-        {
-            MaxWidth = MaxWidth.Small,
-            FullWidth = true,
-            CloseButton = true,
-            BackdropClick = false
-        };
+        var options = new DialogOptions { MaxWidth = MaxWidth.Small, FullWidth = true, CloseButton = true, BackdropClick = false };
 
-        var dialog = await DialogService.ShowAsync<AddressDialog>("Add Contact Details", parameters, options);
+        var dialog = await DialogService.ShowAsync<AddressDialog>(title, parameters, options);
 
-        if(await dialog.Result is not { Canceled: true })
+        if (await dialog.Result is not { Canceled: true })
         {
             await Refresh();
         }
     }
 
-    Task Edit() => Task.CompletedTask;
+    async Task DeleteContact(ParticipantContactDetailDto contact)
+    {
+        var parameters = new DialogParameters<DeleteConfirmation>
+        {
+            { x => x.Command, new DeleteContactDetail.Command() { ContactDetailId = contact.Id } },
+            { x => x.ContentText, string.Format(ConstantString.DeleteConfirmation, contact.Description) }
+        };
+
+        var options = new DialogOptions { CloseButton = true, MaxWidth = MaxWidth.Small, FullWidth = true };
+
+        var dialog = await DialogService.ShowAsync<DeleteConfirmation>("Delete Confirmation", parameters, options);
+
+        if (await dialog.Result is not { Canceled: true })
+        {
+            await Refresh();
+        }
+    }
+
+    async Task EditPersonalInformation() => await Task.CompletedTask;
 }
