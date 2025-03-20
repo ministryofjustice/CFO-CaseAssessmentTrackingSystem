29c848a9b3fecbed9eb0c3316b4817d7573d0185
PaulCooperWorkJustice <paul.cooper5@justice.gov.uk>
2025-03-20 12:19:26 +0000

    CFODEV-926:  Add abandoning to Wing Induction Phases

--- src/Domain/Entities/Inductions/InductionPhase.cs
+++ src/Domain/Entities/Inductions/InductionPhase.cs
@@ -1,13 +1,35 @@
 ﻿
+using Cfo.Cats.Domain.Common.Enums;
+
 namespace Cfo.Cats.Domain.Entities.Inductions;
 
-public class InductionPhase(int number, DateTime startDate, DateTime? completedDate) : ValueObject
+public class InductionPhase(int number, DateTime startDate, DateTime? completedDate, WingInductionPhaseStatus status, string? abandonJustification, WingInductionPhaseAbandonReason? abandonReason, string? completedBy) : ValueObject
 {
     public int Number { get; private set; } = number;
 
     public DateTime StartDate { get; private set; } = startDate;
     
     public DateTime? CompletedDate { get; private set; } = completedDate;
+
+    /// <summary>
+    /// Status of the Wing Induction Phase
+    /// </summary>
+    public WingInductionPhaseStatus Status { get; private set; } = status;
+
+    /// <summary>
+    /// The justification for Abandoning Wing Induction Phase
+    /// </summary>
+    public string? AbandonJustification { get; private set; } = abandonJustification;
+
+    /// <summary>
+    /// The reason for Abandoning Wing Induction Phase
+    /// </summary>
+    public WingInductionPhaseAbandonReason? AbandonReason { get; private set; } = abandonReason;
+
+    /// <summary>
+    /// Who Abandoned the Wing Induction Phase
+    /// </summary>
+    public string? CompletedBy { get; private set; } = completedBy;
 
     protected override IEnumerable<object> GetEqualityComponents()
     {
--- src/Infrastructure/Persistence/Configurations/Inductions/WingInductionEntityTypeConfiguration.cs
+++ src/Infrastructure/Persistence/Configurations/Inductions/WingInductionEntityTypeConfiguration.cs
@@ -1,4 +1,6 @@
-﻿using Cfo.Cats.Domain.Entities.Inductions;
+﻿using Cfo.Cats.Application.Common.Validators;
+using Cfo.Cats.Domain.Common.Enums;
+using Cfo.Cats.Domain.Entities.Inductions;
 using Cfo.Cats.Domain.Entities.Participants;
 using Cfo.Cats.Domain.Identity;
 using Cfo.Cats.Infrastructure.Constants.Database;
@@ -67,7 +69,25 @@ public class WingInductionEntityTypeConfiguration : IEntityTypeConfiguration<Win
                 );
             phase.Property(x => x.StartDate).IsRequired();
             phase.Property(x => x.CompletedDate).IsRequired(false);
-        });
 
+            phase.Property(a => a.Status)
+              .IsRequired()
+              .HasConversion(
+                s => s!.Value,
+                s => WingInductionPhaseStatus.FromValue(s));
+
+            phase.HasOne<ApplicationUser>()
+                .WithMany()
+                .HasForeignKey(p => p.CompletedBy)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            phase.Property(e => e.AbandonReason)
+                .HasConversion(
+                ar => ar!.Value,
+                ar => WingInductionPhaseAbandonReason.FromValue(ar));
+
+            phase.Property(p => p.AbandonJustification)
+                .HasMaxLength(ValidationConstants.NotesLength);
+        });
     }
 }
