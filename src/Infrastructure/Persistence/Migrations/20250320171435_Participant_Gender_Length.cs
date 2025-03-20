fdcbbfca74a78cf0dc43fca90a127a4964e32a42
samgibsonmoj <sam.gibson@justice.gov.uk>
2025-03-20 17:15:02 +0000

    Increase gender column length from 9 -> 29 characters

---
+++ src/Infrastructure/Persistence/Migrations/20250320171435_Participant_Gender_Length.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cfo.Cats.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Participant_Gender_Length : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Gender",
+                schema: "Participant",
+                table: "Participant",
+                type: "nvarchar(29)",
+                maxLength: 29,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(6)",
+                oldMaxLength: 6,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Gender",
+                schema: "Participant",
+                table: "Participant",
+                type: "nvarchar(6)",
+                maxLength: 6,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(29)",
+                oldMaxLength: 29,
+                oldNullable: true);
+        }
+    }
+}
