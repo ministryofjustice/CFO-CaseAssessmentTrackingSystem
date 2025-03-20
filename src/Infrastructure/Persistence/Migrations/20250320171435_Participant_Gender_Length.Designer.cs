15735861f3c653f3bfc48cc61c450374a97afc51
samgibsonmoj <sam.gibson@justice.gov.uk>
2025-03-20 17:15:02 +0000

    Increase gender column length from 9 -> 29 characters

--- src/Infrastructure/Persistence/Migrations/20250320171435_Participant_Gender_Length.Designer.cs
+++ src/Infrastructure/Persistence/Migrations/20250320171435_Participant_Gender_Length.Designer.cs
@@ -4,6 +4,7 @@ using Cfo.Cats.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cfo.Cats.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250320171435_Participant_Gender_Length")]
+    partial class Participant_Gender_Length
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -1004,16 +1007,10 @@ namespace Cfo.Cats.Infrastructure.Persistence.Migrations
                     b.Property<Guid>("ActivityId")
                         .HasColumnType("uniqueidentifier");
 
-                    b.Property<DateTime>("ActivityInput")
-                        .HasColumnType("date");
-
                     b.Property<string>("ActivityType")
                         .IsRequired()
                         .HasMaxLength(50)
                         .HasColumnType("nvarchar(50)");
-
-                    b.Property<DateTime>("CommencedDate")
-                        .HasColumnType("date");
 
                     b.Property<string>("ContractId")
                         .IsRequired()
@@ -1042,9 +1039,6 @@ namespace Cfo.Cats.Infrastructure.Persistence.Migrations
                         .IsRequired()
                         .HasMaxLength(9)
                         .HasColumnType("nvarchar(9)");
-
-                    b.Property<DateTime>("PaymentPeriod")
-                        .HasColumnType("date");
 
                     b.Property<string>("TenantId")
                         .IsRequired()
@@ -1196,12 +1190,6 @@ namespace Cfo.Cats.Infrastructure.Persistence.Migrations
                     b.Property<Guid>("ActivityId")
                         .HasColumnType("uniqueidentifier");
 
-                    b.Property<DateTime>("ActivityInput")
-                        .HasColumnType("date");
-
-                    b.Property<DateTime>("CommencedDate")
-                        .HasColumnType("date");
-
                     b.Property<string>("ContractId")
                         .IsRequired()
                         .HasMaxLength(12)
@@ -1240,9 +1228,6 @@ namespace Cfo.Cats.Infrastructure.Persistence.Migrations
                         .HasMaxLength(9)
                         .HasColumnType("nvarchar(9)");
 
-                    b.Property<DateTime>("PaymentPeriod")
-                        .HasColumnType("date");
-
                     b.Property<string>("TenantId")
                         .IsRequired()
                         .HasMaxLength(50)
@@ -1250,7 +1235,7 @@ namespace Cfo.Cats.Infrastructure.Persistence.Migrations
 
                     b.HasKey("Id");
 
-                    b.HasIndex("ParticipantId", "ContractId", "CourseLevel", "CourseTitle", "EligibleForPayment")
+                    b.HasIndex("ParticipantId", "ContractId", "ActivityApproved")
                         .HasDatabaseName("ix_ActivityPayment_ParticipantId");
 
                     b.ToTable("EducationPayment", "Mi");
@@ -1267,12 +1252,6 @@ namespace Cfo.Cats.Infrastructure.Persistence.Migrations
 
                     b.Property<Guid>("ActivityId")
                         .HasColumnType("uniqueidentifier");
-
-                    b.Property<DateTime>("ActivityInput")
-                        .HasColumnType("date");
-
-                    b.Property<DateTime>("CommencedDate")
-                        .HasColumnType("date");
 
                     b.Property<string>("ContractId")
                         .IsRequired()
@@ -1302,9 +1281,6 @@ namespace Cfo.Cats.Infrastructure.Persistence.Migrations
                         .HasMaxLength(9)
                         .HasColumnType("nvarchar(9)");
 
-                    b.Property<DateTime>("PaymentPeriod")
-                        .HasColumnType("date");
-
                     b.Property<string>("TenantId")
                         .IsRequired()
                         .HasMaxLength(50)
@@ -1312,7 +1288,7 @@ namespace Cfo.Cats.Infrastructure.Persistence.Migrations
 
                     b.HasKey("Id");
 
-                    b.HasIndex("ParticipantId", "ContractId", "CommencedDate", "EligibleForPayment")
+                    b.HasIndex("ParticipantId", "ContractId", "ActivityApproved")
                         .HasDatabaseName("ix_ActivityPayment_ParticipantId");
 
                     b.ToTable("EmploymentPayment", "Mi");
@@ -1403,9 +1379,6 @@ namespace Cfo.Cats.Infrastructure.Persistence.Migrations
                     b.Property<DateTime>("Approved")
                         .HasColumnType("date");
 
-                    b.Property<DateTime>("CommencedDate")
-                        .HasColumnType("date");
-
                     b.Property<string>("ContractId")
                         .IsRequired()
                         .HasMaxLength(12)
@@ -1418,9 +1391,6 @@ namespace Cfo.Cats.Infrastructure.Persistence.Migrations
                         .HasColumnType("bit");
 
                     b.Property<DateTime>("Induction")
-                        .HasColumnType("date");
-
-                    b.Property<DateTime>("InductionInput")
                         .HasColumnType("date");
 
                     b.Property<string>("IneligibilityReason")
@@ -1441,9 +1411,6 @@ namespace Cfo.Cats.Infrastructure.Persistence.Migrations
                         .IsRequired()
                         .HasMaxLength(9)
                         .HasColumnType("nvarchar(9)");
-
-                    b.Property<DateTime>("PaymentPeriod")
-                        .HasColumnType("date");
 
                     b.Property<string>("SupportWorker")
                         .IsRequired()
@@ -1468,9 +1435,6 @@ namespace Cfo.Cats.Infrastructure.Persistence.Migrations
                     b.Property<Guid>("Id")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("uniqueidentifier");
-
-                    b.Property<DateTime>("ActivityInput")
-                        .HasColumnType("date");
 
                     b.Property<DateTime>("Approved")
                         .HasColumnType("date");
@@ -1502,9 +1466,6 @@ namespace Cfo.Cats.Infrastructure.Persistence.Migrations
                         .IsRequired()
                         .HasMaxLength(9)
                         .HasColumnType("nvarchar(9)");
-
-                    b.Property<DateTime>("PaymentPeriod")
-                        .HasColumnType("date");
 
                     b.Property<Guid>("PriId")
                         .HasColumnType("uniqueidentifier");
