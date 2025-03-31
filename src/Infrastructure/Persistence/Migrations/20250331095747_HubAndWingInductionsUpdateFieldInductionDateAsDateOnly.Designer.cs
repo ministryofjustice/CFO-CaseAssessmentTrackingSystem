eaa962ae76cea6464503dda971619be8ecfcc527
VS <vinay.saini@justice.gov.uk>
2025-03-31 11:33:07 +0100

    migration for updated InductionDate field, now records date only

--- src/Infrastructure/Persistence/Migrations/20250331095747_HubAndWingInductionsUpdateFieldInductionDateAsDateOnly.Designer.cs
+++ src/Infrastructure/Persistence/Migrations/20250331095747_HubAndWingInductionsUpdateFieldInductionDateAsDateOnly.Designer.cs
@@ -4,6 +4,7 @@ using Cfo.Cats.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cfo.Cats.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250331095747_HubAndWingInductionsUpdateFieldInductionDateAsDateOnly")]
+    partial class HubAndWingInductionsUpdateFieldInductionDateAsDateOnly
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -1988,9 +1991,6 @@ namespace Cfo.Cats.Infrastructure.Persistence.Migrations
 
                     b.Property<string>("AssessmentJustification")
                         .HasColumnType("nvarchar(max)");
-
-                    b.Property<DateTime?>("BioDue")
-                        .HasColumnType("datetime2");
 
                     b.Property<int>("ConsentStatus")
                         .HasColumnType("int");
---
+++ src/Infrastructure/Persistence/Migrations/20250331095747_HubAndWingInductionsUpdateFieldInductionDateAsDateOnly.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cfo.Cats.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class HubAndWingInductionsUpdateFieldInductionDateAsDateOnly : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InductionDate",
+                schema: "Induction",
+                table: "WingInduction",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InductionDate",
+                schema: "Induction",
+                table: "HubInduction",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InductionDate",
+                schema: "Induction",
+                table: "WingInduction",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InductionDate",
+                schema: "Induction",
+                table: "HubInduction",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+        }
+    }
+}
