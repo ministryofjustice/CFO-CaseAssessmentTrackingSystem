1ae9c39607e8fb9be08d8c93630d16280fd957f7
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2025-04-03 07:04:58 +0100

    CFODEV-736: make mobile a text field
    makes the mobile number a text field

--- src/Server.UI/Pages/Participants/Components/AddressDialog.razor.cs
+++ src/Server.UI/Pages/Participants/Components/AddressDialog.razor.cs
@@ -5,31 +5,28 @@ using Cfo.Cats.Infrastructure.Services.Ordnance;
 
 namespace Cfo.Cats.Server.UI.Pages.Participants.Components;
 
-public partial class AddressDialog(IAddressLookupService AddressLookupService)
+public partial class AddressDialog(IAddressLookupService addressLookupService)
 {
-    MudForm form = new();
+    private MudForm _form = new();
+    private bool _saving = false;
 
     [CascadingParameter] private IMudDialogInstance Dialog { get; set; } = default!;
 
     [Parameter]
     public required AddOrUpdateContactDetail.Command Model { get; set; }
-
-    bool saving = false;
-
-    async Task<IEnumerable<ParticipantAddressDto>> Search(string searchText, CancellationToken cancellationToken)
+    
+    private async Task<IEnumerable<ParticipantAddressDto>> Search(string searchText, CancellationToken cancellationToken)
     {
         if (string.IsNullOrWhiteSpace(searchText) is false)
         {
-            var response = await AddressLookupService.SearchAsync(searchText, CancellationToken.None);
+            var response = await addressLookupService.SearchAsync(searchText, CancellationToken.None);
 
-            if (response.Succeeded && response.Data is not null)
+            if (response is { Succeeded: true, Data: not null })
             {
                 return response.Data;
             }
-            else
-            {
-                Snackbar.Add(response.ErrorMessage, Severity.Error);
-            }
+
+            Snackbar.Add(response.ErrorMessage, Severity.Error);
         }
 
         return [];
@@ -39,11 +36,11 @@ public partial class AddressDialog(IAddressLookupService AddressLookupService)
     {
         try
         {
-            saving = true;
+            _saving = true;
 
-            await form.Validate();
+            await _form.Validate();
 
-            if(form.IsValid is false)
+            if (_form.IsValid is false)
             {
                 return;
             }
@@ -61,7 +58,10 @@ public partial class AddressDialog(IAddressLookupService AddressLookupService)
             }
 
         }
-        finally { saving = false; }
+        finally
+        {
+            _saving = false;
+        }
     }
 
 }
