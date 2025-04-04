9a6ae518cf0135b2fff8730f636a5c273fa83e25
PaulCooperWorkJustice <paul.cooper5@justice.gov.uk>
2025-04-04 12:07:32 +0100

    Add risk due reason database and dto stuff.

--- src/Infrastructure/Persistence/Migrations/20250404104319_Participant_AddRiskDueReason.Designer.cs
+++ src/Infrastructure/Persistence/Migrations/20250404104319_Participant_AddRiskDueReason.Designer.cs
@@ -4,6 +4,7 @@ using Cfo.Cats.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cfo.Cats.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250404104319_Participant_AddRiskDueReason")]
+    partial class Participant_AddRiskDueReason
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -846,7 +849,7 @@ namespace Cfo.Cats.Infrastructure.Persistence.Migrations
                         .HasColumnType("nvarchar(36)");
 
                     b.Property<DateTime>("InductionDate")
-                        .HasColumnType("date");
+                        .HasColumnType("datetime2");
 
                     b.Property<DateTime?>("LastModified")
                         .HasColumnType("datetime2");
@@ -902,7 +905,7 @@ namespace Cfo.Cats.Infrastructure.Persistence.Migrations
                         .HasColumnType("nvarchar(36)");
 
                     b.Property<DateTime>("InductionDate")
-                        .HasColumnType("date");
+                        .HasColumnType("datetime2");
 
                     b.Property<DateTime?>("LastModified")
                         .HasColumnType("datetime2");
@@ -1989,9 +1992,6 @@ namespace Cfo.Cats.Infrastructure.Persistence.Migrations
                     b.Property<string>("AssessmentJustification")
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<DateTime?>("BioDue")
-                        .HasColumnType("datetime2");
-
                     b.Property<int>("ConsentStatus")
                         .HasColumnType("int");
 
@@ -2068,7 +2068,7 @@ namespace Cfo.Cats.Infrastructure.Persistence.Migrations
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<DateTime?>("RiskDue")
-                        .HasColumnType("date");
+                        .HasColumnType("datetime2");
 
                     b.Property<int>("RiskDueReason")
                         .HasColumnType("int");
