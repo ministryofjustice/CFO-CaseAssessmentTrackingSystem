b959b2febe5080b99fe3c92812bf47ce10bfe582
VS <vinay.saini@justice.gov.uk>
2025-04-23 11:52:50 +0100

    migration- made Address, PostCode and UPCRN fields in ParticipantContactDetails table optional

---
+++ src/Infrastructure/Persistence/Migrations/20250424101413_ParticipantContactUpdatePostCodeAndUPCRNOptional.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cfo.Cats.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ParticipantContactUpdatePostCodeAndUPCRNOptional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UPRN",
+                table: "ParticipantContactDetails",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldMaxLength: 12);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PostCode",
+                table: "ParticipantContactDetails",
+                type: "nvarchar(8)",
+                maxLength: 8,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(8)",
+                oldMaxLength: 8);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UPRN",
+                table: "ParticipantContactDetails",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldMaxLength: 12,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PostCode",
+                table: "ParticipantContactDetails",
+                type: "nvarchar(8)",
+                maxLength: 8,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(8)",
+                oldMaxLength: 8,
+                oldNullable: true);
+        }
+    }
+}
