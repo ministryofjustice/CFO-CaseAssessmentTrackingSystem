fb7112012c4ef32624a8b37a845e69c774add2a7
samgibsonmoj <sam.gibson@justice.gov.uk>
2025-04-25 11:27:44 +0100

    Reapply migrations

--- src/Infrastructure/Persistence/Migrations/20250425102647_ReassessmentPayments.cs
+++ src/Infrastructure/Persistence/Migrations/20250425102647_ReassessmentPayments.cs
@@ -19,12 +19,15 @@ namespace Cfo.Cats.Infrastructure.Persistence.Migrations
                     Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
                     AssessmentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
                     AssessmentCompleted = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AssessmentCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
                     ParticipantId = table.Column<string>(type: "nvarchar(9)", maxLength: 9, nullable: false),
                     TenantId = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
                     EligibleForPayment = table.Column<bool>(type: "bit", nullable: false),
                     IneligibilityReason = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
                     PaymentPeriod = table.Column<DateTime>(type: "date", nullable: false),
                     ContractId = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: false),
+                    LocationId = table.Column<int>(type: "int", nullable: false),
+                    LocationType = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
                     SupportWorker = table.Column<string>(type: "nvarchar(36)", maxLength: 36, nullable: false)
                 },
                 constraints: table =>
