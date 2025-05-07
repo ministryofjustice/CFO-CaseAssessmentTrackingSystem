64e1eba02e320c08afb389434fd8835b564557a7
VS <vinay.saini@justice.gov.uk>
2025-05-07 09:51:15 +0100

    Undo the accidental creation of an assessment

---
+++ src/Application/Features/Assessments/Commands/DeleteAssessment.cs
@@ -0,0 +1,47 @@
+﻿using Cfo.Cats.Application.Common.Security;
+using Cfo.Cats.Application.SecurityConstants;
+
+namespace Cfo.Cats.Application.Features.Assessments.Commands;
+
+public static class DeleteAssessment
+{
+    [RequestAuthorize(Policy = SecurityPolicies.AuthorizedUser)]
+    public class Command : IRequest<Result<int>>
+    {
+        public required Guid AssessmentId { get; set; }
+    }
+
+    class Handler(IUnitOfWork unitOfWork) : IRequestHandler<Command, Result<int>>
+    {
+        public async Task<Result<int>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var assessment = await unitOfWork.DbContext.ParticipantAssessments.Where(x => x.Id == request.AssessmentId).FirstOrDefaultAsync(cancellationToken);
+            int result = 0;
+            if (assessment is not null)
+            {
+                unitOfWork.DbContext.ParticipantAssessments.Remove(assessment);
+                result = await unitOfWork.SaveChangesAsync(cancellationToken);
+            }
+            return Result<int>.Success(result);
+        }
+
+    }
+
+    public class Validator : AbstractValidator<Command>
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public Validator(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+
+            RuleFor(c => c.AssessmentId)
+                .Must(ExistAndIncomplete)
+                .WithMessage("Assessment already complete or it doesn't exist!");
+
+        }
+
+        bool ExistAndIncomplete(Guid identifier) => _unitOfWork.DbContext.ParticipantAssessments.Any(asmt => asmt.Id == identifier && asmt.Completed.HasValue == false);
+    }
+
+}
