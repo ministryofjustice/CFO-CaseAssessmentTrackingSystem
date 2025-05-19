d4c1b22751f30a1abbd4c575ba69e2218ea85c49
VS <vinay.saini@justice.gov.uk>
2025-05-19 15:14:09 +0100

    prevent double clicking submit button at PQA, QA1 and QA2 stages

---
+++ src/Server.UI/Pages/QA/Enrolments/QA1.razor.cs
@@ -0,0 +1,111 @@
+﻿using Cfo.Cats.Application.Common.Security;
+using Cfo.Cats.Application.Features.Participants.DTOs;
+using Cfo.Cats.Application.Features.Participants.Queries;
+using Cfo.Cats.Application.Features.QualityAssurance.Commands;
+using Cfo.Cats.Application.Features.QualityAssurance.DTOs;
+using Cfo.Cats.Server.UI.Pages.QA.Enrolments.Components;
+using IResult = Cfo.Cats.Application.Common.Interfaces.IResult;
+
+namespace Cfo.Cats.Server.UI.Pages.QA.Enrolments;
+
+public partial class QA1
+{
+    private int CharacterCount => Command.Message?.Length ?? 0;
+    private QaExternalMessageWarning? _warningMessage;
+    private MudForm? _form;
+    private EnrolmentQueueEntryDto? _queueEntry = null;
+    private ParticipantDto? _participantDto = null;
+
+    bool saving = false;
+
+    [CascadingParameter]
+    public UserProfile? UserProfile { get; set; } = null!;
+
+    private SubmitQa1Response.Command Command { get; set; } = default!;
+
+    private async Task GetQueueItem()
+    {
+        GrabQa1Entry.Command command = new GrabQa1Entry.Command()
+        {
+            CurrentUser = UserProfile!
+        };
+
+        var result = await GetNewMediator().Send(command);
+
+        if (result.Succeeded)
+        {
+            _queueEntry = result.Data!;
+
+            _participantDto = await GetNewMediator().Send(new GetParticipantById.Query()
+            {
+                Id = _queueEntry.ParticipantId
+            });
+
+            Command = new SubmitQa1Response.Command()
+            {
+                QueueEntryId = _queueEntry.Id,
+                CurrentUser = UserProfile
+            };
+        }
+        else
+        {
+            Snackbar.Add(result.ErrorMessage, Severity.Info);
+        }
+    }
+
+    protected async Task SubmitToQa()
+    {
+        try
+        {
+            saving = true;
+            await _form!.Validate().ConfigureAwait(false);
+
+            if (_form.IsValid is false)
+            {
+                return;
+            }
+            var result = await GetNewMediator().Send(Command);
+
+            if (result.Succeeded)
+            {
+                Snackbar.Add("Participant submitted to QA2", Severity.Info);
+                Navigation.NavigateTo("/pages/qa/enrolments/qa1", true);
+            }
+            else
+            {
+                ShowActionFailure("Failed to submit", result);
+            }
+        }
+        finally { saving = false; }
+    }
+
+    private void ShowActionFailure(string title, IResult result)
+    {
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+        sb.Append("<div>");
+        sb.Append($"<h2>{title}</h2>");
+        sb.Append("<ul>");
+
+        foreach (var e in result.Errors)
+        {
+            sb.Append($"<li>{e}</li>");
+        }
+
+        sb.Append("</ul>");
+        sb.Append("</div>");
+
+        Snackbar.Add(sb.ToString(), Severity.Error, options =>
+        {
+            options.RequireInteraction = true;
+            options.SnackbarVariant = Variant.Text;
+        });
+    }
+
+
+
+    private void UpdateCharacterCount(ChangeEventArgs args)
+    {
+        Command.Message = args?.Value?.ToString() ?? string.Empty;
+    }
+}
