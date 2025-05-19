20e2cc417d6841469de43c6f70f25032472e42d0
VS <vinay.saini@justice.gov.uk>
2025-05-19 15:14:09 +0100

    prevent double clicking submit button at PQA, QA1 and QA2 stages

---
+++ src/Server.UI/Pages/QA/Enrolments/Escalation.razor.cs
@@ -0,0 +1,125 @@
+﻿using Cfo.Cats.Application.Common.Security;
+using Cfo.Cats.Application.Features.Participants.DTOs;
+using Cfo.Cats.Application.Features.Participants.Queries;
+using Cfo.Cats.Application.Features.QualityAssurance.Commands;
+using Cfo.Cats.Application.Features.QualityAssurance.DTOs;
+using Cfo.Cats.Application.Features.QualityAssurance.Queries;
+using Cfo.Cats.Server.UI.Pages.QA.Enrolments.Components;
+using IResult = Cfo.Cats.Application.Common.Interfaces.IResult;
+
+namespace Cfo.Cats.Server.UI.Pages.QA.Enrolments;
+
+public partial class Escalation
+{
+    private QaExternalMessageWarning? warningMessage;
+    private MudForm? _form;
+    private EnrolmentQueueEntryDto? _queueEntry;
+    private ParticipantDto? _participantDto;
+
+    bool saving = false;
+    [Parameter] public Guid Id { get; set; }
+
+    [CascadingParameter] public UserProfile? UserProfile { get; set; }
+
+    private SubmitEscalationResponse.Command Command { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (_participantDto is null)
+        {
+            var result = await GetNewMediator().Send(new GetEscalationEntryById.Query
+            {
+                Id = Id,
+                CurrentUser = UserProfile
+            });
+
+            if (result.Succeeded)
+            {
+                _queueEntry = result.Data!;
+
+                _participantDto = await GetNewMediator().Send(new GetParticipantById.Query
+                {
+                    Id = _queueEntry.ParticipantId
+                });
+
+                Command = new SubmitEscalationResponse.Command
+                {
+                    QueueEntryId = Id,
+                    CurrentUser = UserProfile
+                };
+            }
+
+            StateHasChanged();
+        }
+    }
+
+    protected async Task SubmitToQa()
+    {
+        try
+        {
+            saving = true;
+            await _form!.Validate().ConfigureAwait(false);
+            if (_form.IsValid is false)
+            {
+                return;
+            }
+
+            bool submit = true;
+
+            if (Command is { IsMessageExternal: true, Message.Length: > 0 })
+            {
+                submit = await warningMessage!.ShowAsync();
+            }
+
+            if (submit)
+            {
+                var result = await GetNewMediator().Send(Command);
+
+                var message = Command.Response switch
+                {
+                    SubmitEscalationResponse.EscalationResponse.Accept => "Participant accepted",
+                    SubmitEscalationResponse.EscalationResponse.Return => "Participant returned to PQA",
+                    _ => "Comment added"
+                };
+
+
+                if (result.Succeeded)
+                {
+                    Snackbar.Add(message, Severity.Info);
+                    Navigation.NavigateTo("/pages/qa/servicedesk/enrolments");
+                }
+                else
+                {
+                    ShowActionFailure("Failed to return to submit", result);
+                }
+            }
+        }
+        finally { saving = false; }
+    }
+
+    private void ShowActionFailure(string title, IResult result)
+    {
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+        sb.Append("<div>");
+        sb.Append($"<h2>{title}</h2>");
+        sb.Append("<ul>");
+
+        foreach (var e in result.Errors)
+        {
+            sb.Append($"<li>{e}</li>");
+        }
+
+        sb.Append("</ul>");
+        sb.Append("</div>");
+
+        Snackbar.Add(sb.ToString(), Severity.Error, options =>
+        {
+            options.RequireInteraction = true;
+            options.SnackbarVariant = Variant.Text;
+        });
+    }
+
+    private int characterCount => Command.Message?.Length ?? 0;
+
+}
