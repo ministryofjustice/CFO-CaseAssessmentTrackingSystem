b1c5f9fcd9c2eb551389db3b8fcb1286aa3fa974
PaulCooperWorkJustice <paul.cooper5@justice.gov.uk>
2025-05-22 13:50:53 +0100

    CFODEV-1214:  added to participant page with different options to see which works best.  Also moved page into component.

---
+++ src/Server.UI/Pages/Participants/Components/Histories/ParticipantActiveHistory.razor.cs
@@ -0,0 +1,31 @@
+using Cfo.Cats.Application.Features.Participants.DTOs;
+using Cfo.Cats.Application.Features.Participants.Queries;
+
+namespace Cfo.Cats.Server.UI.Pages.Participants.Components.Histories;
+
+public partial class ParticipantActiveHistory
+{
+    public IEnumerable<ParticipantEnrolmentHistoryDto> ParticipantEnrolmentHistory { get; set; } = Enumerable.Empty<ParticipantEnrolmentHistoryDto>();
+
+    [Parameter, EditorRequired]
+    public required string ParticipantId { get; set; } = string.Empty;
+    
+    protected override async Task OnInitializedAsync()
+    {
+        await Refresh();
+        await base.OnInitializedAsync();
+    }
+
+    async Task Refresh()
+    {
+        if (!string.IsNullOrEmpty(ParticipantId))
+        {
+            var query = new GetParticipantEnrolmentHistory.Query()
+            {
+                ParticipantId = ParticipantId
+            };
+
+            ParticipantEnrolmentHistory = await GetNewMediator().Send(query);
+        }
+    }
+}
