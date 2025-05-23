c6e833443f348eae2298357eb5d6d76c8b6e5e1d
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2025-05-23 15:22:31 +0100

    Code review amends
    Co-authored-by: Sam Gibson <140488216+samgibsonmoj@users.noreply.github.com>
    Update AssessmentHistory.razor.cs
    
    Co-authored-by: Sam Gibson <140488216+samgibsonmoj@users.noreply.github.com>
    Update AssessmentHistory.razor
    
    Co-authored-by: Sam Gibson <140488216+samgibsonmoj@users.noreply.github.com>

--- src/Server.UI/Pages/Assessment/AssessmentComponents/AssessmentHistory.razor.cs
+++ src/Server.UI/Pages/Assessment/AssessmentComponents/AssessmentHistory.razor.cs
@@ -9,38 +9,5 @@ public partial class AssessmentHistory
     [Parameter, EditorRequired]
     public DateTime? ConsentDate { get; set; }
 
-    private string GetSinceConsentDate(DateTime? assessmentCompletedDate)
-    {
-        if (ConsentDate.HasValue && assessmentCompletedDate.HasValue)
-        {
-            DateTime startDate = ConsentDate.Value;
-            DateTime endDate = assessmentCompletedDate.Value;
-
-            int years = endDate.Year - startDate.Year;
-            int months = endDate.Month - startDate.Month;
-            int days = endDate.Day - startDate.Day;
-
-            // Adjust for negative values
-            if (days < 0)
-            {
-                months--;
-                DateTime previousMonth = endDate.AddMonths(-1);
-                days += DateTime.DaysInMonth(previousMonth.Year, previousMonth.Month);
-            }
-            if (months < 0)
-            {
-                years--;
-                months += 12;
-            }
-
-            return string.Join(", ", new[]
-            {
-                years > 0 ? $"{years} years" : null,
-                months > 0 || years > 0 ? $"{months} months" : null,
-                days > 0 ? $"{days} days" : null
-            }.Where(s => !string.IsNullOrEmpty(s)));
-        }
-        return string.Empty;
-    }
 
 }
