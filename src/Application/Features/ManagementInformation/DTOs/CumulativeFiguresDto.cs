e69fdb6f615000aa2e49f4cc2a347d9b1435a53d
Carl Sixsmith <carl@sixsmith.net>
2025-05-30 13:51:58 +0100

    WIP cumulatives

---
+++ src/Application/Features/ManagementInformation/DTOs/CumulativeFiguresDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Cfo.Cats.Application.Features.ManagementInformation.DTOs;
+
+public record CumulativeFiguresDto(
+    string contract_id,
+    string description,
+    int custody_enrolments,
+    int community_enrolments,
+    int wing_inductions,
+    int hub_inductions,
+    int prerelease_support,
+    int ttg,
+    int support_work,
+    int human_citizenship,
+    int community_and_social,
+    int isws,
+    int employment,
+    int education
+);
