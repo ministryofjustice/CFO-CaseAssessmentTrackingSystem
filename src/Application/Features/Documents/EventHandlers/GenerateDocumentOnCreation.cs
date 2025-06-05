e15535fcb895bc119519d7e1a3406deaabcd14bd
samgibsonmoj <sam.gibson@justice.gov.uk>
2025-06-05 17:11:45 +0100

    Consistent integration event + document templates + keyvalues export

---
+++ src/Application/Features/Documents/EventHandlers/GenerateDocumentOnCreation.cs
@@ -0,0 +1,20 @@
+﻿using Cfo.Cats.Application.Features.Documents.IntegrationEvents;
+using Cfo.Cats.Application.Outbox;
+using Cfo.Cats.Domain.Events;
+
+namespace Cfo.Cats.Application.Features.Documents.EventHandlers;
+
+public class GenerateDocumentOnCreation(IUnitOfWork unitOfWork) : INotificationHandler<GeneratedDocumentCreatedDomainEvent>
+{
+    public async Task Handle(GeneratedDocumentCreatedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        var document = notification.Entity;
+
+        await unitOfWork.DbContext.InsertOutboxMessage(new ExportDocumentIntegrationEvent(
+            document.Id,
+            document.Template.Name, 
+            document.CreatedBy!, 
+            document.TenantId!, 
+            document.SearchCriteriaUsed));
+    }
+}
