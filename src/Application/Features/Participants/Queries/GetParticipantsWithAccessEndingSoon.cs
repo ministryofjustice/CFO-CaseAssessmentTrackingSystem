63ca6053d67331b81a6701a5ce5d4ee39b5b3926
Carl Sixsmith <carl@sixsmith.net>
2025-06-05 09:41:13 +0100

    Clean up code

--- src/Application/Features/Participants/Queries/GetParticipantsWithAccessEndingSoon.cs
+++ src/Application/Features/Participants/Queries/GetParticipantsWithAccessEndingSoon.cs
@@ -1,6 +1,4 @@
-using Cfo.Cats.Application.Common.Interfaces.Locations;
 using Cfo.Cats.Application.Common.Security;
-using Cfo.Cats.Application.Features.Participants.DTOs;
 using Cfo.Cats.Application.SecurityConstants;
 
 namespace Cfo.Cats.Application.Features.Participants.Queries;
@@ -11,7 +9,7 @@ public static class GetParticipantsWithAccessEndingSoon
     public class Query : IRequest<IEnumerable<ParticipantWithAccessEndingSoonDto>>
     {
         /// <summary>
-        /// Participant Owner. Current user will be used if not provided.
+        ///     Participant Owner. Current user will be used if not provided.
         /// </summary>
         public string? OwnerId { get; set; }
     }
@@ -25,68 +23,62 @@ public static class GetParticipantsWithAccessEndingSoon
         public DateTime HasAccessTo => LostAccessOn.AddDays(90);
     }
 
-    class Handler(
-        IUnitOfWork unitOfWork, 
+    private class Handler(
+        IUnitOfWork unitOfWork,
         ICurrentUserService currentUser
-        ) : IRequestHandler<Query, IEnumerable<ParticipantWithAccessEndingSoonDto>>
+    ) : IRequestHandler<Query, IEnumerable<ParticipantWithAccessEndingSoonDto>>
     {
         public async Task<IEnumerable<ParticipantWithAccessEndingSoonDto>> Handle(Query request, CancellationToken cancellationToken)
         {
             var lastNinetyDays = DateTime.UtcNow.AddDays(-90);
-            string ownerId = request.OwnerId ?? currentUser.UserId!;
+            var ownerId = request.OwnerId ?? currentUser.UserId!;
 
             FormattableString sql = $"""
-            SELECT 
-                [Id] as ParticipantId, 
-                [To] as [LostAccessOn], 
-                CONCAT(Participant.FirstName, ' ', Participant.LastName) as [FullName]  
-            FROM 
-                [Participant].[Participant]	
-            INNER JOIN
-            (
-                -- They were assigned to me, but unassigned within the last x days
-                SELECT 
-                    [oh].[ParticipantId], 
-                    MAX([oh].[To]) as [To] 
-                FROM 
-                    [Participant].[OwnershipHistory] as [oh]
-                WHERE 
-                    [oh].[OwnerId] = {ownerId} and [oh].[To] > {lastNinetyDays}
-                GROUP BY [oh].[ParticipantId] 
-                    ) AS [LostAccessTo] on Participant.Id = LostAccessTo.ParticipantId
-            WHERE
-                -- who is not currently owned by me
-                COALESCE( OwnerId, '' ) <> {ownerId}
-                -- AND IS NOT IN AN AREA I CAN SEE
-                AND CurrentLocationId NOT IN (
-                    SELECT 
-                        tl.LocationId 
-                    FROM 
-                        [Identity].[User] as [u]
-                    INNER JOIN
-                        [Configuration].TenantLocation as [tl]
-                            on [u].TenantId = [tl].TenantId
-                    WHERE 
-                        [u].Id = {ownerId}
-                );
-            """;
-                
-            
+                                     SELECT 
+                                         [Id] as ParticipantId, 
+                                         [To] as [LostAccessOn], 
+                                         CONCAT(Participant.FirstName, ' ', Participant.LastName) as [FullName]  
+                                     FROM 
+                                         [Participant].[Participant]	
+                                     INNER JOIN
+                                     (
+                                         -- They were assigned to me, but unassigned within the last x days
+                                         SELECT 
+                                             [oh].[ParticipantId], 
+                                             MAX([oh].[To]) as [To] 
+                                         FROM 
+                                             [Participant].[OwnershipHistory] as [oh]
+                                         WHERE 
+                                             [oh].[OwnerId] = {ownerId} and [oh].[To] > {lastNinetyDays}
+                                         GROUP BY [oh].[ParticipantId] 
+                                             ) AS [LostAccessTo] on Participant.Id = LostAccessTo.ParticipantId
+                                     WHERE
+                                         -- who is not currently owned by me
+                                         COALESCE( OwnerId, '' ) <> {ownerId}
+                                         -- AND IS NOT IN AN AREA I CAN SEE
+                                         AND CurrentLocationId NOT IN (
+                                             SELECT 
+                                                 tl.LocationId 
+                                             FROM 
+                                                 [Identity].[User] as [u]
+                                             INNER JOIN
+                                                 [Configuration].TenantLocation as [tl]
+                                                     on [u].TenantId = [tl].TenantId
+                                             WHERE 
+                                                 [u].Id = {ownerId}
+                                         );
+                                     """;
+
+
             var results = await unitOfWork.DbContext.Database
                 .SqlQuery<ParticipantWithAccessEndingSoonDto>(sql)
                 .ToArrayAsync(cancellationToken);
 
             return results;
         }
-
-     
     }
 
     public class Validator : AbstractValidator<Query>
     {
-        public Validator()
-        {
-        }
-
     }
 }
