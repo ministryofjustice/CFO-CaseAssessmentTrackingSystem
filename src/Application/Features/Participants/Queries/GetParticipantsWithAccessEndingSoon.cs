5b0e45975adc0254c71ba9922aed942ad1a866a7
Carl Sixsmith <carl@sixsmith.net>
2025-06-05 09:38:45 +0100

    Optomize the moved participants screen

--- src/Application/Features/Participants/Queries/GetParticipantsWithAccessEndingSoon.cs
+++ src/Application/Features/Participants/Queries/GetParticipantsWithAccessEndingSoon.cs
@@ -19,93 +19,67 @@ public static class GetParticipantsWithAccessEndingSoon
     // Todo: move this dto
     public record ParticipantWithAccessEndingSoonDto
     {
+        public required string ParticipantId { get; set; }
+        public required string FullName { get; set; }
         public required DateTime LostAccessOn { get; set; }
         public DateTime HasAccessTo => LostAccessOn.AddDays(90);
-        public required ParticipantDto Participant { get; set; }
     }
 
     class Handler(
         IUnitOfWork unitOfWork, 
-        ICurrentUserService currentUser,
-        ILocationService locationService,
-        IMapper mapper) : IRequestHandler<Query, IEnumerable<ParticipantWithAccessEndingSoonDto>>
+        ICurrentUserService currentUser
+        ) : IRequestHandler<Query, IEnumerable<ParticipantWithAccessEndingSoonDto>>
     {
         public async Task<IEnumerable<ParticipantWithAccessEndingSoonDto>> Handle(Query request, CancellationToken cancellationToken)
         {
             var lastNinetyDays = DateTime.UtcNow.AddDays(-90);
             string ownerId = request.OwnerId ?? currentUser.UserId!;
 
-            var participantsWhoLeftOwnershipInLastNinetyDays = await GetParticipantsWhoLeftOwnershipInPeriod(ownerId, lastNinetyDays, cancellationToken);
+            FormattableString sql = $"""
+            SELECT 
+                [Id] as ParticipantId, 
+                [To] as [LostAccessOn], 
+                CONCAT(Participant.FirstName, ' ', Participant.LastName) as [FullName]  
+            FROM 
+                [Participant].[Participant]	
+            INNER JOIN
+            (
+                -- They were assigned to me, but unassigned within the last x days
+                SELECT 
+                    [oh].[ParticipantId], 
+                    MAX([oh].[To]) as [To] 
+                FROM 
+                    [Participant].[OwnershipHistory] as [oh]
+                WHERE 
+                    [oh].[OwnerId] = {ownerId} and [oh].[To] > {lastNinetyDays}
+                GROUP BY [oh].[ParticipantId] 
+                    ) AS [LostAccessTo] on Participant.Id = LostAccessTo.ParticipantId
+            WHERE
+                -- who is not currently owned by me
+                COALESCE( OwnerId, '' ) <> {ownerId}
+                -- AND IS NOT IN AN AREA I CAN SEE
+                AND CurrentLocationId NOT IN (
+                    SELECT 
+                        tl.LocationId 
+                    FROM 
+                        [Identity].[User] as [u]
+                    INNER JOIN
+                        [Configuration].TenantLocation as [tl]
+                            on [u].TenantId = [tl].TenantId
+                    WHERE 
+                        [u].Id = {ownerId}
+                );
+            """;
+                
+            
+            var results = await unitOfWork.DbContext.Database
+                .SqlQuery<ParticipantWithAccessEndingSoonDto>(sql)
+                .ToArrayAsync(cancellationToken);
 
-            // Has the owner ever worked with anyone?
-            if (participantsWhoLeftOwnershipInLastNinetyDays.Any() is false)
-            {
-                return [];
-            }
-
-            // Retrieve a complete history of participant ownership (includes the other owners)
-            var completeParticipantsOwnerHistory = await unitOfWork.DbContext.ParticipantOwnershipHistories
-                .Where(h => participantsWhoLeftOwnershipInLastNinetyDays.Contains(h.ParticipantId))
-                .GroupBy(h => h.ParticipantId)
-                .Select(g => new
-                {
-                    ParticipantId = g.Key,
-                    OwnershipHistory = g.OrderByDescending(h => h.From).ToList()
-                }).ToListAsync(cancellationToken);
-
-            // Work out which participants have been unassigned, and when the ownership changed
-            var participantsUnassignedInLastNinetyDays = completeParticipantsOwnerHistory
-                .Select(g => new
-                {
-                    g.ParticipantId,
-                    CurrentOwnerId = g.OwnershipHistory.First().OwnerId,
-                    PreviousOwnerHasAccessTo = g.OwnershipHistory.First(h => h.OwnerId == ownerId).To
-                })
-                .Where(p => p.CurrentOwnerId != ownerId)
-                .ToDictionary(p => p.ParticipantId);
-
-            if (participantsUnassignedInLastNinetyDays.Any() is false)
-            {
-                return [];
-            }
-
-            // At this point, our owner has been unassigned at some point within the last 90 days.
-            // However, they may still have access to the participants' current location.
-            var locations = await GetAccessibleLocations(ownerId, cancellationToken);
-
-            var participantsWithAccessEndingSoon = await unitOfWork.DbContext.Participants
-                .Where(p => participantsUnassignedInLastNinetyDays.Select(x => x.Key).Contains(p.Id))
-                .Where(p => locations.Contains(p.CurrentLocation.Id) == false)
-                .ProjectTo<ParticipantDto>(mapper.ConfigurationProvider)
-                .ToListAsync(cancellationToken);
-
-            // Translate data
-            return participantsWithAccessEndingSoon.Select(participant => new ParticipantWithAccessEndingSoonDto
-            {
-                Participant = participant, 
-                LostAccessOn = participantsUnassignedInLastNinetyDays[participant.Id].PreviousOwnerHasAccessTo!.Value
-            });
+            return results;
         }
 
-        async Task<List<string>> GetParticipantsWhoLeftOwnershipInPeriod(string ownerId, DateTime backdatePeriod, CancellationToken cancellationToken)
-        {
-            var participants = await unitOfWork.DbContext.ParticipantOwnershipHistories
-                .Where(h => h.OwnerId == ownerId && h.To >= backdatePeriod)
-                .GroupBy(h => h.ParticipantId)
-                .Select(h => h.Key)
-                .ToListAsync(cancellationToken);
-
-            return participants;
-        }
-
-        async Task<IEnumerable<int>> GetAccessibleLocations(string ownerId, CancellationToken cancellationToken)
-        {
-            var owner = await unitOfWork.DbContext.Users
-                .FirstAsync(u => u.Id == ownerId, cancellationToken);
-
-            return locationService.GetVisibleLocations(owner.TenantId!)
-                .Select(l => l.Id);
-        }
+     
     }
 
     public class Validator : AbstractValidator<Query>
