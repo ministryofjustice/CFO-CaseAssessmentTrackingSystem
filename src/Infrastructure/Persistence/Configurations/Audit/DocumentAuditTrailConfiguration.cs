81cdb0d74dfee63bf65c0900d6ffcb56c446e724
samgibsonmoj <sam.gibson@justice.gov.uk>
2025-06-05 15:43:00 +0100

    CFODEV-1195: Audit functionality for export

---
+++ src/Infrastructure/Persistence/Configurations/Audit/DocumentAuditTrailConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Cfo.Cats.Domain.Entities.Documents;
+using Cfo.Cats.Domain.Identity;
+using Cfo.Cats.Infrastructure.Constants.Database;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Cfo.Cats.Infrastructure.Persistence.Configurations.Audit;
+
+public class DocumentAuditTrailConfiguration : IEntityTypeConfiguration<DocumentAuditTrail>
+{
+    public void Configure(EntityTypeBuilder<DocumentAuditTrail> builder)
+    {
+        builder.ToTable(DatabaseConstants.Tables.DocumentAuditTrail, DatabaseConstants.Schemas.Audit);
+
+        builder.HasKey(a => a.Id);
+        
+        builder.HasOne<ApplicationUser>()
+            .WithMany()
+            .HasForeignKey(a => a.UserId);
+
+        builder.HasOne<GeneratedDocument>()
+            .WithMany()
+            .HasForeignKey(a => a.DocumentId);
+
+        builder.Property(a => a.RequestType)
+            .IsRequired()
+            .HasMaxLength(300);
+
+        builder.Property(a => a.OccurredOn)
+            .IsRequired();
+    }
+}
---
+++ src/Infrastructure/Persistence/Configurations/Documents/GeneratedDocumentEntityTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Cfo.Cats.Domain.Entities.Documents;
+using Cfo.Cats.Infrastructure.Constants.Database;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Cfo.Cats.Infrastructure.Persistence.Configurations.Documents;
+
+public class GeneratedDocumentEntityTypeConfiguration : IEntityTypeConfiguration<GeneratedDocument>
+{
+    public void Configure(EntityTypeBuilder<GeneratedDocument> builder)
+    {
+        builder.ToTable(DatabaseConstants.Tables.GeneratedDocument,
+                DatabaseConstants.Schemas.Document);
+
+        builder.Property(d => d.ExpiresOn).IsRequired();
+        builder.Property(d => d.Status).HasConversion<string>();
+    }
+}
