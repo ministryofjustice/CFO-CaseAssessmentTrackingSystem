8b6493ed6a1f8526dcce76fd2701945a4aedab7b
samgibsonmoj <sam.gibson@justice.gov.uk>
2025-06-09 13:44:55 +0100

    Document Audit Trails screen (incl pagination, query, validators)

---
+++ src/Application/Features/AuditTrails/Caching/SystemAuditTrailsCacheKey.cs
@@ -0,0 +1,38 @@
+﻿namespace Cfo.Cats.Application.Features.AuditTrails.Caching;
+
+public static class SystemAuditTrailsCacheKey
+{
+    public const string GetAllCacheKey = "all-audittrails";
+    private static readonly TimeSpan RefreshInterval = TimeSpan.FromMilliseconds(30);
+    private static CancellationTokenSource tokenSource;
+
+    static SystemAuditTrailsCacheKey()
+    {
+        tokenSource = new CancellationTokenSource(RefreshInterval);
+    }
+
+    public static MemoryCacheEntryOptions MemoryCacheEntryOptions =>
+        new MemoryCacheEntryOptions().AddExpirationToken(
+            new CancellationChangeToken(SharedExpiryTokenSource().Token)
+        );
+
+    public static string GetPaginationCacheKey(string parameters)
+    {
+        return $"AuditTrailsWithPaginationQuery,{parameters}";
+    }
+
+    public static CancellationTokenSource SharedExpiryTokenSource()
+    {
+        if (tokenSource.IsCancellationRequested)
+        {
+            tokenSource = new CancellationTokenSource(RefreshInterval);
+        }
+
+        return tokenSource;
+    }
+
+    public static void Refresh()
+    {
+        SharedExpiryTokenSource().Cancel();
+    }
+}
---
+++ src/Application/Features/AuditTrails/Specifications/DocumentAuditTrail/SystemAuditTrailAdvancedFilter.cs
@@ -0,0 +1,11 @@
+﻿using Cfo.Cats.Application.Common.Security;
+using Cfo.Cats.Domain.Entities.Documents;
+
+namespace Cfo.Cats.Application.Features.AuditTrails.Specifications.DocumentAuditTrail;
+
+public class DocumentAuditTrailAdvancedFilter : PaginationFilter
+{
+    public DocumentAuditTrailRequestType? RequestType { get; set; }
+    public UserProfile? CurrentUser { get; set; }
+    public DocumentAuditTrailListView ListView { get; set; } = DocumentAuditTrailListView.All;
+}
