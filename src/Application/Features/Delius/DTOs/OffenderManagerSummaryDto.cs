bab49cb32cfebb51ce9c20a4900a973f95f1a848
VS <vinay.saini@justice.gov.uk>
2025-06-10 16:31:34 +0100

    Added menu item to view Organisation and Team for a participant in a dialog, it makes an api call to dms to retrieve the required info

---
+++ src/Application/Features/Delius/DTOs/OffenderManagerSummaryDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cfo.Cats.Application.Features.Delius.DTOs;
+
+public class OffenderManagerSummaryDto
+{
+    public string? OrganisationCode { get; set; } = default;
+    public string? OrganistationDescription { get; set; } = default;
+    public string? TeamCode { get; set; } = default;
+    public string? TeamDescription { get; set; } = default;
+
+}
