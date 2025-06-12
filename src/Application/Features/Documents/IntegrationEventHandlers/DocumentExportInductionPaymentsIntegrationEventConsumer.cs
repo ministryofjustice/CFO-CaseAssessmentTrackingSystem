671005435afb8bd6420a1d61d01b146b674c4dd5
samgibsonmoj <sam.gibson@justice.gov.uk>
2025-06-12 15:45:37 +0100

    WIP - payment integration event consumers

---
+++ src/Application/Features/Documents/IntegrationEventHandlers/DocumentExportInductionPaymentsIntegrationEventConsumer.cs
@@ -0,0 +1,69 @@
+﻿using Cfo.Cats.Application.Features.Documents.IntegrationEvents;
+using Cfo.Cats.Application.Features.ManagementInformation;
+using Cfo.Cats.Application.Features.Payments.DTOs;
+using Cfo.Cats.Application.Features.Payments.Queries;
+using Cfo.Cats.Domain.Entities.Documents;
+using MassTransit;
+using Newtonsoft.Json;
+
+namespace Cfo.Cats.Application.Features.Documents.IntegrationEventHandlers;
+
+public class DocumentExportInductionPaymentsIntegrationEventConsumer(
+    IUnitOfWork unitOfWork,
+    IExcelService excelService,
+    IUploadService uploadService,
+    IDomainEventDispatcher domainEventDispatcher,
+    ITargetsProvider targetsProvider) : IConsumer<ExportDocumentIntegrationEvent>
+{
+    public async Task Consume(ConsumeContext<ExportDocumentIntegrationEvent> context)
+    {
+        if (context.Message.Key != DocumentTemplate.InductionPayments.Name)
+        {
+            return;
+        }
+
+        var document = await unitOfWork.DbContext.GeneratedDocuments.FindAsync(context.Message.DocumentId);
+
+        if (document is null)
+        {
+            return;
+        }
+
+        try
+        {
+            var request = JsonConvert.DeserializeObject<GetInductionPayments.Query>(context.Message.SearchCriteria!)
+                ?? throw new Exception();
+
+            // Hack: call handler directly (skips Authorization pipeline, as we're outside of the HttpContext).
+            var data = await new GetInductionPayments.Handler(unitOfWork, targetsProvider).Handle(request!, CancellationToken.None);
+
+            if (data is not { Succeeded: true })
+            {
+                throw new Exception(data.ErrorMessage);
+            }
+
+            var results = await excelService.ExportAsync(data.Data?.Items ?? [],
+                new Dictionary<string, Func<InductionPaymentDto, object?>>
+                {
+                    { "Participant Id", item => item.ParticipantId }
+                }
+            );
+
+            var uploadRequest = new UploadRequest(document.Title!, UploadType.Document, results);
+
+            var result = await uploadService.UploadAsync($"MyDocuments/{context.Message.UserId}", uploadRequest);
+
+            document
+                .WithStatus(DocumentStatus.Available)
+                .SetURL(result);
+
+            await domainEventDispatcher.DispatchEventsAsync(unitOfWork.DbContext, CancellationToken.None);
+            await unitOfWork.CommitTransactionAsync();
+        }
+        catch
+        {
+            document.WithStatus(DocumentStatus.Error);
+            await unitOfWork.CommitTransactionAsync();
+        }
+    }
+}
