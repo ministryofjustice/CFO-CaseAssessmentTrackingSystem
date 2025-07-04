d667311e5732a43f5d613da3e2696d1c46ab95d5
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2025-07-04 09:34:09 +0100

    Move code to code behind on dashboard components. (#656)

---
+++ src/Server.UI/Pages/Dashboard/Components/LoadingCard.razor.cs
@@ -0,0 +1,7 @@
+﻿namespace Cfo.Cats.Server.UI.Pages.Dashboard.Components;
+
+public partial class LoadingCard
+{
+    [Parameter, EditorRequired] 
+    public string Title { get; set; } = default!;
+}
