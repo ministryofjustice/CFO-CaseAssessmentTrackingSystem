468f101793af8a65ce2e1b3fe94120c734a38350
Sam Gibson <140488216+samgibsonmoj@users.noreply.github.com>
2025-07-11 14:10:11 +0100

    CFODEV-1270: Visibility of participant sample for quality checks (mock pages) (#662)

---
+++ src/Server.UI/Pages/Analytics/DipSampling.razor.cs
@@ -0,0 +1,19 @@
+﻿using Cfo.Cats.Application.Common.Security;
+using Cfo.Cats.Application.Features.Contracts.DTOs;
+
+namespace Cfo.Cats.Server.UI.Pages.Analytics;
+
+public partial class DipSampling
+{
+    [CascadingParameter] public UserProfile? CurrentUser { get; set; }
+
+    public int Month { get; set; } = DateTime.Now.AddMonths(-4).Month;
+    public int Year { get; set; } = DateTime.Now.AddMonths(-4).Year;
+    public ContractDto? SelectedContract { get; set; }
+
+    protected override Task OnInitializedAsync()
+    {
+        return base.OnInitializedAsync();
+    }
+
+}
