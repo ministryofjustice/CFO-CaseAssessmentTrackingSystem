8e8af8b3cd808b77dc6610e43b9b3790e4283d20
PaulCooperWorkJustice <paul.cooper5@justice.gov.uk>
2025-07-14 09:25:55 +0100

    This is changes requested by Sam

--- src/Application/Features/Participants/Queries/GetParticipantLocationHistory.cs
+++ src/Application/Features/Participants/Queries/GetParticipantLocationHistory.cs
@@ -16,85 +16,44 @@ public static class GetParticipantLocationHistory
 
     class Handler(IUnitOfWork unitOfWork) : IRequestHandler<Query, IEnumerable<ParticipantLocationHistoryDto>>
     {
-        //Hide CFO Users details from providers
-        private static readonly string[] HiddenTenantIds = ["1.1.", "1."];
-        private static readonly string[] AllowedRoles =
-        [
-            RoleNames.QAOfficer,
-            RoleNames.QASupportManager,
-            RoleNames.QAManager,
-            RoleNames.SMT,
-            RoleNames.SystemSupport
-        ];
-
         public async Task<IEnumerable<ParticipantLocationHistoryDto>> Handle(Query request, CancellationToken cancellationToken)
         {
             var db = unitOfWork.DbContext;
 
-            var participantId = request.ParticipantId;
-
-            // Get enrolment location first
-            var enrolmentLocationQuery =
-                from p in db.Participants
-                join l in db.Locations on p.EnrolmentLocation!.Id equals l.Id
-                join u in db.Users on p.CreatedBy equals u.Id
-                where p.Id == participantId
-                select new ParticipantLocationHistoryDto
-                {
-                    Id = 0, 
-                    MoveDate = p.Created, 
-                    LocationName = l.Name + " (Enrolment Location)",
-                    SupportWorker = u!.DisplayName!,
-                    SupportWorkerTenantId = u!.TenantId,
-                    IsEnrolment = true
-                };
-
-            // Then get Location history
-            var locationHistoryQuery =
+            var query = 
                 from plh in db.ParticipantLocationHistories
                 join u in db.Users on plh.CreatedBy equals u.Id
                 join l in db.Locations on plh.LocationId equals l.Id
-                where plh.ParticipantId == participantId
+                where plh.ParticipantId == request.ParticipantId
                 select new ParticipantLocationHistoryDto
                 {
                     Id = plh.Id,
                     MoveDate = plh.From,
-                    LocationName = l.Name,
-                    SupportWorker = u.DisplayName!,
-                    SupportWorkerTenantId = u.TenantId,
-                    IsEnrolment = false
+                    LocationName = l.Name
                 };
-                    
-            var results = await enrolmentLocationQuery
-                .Union(locationHistoryQuery)
-                .OrderBy(x => x.IsEnrolment ? 0 : 1)
-                .ThenBy(x => x.MoveDate ?? DateTime.MinValue)
-                .ToListAsync();
 
-            if (results.Count == 0)
-                return results;
+            var locationHistories = await query
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+            
+            if (locationHistories.Any() is false)
+                return locationHistories;
 
-            bool hideUser = !request.CurrentUser.AssignedRoles.Any(role => AllowedRoles.Contains(role));
+            var last = locationHistories.Last();
+                        
+            for (int i = 0; i < locationHistories.Count; i++)
+            {            
+                var current = locationHistories[i];
 
-            for (int i = 0; i < results.Count; i++)
-            {
-                // Mask support worker if needed
-                if (hideUser && HiddenTenantIds.Contains(results[i].SupportWorkerTenantId))
-                {
-                    results[i].SupportWorker = "CFO User";
-                }
-
-                var current = results[i];
-                var next = i == results.Count - 1
+                var next = current == last
                     ? DateTime.UtcNow
-                    : results[i + 1].MoveDate;
+                    : locationHistories[i + 1].MoveDate;
 
-                current.DaysSincePrevious = (current.MoveDate.HasValue && next.HasValue)
-                    ? (next.Value - current.MoveDate.Value).Days
-                    : 0;
+                var diff = (next - current.MoveDate) ?? TimeSpan.Zero;
+                current.DaysSincePrevious = diff.Days;
             }
 
-            return results;
+            return locationHistories;
         }
     }
 
