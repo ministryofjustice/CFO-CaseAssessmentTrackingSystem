0cf9debd688e8c89aac949ef9dd612d6ed618eef
samgibsonmoj <sam.gibson@justice.gov.uk>
2025-07-17 13:39:07 +0100

    Configurable dip sample sizes

---
+++ src/Infrastructure/Configurations/OutcomeQualityDipSampleSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace Cfo.Cats.Infrastructure.Configurations;
+
+public class OutcomeQualityDipSampleSettings
+{
+    public const string Key = nameof(OutcomeQualityDipSampleSettings);
+
+    public int Wing { get; set; } = 10;
+    public int Hub { get; set; } = 10;
+    public int Satellite { get; set; } = 5;
+    public int Community { get; set; } = 10;
+    public int WiderCustody { get; set; } = 10;
+}
