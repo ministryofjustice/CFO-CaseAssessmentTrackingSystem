799d83957def9e654e9ae51522ec714f137827f1
PaulCooperWorkJustice <paul.cooper5@justice.gov.uk>
2025-07-17 10:24:18 +0100

    CFODEV-1015:  Add audit for disabling dormant accounts.

--- src/Infrastructure/Jobs/DisableDormantAccountsJob.cs
+++ src/Infrastructure/Jobs/DisableDormantAccountsJob.cs
@@ -1,47 +1,74 @@
 ﻿using Cfo.Cats.Domain.Identity;
+using FluentValidation;
 using Quartz;
 using System.Linq.Dynamic.Core;
 
 namespace Cfo.Cats.Infrastructure.Jobs;
 
 public class DisableDormantAccountsJob(
-    ILogger<DisableDormantAccountsJob> logger,
-    UserManager<ApplicationUser> userManager) : IJob
+                ILogger<DisableDormantAccountsJob> logger,
+                UserManager<ApplicationUser> userManager,
+                IUnitOfWork unitOfWork
+                ) : IJob
 {
     public static readonly JobKey Key = new JobKey(name: nameof(DisableDormantAccountsJob));
     public static readonly string Description = "A job to deactivate accounts that have not logged in within the last 30 days.";
 
     public async Task Execute(IJobExecutionContext context)
     {
+
         using (logger.BeginScope(Key))
 
-        if (context.RefireCount > 3)
-        {
-            logger.LogWarning($"Failed to complete within 3 tries, aborting...");
-            return;
-        }
+            if (context.RefireCount > 3)
+            {
+                logger.LogWarning($"Failed to complete Disable Dormant Accounts Job within 3 tries, aborting...");
+                return;
+            }
 
         try
         {
-            logger.LogInformation("Deactivating dormant accounts");
+            logger.LogInformation("Starting deactivation of dormant accounts");
 
             DateTime thirtyDaysAgo = DateTime.Now.AddDays(-30);
 
-            var query = userManager.Users
+            var usersToDeactivate = userManager.Users
                 .IgnoreAutoIncludes()
                 .Where(user => user.IsActive)
-                .Where(user => user.LastLogin < thirtyDaysAgo
-                    || (user.LastLogin == null && user.Created < thirtyDaysAgo));
+                .Where(user => user.LastLogin < thirtyDaysAgo ||
+                               (user.LastLogin == null && user.Created < thirtyDaysAgo));
 
-            int noOfAffectedUsers = await query.ExecuteUpdateAsync(
-                p => p.SetProperty(user => user.IsActive, false));
+            if (usersToDeactivate.Any())
+            {
+                foreach (var user in usersToDeactivate)
+                {
+                    await DeactivateUser(user, context.CancellationToken);
+                }
 
-            logger.LogInformation($"Deactivated {noOfAffectedUsers} account(s)");
+                int noOfAffectedUsers = await usersToDeactivate.ExecuteUpdateAsync(
+                    p => p.SetProperty(user => user.IsActive, false), context.CancellationToken);
+
+                logger.LogInformation($"Deactivated/Disabled {noOfAffectedUsers} account(s)");
+            }
+            else
+            {
+                logger.LogInformation($"No Accounts Disabled");
+            }
         }
         catch (Exception ex)
         {
-            throw new JobExecutionException(msg: $"An unexpected error occurred executing job", refireImmediately: true, cause: ex);
+            throw new JobExecutionException(msg: $"An unexpected error occurred executing Disable Dormant Accounts job", refireImmediately: true, cause: ex);
         }
+    }
 
+    private async Task DeactivateUser(ApplicationUser user, CancellationToken cancellationToken)
+    {
+        const string systemSupportEmail = "System.Support@justice.gov.uk";
+
+        IdentityAuditTrail audit = IdentityAuditTrail.Create(user.UserName, systemSupportEmail, IdentityActionType.AccountDeactivated, "Overnight System Job");
+        await unitOfWork.DbContext.IdentityAuditTrails.AddAsync(audit);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        logger.LogInformation($"Deactivated {user.UserName} account");
+        await Task.CompletedTask;
     }
 }
