3c91f69ff0f626c5b920faa351bf286076ca28ea
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2025-07-23 13:22:04 +0100

    missing files from push

---
+++ src/Server.UI/Pages/Analytics/Components/ActivitiyDipDialog.razor.cs
@@ -0,0 +1,23 @@
+﻿using Cfo.Cats.Application.Features.PerformanceManagement.DTOs;
+
+namespace Cfo.Cats.Server.UI.Pages.Analytics.Components;
+
+public partial class ActivitiyDipDialog
+{
+    [CascadingParameter] private IMudDialogInstance MudDialog { get; set; } = null!;
+
+    [Parameter, EditorRequired] public ParticipantDipSampleActivityDto Model { get; set; } = null!;
+
+    private void Ok() => MudDialog.Cancel();
+
+    private Guid? DocumentId => Model switch
+    {
+        ParticipantDipSampleEmploymentActivityDto emp => emp.DocumentId,
+        ParticipantDipSampleEducationAndTrainingActivityDto ete => ete.DocumentId,
+        ParticipantDipSampleIswActivityDto isw => isw.DocumentId,
+        _ => null
+    };
+
+    private bool DisableDocumentTab()
+        => DocumentId is null;
+}
