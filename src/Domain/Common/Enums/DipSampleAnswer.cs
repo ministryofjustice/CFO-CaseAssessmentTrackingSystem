d9e7887f00e9ff8e70b73b29f42fa9b92b4a6da8
samgibsonmoj <sam.gibson@justice.gov.uk>
2025-08-06 18:20:30 +0100

    Assigns distinct value to 'Not Applicable' for CSO answer

--- src/Domain/Common/Enums/DipSampleAnswer.cs
+++ src/Domain/Common/Enums/DipSampleAnswer.cs
@@ -9,7 +9,7 @@ public class DipSampleAnswer : SmartEnum<DipSampleAnswer>
     public static readonly DipSampleAnswer NotAnswered = new("Not Answered", 0, isAnswer: false);
     public static readonly DipSampleAnswer Yes = new("Yes", 1);
     public static readonly DipSampleAnswer No = new("No", 2);
-    public static readonly DipSampleAnswer NotApplicable = new("Not Applicable", 2);
+    public static readonly DipSampleAnswer NotApplicable = new("Not Applicable", 3);
 
     private DipSampleAnswer(string name, int value, bool isAnswer = true) : base(name, value)
     {
