5855b40ffdf9f160e07de7656deea8a975ccb529
Sam Gibson <140488216+samgibsonmoj@users.noreply.github.com>
2025-09-01 13:41:39 +0100

    CFODEV-1352: Adds a 'To' column for the participant location history (#748)
    Adds a 'To' column for the participant location history
    
    Makes querying a hell of a lot easier

--- src/Application/Features/Participants/EventHandlers/RecordLocationHistoryOnMovement.cs
+++ src/Application/Features/Participants/EventHandlers/RecordLocationHistoryOnMovement.cs
@@ -7,10 +7,19 @@ public class RecordLocationHistoryOnMovement(IUnitOfWork unitOfWork) : INotifica
 {
     public async Task Handle(ParticipantMovedDomainEvent notification, CancellationToken cancellationToken)
     {
+        var dateTime = DateTime.UtcNow;
+
+        var lastMovement = await unitOfWork.DbContext.ParticipantLocationHistories
+            .Where(h => h.ParticipantId == notification.Item.Id)
+            .OrderByDescending(h => h.From)
+            .FirstAsync(cancellationToken);
+
+        lastMovement.WithTo(dateTime);
+
         var history = ParticipantLocationHistory.Create(
             notification.Item.Id, 
-            notification.To.Id, 
-            DateTime.UtcNow);
+            notification.To.Id,
+            dateTime);
 
         await unitOfWork.DbContext.ParticipantLocationHistories.AddAsync(history, cancellationToken);
     }
