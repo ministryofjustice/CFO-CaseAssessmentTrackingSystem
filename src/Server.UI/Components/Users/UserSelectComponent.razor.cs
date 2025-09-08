f54718f38e1010c338078e420a38a4bbf339ff44
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2025-09-08 12:40:27 +0100

    This adds an inital dashboard for support worker performance. (#756)
    It includes a new lookup for selecting a user (this is re-usable in other areas)

---
+++ src/Server.UI/Components/Users/UserSelectComponent.razor.cs
@@ -0,0 +1,55 @@
+using Cfo.Cats.Application.Common.Security;
+using Cfo.Cats.Application.Features.Identity.DTOs;
+
+namespace Cfo.Cats.Server.UI.Components.Users;
+
+public partial class UserSelectComponent
+{
+    [CascadingParameter]
+    public UserProfile CurrentUser { get; set; } = null!;
+    [Parameter] public ApplicationUserDto? Value { get; set; }
+    [Parameter] public EventCallback<ApplicationUserDto?> ValueChanged { get; set; }
+    [Parameter] public string Label { get; set; } = "Select User";
+    [Parameter] public string? Placeholder { get; set; } = "Choose a user...";
+    [Parameter] public bool Required { get; set; }
+    [Parameter] public bool Disabled { get; set; }
+    [Parameter] public Variant Variant { get; set; } = MudBlazor.Variant.Outlined;
+    private ApplicationUserDto[] _users = [];
+    protected override void OnInitialized() => _users = UserService.DataSource
+        .Where(d => d.TenantId!.StartsWith(CurrentUser.TenantId!))
+        .OrderBy(u => u.DisplayName)
+        .ToArray();
+    private string GetDisplayName(ApplicationUserDto? user) => user?.DisplayName ?? string.Empty;
+    private async Task HandleValueChanged(ApplicationUserDto? value)
+    {
+        Value = value;
+        await ValueChanged.InvokeAsync(value);
+    }
+    private bool Search(ApplicationUserDto? user, string searchText)
+    {
+        if (user == null)
+        {
+            return true;
+        }
+
+        if (user.DisplayName.Contains(searchText, StringComparison.CurrentCultureIgnoreCase))
+        {
+            return true;
+        }
+
+        if(user is {  TenantName: not null, AssignedRoles: not null })
+        {
+            if (user.TenantName.Contains(searchText, StringComparison.CurrentCultureIgnoreCase))
+            {
+                return true;
+            }
+
+            if (user.AssignedRoles.Any(r => r.Contains(searchText, StringComparison.CurrentCultureIgnoreCase)))
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
---
+++ src/Server.UI/Pages/Dashboard/SupportWorkerPerformance.razor.cs
@@ -0,0 +1,27 @@
+using Cfo.Cats.Application.Common.Security;
+using Cfo.Cats.Application.Features.Identity.DTOs;
+
+namespace Cfo.Cats.Server.UI.Pages.Dashboard;
+
+public partial class SupportWorkerPerformance
+{
+    private bool _showSelect;
+    private ApplicationUserDto? SelectedUser { get; set; }
+    public string? SelectedUserId { get; set; }
+    
+    [CascadingParameter]
+    public UserProfile CurrentUser { get; set; } = null!;
+    protected override void OnInitialized()
+    {
+        _showSelect = CurrentUser.AssignedRoles is { Length: > 0 };
+
+        // if the current user has access to select, don't set the selected user.
+        SelectedUserId = _showSelect ? null : CurrentUser.UserId;
+    }
+
+    private void OnUserSelectedChanged(ApplicationUserDto? dto)
+    {
+        SelectedUser = dto;
+        SelectedUserId = dto?.Id;
+    }
+}
