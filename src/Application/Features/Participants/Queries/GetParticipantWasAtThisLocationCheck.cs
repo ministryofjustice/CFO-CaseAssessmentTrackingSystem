902a98646624eb16980cd8949a9a749f1c3f3c76
Paul Cooper <62419931+PaulCooperWorkJustice@users.noreply.github.com>
2025-09-08 10:41:35 +0100

    CFODEV-1368:  Remove the warning for activities completed at Hubs (#760)
    * CFODEV-1368:  Remove the warning for activities completed at Hubs
    
    * CFODEV-1368 - only display the warning message when there not been at location and not hub.  As not always at hub
    
    * Adjust check and return specific error for hub induction failures.
    
    ---------
    
    Co-authored-by: Carl Sixsmith <carl.sixsmith1@justice.gov.uk>

--- src/Application/Features/Participants/Queries/GetParticipantWasAtThisLocationCheck.cs
+++ src/Application/Features/Participants/Queries/GetParticipantWasAtThisLocationCheck.cs
@@ -1,13 +1,14 @@
 ﻿using Cfo.Cats.Application.Common.Security;
 using Cfo.Cats.Application.Common.Validators;
 using Cfo.Cats.Application.SecurityConstants;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
 namespace Cfo.Cats.Application.Features.Participants.Queries;
 
 public static class GetParticipantWasAtThisLocationCheck
 {
     [RequestAuthorize(Policy = SecurityPolicies.AuthorizedUser)]
-    public class Query : IRequest<bool>
+    public class Query : IRequest<Result<bool>>
     {
         public required string ParticipantId { get; set; }
         public required int LocationId { get; set; }
@@ -15,31 +16,57 @@ public static class GetParticipantWasAtThisLocationCheck
         public required DateTime? DateAtLocation { get; set; }
     }
 
-    private class Handler(IUnitOfWork unitOfWork) : IRequestHandler<Query, bool>
+    public class Handler(IUnitOfWork unitOfWork) : IRequestHandler<Query, Result<bool>>
     {
-        public async Task<bool> Handle(Query request, CancellationToken cancellationToken)
+        public async Task<Result<bool>> Handle(Query request, CancellationToken cancellationToken)
         {
+            var dateAtLocation = request.DateAtLocation!.Value.Date;
 
-            var dateAtLocation = request.DateAtLocation?.Date;
+            var location = await unitOfWork.DbContext.Locations
+                .AsNoTracking()
+                .Where(l => l.Id == request.LocationId)
+                .Select(l => l.LocationType)
+                .FirstOrDefaultAsync(cancellationToken);
 
-            if (dateAtLocation == null)
+            return location switch
             {
-                return false;
-            }
+                null => Result<bool>.Failure("Invalid location"),
+                var t when t.IsHub => await CheckHub(request),
+                _ => await CheckNonHub(request)
+            };
+        }
 
-            var db = unitOfWork.DbContext;       
-            
-            var locationOnDate = await db.ParticipantLocationHistories
-                  .Where(plh =>
-                      plh.ParticipantId == request.ParticipantId &&
-                      plh.LocationId == request.LocationId &&
-                      plh.From.Date <= dateAtLocation &&
-                      (!plh.To.HasValue || plh.To.Value.Date >= dateAtLocation)
-                  )
-                  .OrderByDescending(x => x.From)
-                  .FirstOrDefaultAsync(cancellationToken);
+        /// <summary>
+        /// Checks that the participant has a hub induction on the given date.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        private async Task<Result<bool>> CheckHub(Query request)
+        {
+            var query = from hi in unitOfWork.DbContext.HubInductions.AsNoTracking()
+                        where hi.ParticipantId == request.ParticipantId
+                            && hi.LocationId == request.LocationId
+                            && hi.InductionDate <= request.DateAtLocation!.Value.Date
+                        select hi.InductionDate;
 
-            return locationOnDate != null;
+            var result = await query.ToListAsync();
+
+            return result is null ? Result<bool>.Failure("No hub induction recorded for this location on this date") : true;
+        }
+
+        private async Task<Result<bool>> CheckNonHub(Query request)
+        {
+            var dateAtLocation = request.DateAtLocation!.Value.Date;
+
+            var locationOnDate = await unitOfWork.DbContext.ParticipantLocationHistories.AsNoTracking()
+                .Where(plh => plh.ParticipantId == request.ParticipantId &&
+                              plh.LocationId == request.LocationId &&
+                              plh.From.Date <= dateAtLocation &&
+                              (!plh.To.HasValue || plh.To.Value.Date >= dateAtLocation))
+                .OrderByDescending(x => x.From)
+                .FirstOrDefaultAsync();
+
+            return locationOnDate is null ? Result<bool>.Failure("Participant was not at location on date") : true;
         }
     }
 
@@ -58,6 +85,9 @@ public static class GetParticipantWasAtThisLocationCheck
                 .Length(9)
                 .Matches(ValidationConstants.AlphaNumeric)
                 .WithMessage(string.Format(ValidationConstants.AlphaNumericMessage, "Participant Id"));
+
+            RuleFor(x => x.DateAtLocation)
+                .NotNull();
 
             RuleSet(ValidationConstants.RuleSet.MediatR, () =>
             {
