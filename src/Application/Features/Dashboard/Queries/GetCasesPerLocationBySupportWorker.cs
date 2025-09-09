2ab632e5b43d28aa38ef8ff33f77766c84e7b454
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2025-09-09 08:25:20 +0100

    Add cases per location by support worker (#763)
    * Add cases per location by support worker
    
    * Code clean up as per peer review

---
+++ src/Application/Features/Dashboard/Queries/GetCasesPerLocationBySupportWorker.cs
@@ -0,0 +1,54 @@
+﻿using Cfo.Cats.Application.Common.Security;
+using Cfo.Cats.Application.SecurityConstants;
+
+namespace Cfo.Cats.Application.Features.Dashboard.Queries;
+
+public static class GetCasesPerLocationBySupportWorker
+{
+    [RequestAuthorize(Policy = SecurityPolicies.AuthorizedUser)]
+    public class Query : IRequest<Result<CasesPerLocationSupportWorkerDto>>
+    { 
+        public required string UserId { get; set; }
+        public required UserProfile CurrentUser { get; set; }
+    }
+
+    public class Handler(IUnitOfWork unitOfWork) : IRequestHandler<Query, Result<CasesPerLocationSupportWorkerDto>>
+    {
+        public async Task<Result<CasesPerLocationSupportWorkerDto>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var context = unitOfWork.DbContext;
+
+            var query =
+                from p in context.Participants
+                where p.OwnerId == request.UserId
+                      && p.EnrolmentStatus != EnrolmentStatus.ArchivedStatus.Value
+                group p by new { p.CurrentLocation.Name, p.EnrolmentStatus } into g
+                select new
+                {
+                    g.Key.Name,
+                    g.Key.EnrolmentStatus,
+                    Count = g.Count()
+                };
+
+            var result = await query.AsNoTracking()
+                .ToArrayAsync(cancellationToken);
+
+            CasesPerLocationSupportWorkerDto dto = new CasesPerLocationSupportWorkerDto()
+            {
+                Records = result
+                .OrderBy(r => r.Name)
+                .ThenBy(r => r.EnrolmentStatus.Name)
+                .Select(r => (r.Name, r.EnrolmentStatus.Name, r.Count))
+                .ToArray()
+            };
+
+            return dto;
+
+        }
+    }
+}
+
+public record CasesPerLocationSupportWorkerDto
+{
+    public required (string Location, string Status, int Count)[] Records { get; init; }
+}
---
+++ src/Server.UI/Components/Dashboard/CasesPerLocationPerSupportWorkerCardComponent.razor.cs
@@ -0,0 +1,16 @@
+using Cfo.Cats.Application.Features.Dashboard.Queries;
+
+namespace Cfo.Cats.Server.UI.Components.Dashboard;
+
+public partial class CasesPerLocationPerSupportWorkerCardComponent
+{
+    [EditorRequired, Parameter]
+    public string UserId { get; set; } = null!;
+
+    protected override IRequest<Result<CasesPerLocationSupportWorkerDto>> CreateQuery()
+     => new GetCasesPerLocationBySupportWorker.Query()
+     {
+         CurrentUser = CurrentUser, 
+         UserId = UserId
+     };
+}
