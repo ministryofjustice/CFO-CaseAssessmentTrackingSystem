0e5b5f597382bbf9d788b29ba01fd6b305f50caf
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2025-09-17 11:34:33 +0100

    Limit length of string for user column

---
+++ src/Infrastructure/Persistence/Migrations/20250917103354_NewMigration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cfo.Cats.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CompletedBy",
+                schema: "Activities",
+                table: "Activity",
+                type: "nvarchar(36)",
+                maxLength: 36,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CompletedBy",
+                schema: "Activities",
+                table: "Activity",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(36)",
+                oldMaxLength: 36,
+                oldNullable: true);
+        }
+    }
+}
