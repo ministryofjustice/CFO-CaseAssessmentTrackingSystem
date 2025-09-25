ad64a75ba9cf999ea469761bb4a764254362588d
VS <vinay.saini@justice.gov.uk>
2025-09-25 20:11:17 +0100

    Refactors case notes component for reusability
    Updates the case notes component to use AllowAddNote instead
    of ParticipantIsActive to determine if a new note can be added. This allows the CaseNotes component to display notes without Add New Note button.

--- src/Server.UI/Pages/Analytics/Components/OutcomeQualityDipSampleNotesComponent.razor.cs
+++
@@ -1,7 +0,0 @@
-namespace Cfo.Cats.Server.UI.Pages.Analytics.Components;
-public partial class OutcomeQualityDipSampleNotesComponent
-{
-    [Parameter]
-    [EditorRequired]
-    public string ParticipantId { get; set; } = default!;
-}
