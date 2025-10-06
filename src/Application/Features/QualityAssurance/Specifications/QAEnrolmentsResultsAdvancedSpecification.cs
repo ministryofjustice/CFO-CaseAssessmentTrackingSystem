e733afa90137705c61454c4069f5428c772883f6
PaulCooperWorkJustice <paul.cooper5@justice.gov.uk>
2025-10-06 10:21:34 +0100

    CFODEV-1333:  Restrict access to only cases that the user has access to

--- src/Application/Features/QualityAssurance/Specifications/QAEnrolmentsResultsAdvancedSpecification.cs
+++ src/Application/Features/QualityAssurance/Specifications/QAEnrolmentsResultsAdvancedSpecification.cs
@@ -6,12 +6,14 @@ namespace Cfo.Cats.Application.Features.QualityAssurance.Specifications;
 public class QAEnrolmentsResultsAdvancedSpecification : Specification<Participant>
 {
     public QAEnrolmentsResultsAdvancedSpecification(QAEnrolmentsResultsAdvancedFilter filter)
-    {        
-        Query   .Where(e => e.EnrolmentStatus != EnrolmentStatus.ArchivedStatus.Value)
+    {
+        Query.Where(e => e.EnrolmentStatus != EnrolmentStatus.ArchivedStatus.Value)
                 .Where(e => e.EnrolmentLocation.Id == filter.Location!.Id, filter.Location is not null)
                 .Where(e => e.EnrolmentStatus == filter.Status, filter.Status is not null)
-                .Where(e => e.OwnerId == filter.UserProfile.UserId, filter.JustMyParticipants);
-
+                .Where(e => e.OwnerId == filter.UserProfile.UserId, filter.JustMyParticipants)
+                .Where(e => e.Owner.TenantId.StartsWith(filter.UserProfile.TenantId!));
+                
+                
         // Order by enrolment status, with 'Approved' appearing towards the bottom of the list
         Query   .OrderBy(x => x.EnrolmentStatus == EnrolmentStatus.ApprovedStatus.Value ? 1 : 0)            
                 .ThenByDescending(x => x.Created)
