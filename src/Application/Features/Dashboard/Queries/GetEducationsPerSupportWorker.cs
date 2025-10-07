5498258a8f00839cd10c9bbc5905e9de53812f0e
VS <vinay.saini@justice.gov.uk>
2025-10-07 11:27:41 +0100

    Adds education dashboard for support workers
    Introduces a new dashboard component that displays education data for support workers, broken down by location type (Custody and Community).
    
    The dashboard includes a visual representation using ApexCharts, as well as a tabular view of the data, showing location details, payable counts, and total counts.
    This enables a more granular view of support worker performance and education activities.

---
+++ src/Application/Features/Dashboard/Queries/GetEducationsPerSupportWorker.cs
@@ -0,0 +1,71 @@
+﻿using Cfo.Cats.Application.Common.Security;
+using Cfo.Cats.Application.SecurityConstants;
+
+namespace Cfo.Cats.Application.Features.Dashboard.Queries;
+public static class GetEducationsPerSupportWorker
+{
+    [RequestAuthorize(Policy = SecurityPolicies.AuthorizedUser)]
+    public class Query : IRequest<Result<EducationsPerSupportWorkerDto>>
+    {
+        public required DateTime StartDate { get; set; }
+        public required DateTime EndDate { get; set; }
+        public required string UserId { get; set; }
+        public required UserProfile CurrentUser { get; set; }
+    }
+
+    public class Handler(IUnitOfWork unitOfWork) : IRequestHandler<Query, Result<EducationsPerSupportWorkerDto>>
+    {
+        public async Task<Result<EducationsPerSupportWorkerDto>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var context = unitOfWork.DbContext;
+
+            var query = from mi in context.EducationPayments
+                        join ap in context.Activities on mi.ActivityId equals ap.Id
+                        join l in context.Locations on mi.LocationId equals l.Id
+                        where ap.OwnerId == request.UserId
+                        && mi.ActivityApproved >= request.StartDate
+                        && mi.ActivityApproved <= request.EndDate
+                        group mi by l into grp
+                        select new LocationDetail
+                            (
+                                grp.Key.Name,
+                                grp.Key.LocationType,
+                                grp.Count(mi => mi.EligibleForPayment),
+                                grp.Count()
+                            );
+
+            var results = await query
+                .AsNoTracking()
+                .ToArrayAsync(cancellationToken);
+
+            return new EducationsPerSupportWorkerDto(results);
+        }
+    }
+
+    public record EducationsPerSupportWorkerDto
+    {
+        public EducationsPerSupportWorkerDto(LocationDetail[] details)
+        {
+            Details = details;
+
+            Custody = details.Where(d => d.LocationType.IsCustody).Sum(d => d.TotalCount);
+            CustodyPayable = details.Where(d => d.LocationType.IsCustody).Sum(d => d.Payable);
+
+            Community = details.Where(d => d.LocationType.IsCommunity).Sum(d => d.TotalCount);
+            CommunityPayable = details.Where(d => d.LocationType.IsCommunity).Sum(d => d.Payable);
+
+        }
+
+        public LocationDetail[] Details { get; }
+
+        public int Custody { get; }
+        public int CustodyPayable { get; }
+
+        public int Community { get; }
+        public int CommunityPayable { get; }
+
+    }
+
+    public record LocationDetail(string Name, LocationType LocationType, int Payable, int TotalCount);
+
+}
