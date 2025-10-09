8d6e5fd54eefbe68ec99e65886e237844be939dc
VS <vinay.saini@justice.gov.uk>
2025-10-09 22:53:58 +0100

    Replaces MudChart with ApexChart for SW activity
    Migrates from MudChart to ApexChart to display support worker
    paid, commenced and approved activity data in a horizontal bar chart. This change
    improves chart rendering  and is much better than the MudChart.
    TODO: verify queries for approved and commenced, also need to do something about repeating Custody and Community mud chips

---
+++ src/Application/Features/Dashboard/Queries/GetApprovedActivitiesPerSupportWorker.cs
@@ -0,0 +1,63 @@
+﻿using Cfo.Cats.Application.Common.Security;
+using Cfo.Cats.Application.SecurityConstants;
+
+namespace Cfo.Cats.Application.Features.Dashboard.Queries;
+public static class GetApprovedActivitiesPerSupportWorker
+{
+    [RequestAuthorize(Policy = SecurityPolicies.AuthorizedUser)]
+    public class Query : IRequest<Result<ApprovedActivitiesPerSupportWorkerDto>>
+    {
+        public required DateTime StartDate { get; set; }
+        public required DateTime EndDate { get; set; }
+        public required string UserId { get; set; }
+        public required UserProfile CurrentUser { get; set; }
+    }
+    public class Handler(IUnitOfWork unitOfWork) : IRequestHandler<Query, Result<ApprovedActivitiesPerSupportWorkerDto>>
+    {
+        public async Task<Result<ApprovedActivitiesPerSupportWorkerDto>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var context = unitOfWork.DbContext;
+
+            var query = from a in context.Activities 
+                        join l in context.Locations on a.TookPlaceAtLocation.Id equals l.Id
+                        where a.OwnerId == request.UserId
+                              && a.CompletedOn >= request.StartDate
+                              && a.CompletedOn <= request.EndDate
+                        group a by new
+                        {
+                            l.Name,
+                            l.LocationType,
+                            a.Type
+                        } into g
+                        orderby g.Key.Name, g.Key.Type
+                        select new LocationDetail(
+                            g.Key.Name,
+                            g.Key.LocationType,
+                            g.Key.Type,
+                            g.Count()
+                        );
+
+            var results = await query
+                .AsNoTracking()
+                .ToArrayAsync(cancellationToken);
+
+            return new ApprovedActivitiesPerSupportWorkerDto(results);
+        }
+    }
+    public record ApprovedActivitiesPerSupportWorkerDto
+    {
+        public ApprovedActivitiesPerSupportWorkerDto(LocationDetail[] details)
+        {
+            Details = details;
+            Custody = details.Where(d => d.LocationType.IsCustody).Sum(d => d.TotalCount);
+            Community = details.Where(d => d.LocationType.IsCommunity).Sum(d => d.TotalCount);
+
+        }
+        public LocationDetail[] Details { get; }
+        public int Custody { get; }
+        public int Community { get; }
+    }
+
+    public record LocationDetail(string Name, LocationType LocationType, ActivityType ActivityType, int TotalCount);
+
+}
