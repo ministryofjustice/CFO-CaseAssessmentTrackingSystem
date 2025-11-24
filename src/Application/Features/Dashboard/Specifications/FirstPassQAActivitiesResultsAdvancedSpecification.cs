284a8f1cdd3d5b90daf27f24211943d33e8e107a
Paul Cooper <62419931+PaulCooperWorkJustice@users.noreply.github.com>
2025-11-24 13:13:40 +0000

    CFODEV-1434:  Updated code and ready for test

--- src/Application/Features/Dashboard/Specifications/FirstPassQAActivitiesResultsAdvancedSpecification.cs
+++ src/Application/Features/Dashboard/Specifications/FirstPassQAActivitiesResultsAdvancedSpecification.cs
@@ -7,17 +7,14 @@ public sealed class FirstPassQAActivitiesResultsAdvancedSpecification : Specific
     public FirstPassQAActivitiesResultsAdvancedSpecification(FirstPassQAActivitiesResultsAdvancedFilter filter)
     {
         Query.Where(p => p.EnrolmentStatus != EnrolmentStatus.ArchivedStatus.Value);
-        //                   && peh.From >= thirtyDaysAgo);
-        //
-        // Query.Where(peh => peh.Participant!.OwnerId == filter.CurrentUser!.UserId, filter.JustMyParticipants);
-        //      
-        // Query.Where(peh => peh.Participant!.Owner!.TenantId!.StartsWith(filter.CurrentUser!.TenantId!));
-        //
+   
+        Query.Where(p => p.Owner!.TenantId!.StartsWith(filter.CurrentUser!.TenantId!));
+
         // If we have passed a filter through, search the surname and current location
         Query.Where(p => string.IsNullOrWhiteSpace(filter.Keyword)
-                            || p.CurrentLocation.Name.Contains(filter.Keyword)
-                            || p.FirstName.Contains(filter.Keyword)
-                            || p.LastName.Contains(filter.Keyword)
-                            || p.Id.Contains(filter.Keyword));             
+                         || p.CurrentLocation.Name.Contains(filter.Keyword)
+                         || p.FirstName.Contains(filter.Keyword)
+                         || p.LastName.Contains(filter.Keyword)
+                         || p.Id.Contains(filter.Keyword));             
     }
 }
