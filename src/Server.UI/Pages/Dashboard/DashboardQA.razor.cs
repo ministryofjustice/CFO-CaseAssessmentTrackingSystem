dc93ccc33b7b82aeca9b43fa15b832541217eebc
Vinay Saini <vinay.saini@justice.gov.uk>
2025-11-26 07:55:12 +0000

    QA dashboard landing page
    Add QA dashboard landing page to view QA reports with records associated to me(logged in user) within a date range.
    
    Adds QA dashboard landing page which is accessible from menu item: “QA” in Dashboard menu and only visible to CFO internal users i.e. users with SystemSupport, SMT, QAOfficer, QASupportManager, QAManager roles. All reports/components should be based on the selected dates and logged in user.

---
+++ src/Server.UI/Pages/Dashboard/DashboardQA.razor.cs
@@ -0,0 +1,20 @@
+using Cfo.Cats.Application.Common.Security;
+
+namespace Cfo.Cats.Server.UI.Pages.Dashboard;
+
+public partial class DashboardQA
+{
+    private MudDateRangePicker _picker = null!;
+
+    private bool _visualMode = true;
+    public string? SelectedUserId { get; set; }
+    public string? SelectedDisplayName { get; set; }
+    private DateRange _dateRange { get; set; } = new DateRange(new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1), DateTime.Today);
+    
+    [CascadingParameter]
+    public UserProfile CurrentUser { get; set; } = null!;
+    protected override void OnInitialized()
+    {
+
+    }
+}
