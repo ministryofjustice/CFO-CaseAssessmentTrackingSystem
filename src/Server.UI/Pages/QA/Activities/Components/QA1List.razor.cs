bfd3d604a399fcb7abc3d2beb7b922d99a039e55
Paul Cooper <62419931+PaulCooperWorkJustice@users.noreply.github.com>
2025-12-09 09:10:42 +0000

    CFODEV-1438:  Tidy up QA1 List and add notes to QA2List.  Add first name to QA2 search.

--- src/Server.UI/Pages/QA/Activities/Components/QA1List.razor.cs
+++ src/Server.UI/Pages/QA/Activities/Components/QA1List.razor.cs
@@ -5,23 +5,24 @@ using Cfo.Cats.Application.Features.Activities.Queries;
 namespace Cfo.Cats.Server.UI.Pages.QA.Activities.Components;
 
 public partial class QA1List
-{
+{ 
     [CascadingParameter] public UserProfile? UserProfile { get; set; }
 
-    private bool _loading = false;
+    private bool _loading;
     private int _defaultPageSize = 30;
-    //private MudDataGrid<ActivityQueueEntryDto> _table = default!;
-
-    private ActivityQa1WithPagination.Query Query { get; set; } = new();
-    private ActivityQueueEntryDto _currentDto = new();
-
+    private ActivityQa1WithPagination.Query Query { get; } = new();
     private GridData<ActivityQueueEntryDto>? _pagedData;
-    private int _currentPage = 0;
+    private int _currentPage;
+
+    private int TotalPages =>
+        (_pagedData!.TotalItems + _defaultPageSize - 1) / _defaultPageSize;
+
     private async Task OnPaginationChanged(int page)
     {
         _currentPage = page - 1;
         await LoadPage();
     }
+    
     private async Task LoadPage()
     {
         var state = new GridState<ActivityQueueEntryDto>
@@ -67,12 +68,9 @@ public partial class QA1List
         }
         
         Query.Keyword = text;
-
         _currentPage = 0;
 
         await LoadPage();
-
-        //await _table.ReloadServerData();
     }
 
     private async Task OnRefresh()
@@ -81,9 +79,8 @@ public partial class QA1List
         _currentPage = 0;
 
         await LoadPage();
-
-//        await _table.ReloadServerData();
     }
+    
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
         if (firstRender)
@@ -92,25 +89,16 @@ public partial class QA1List
         }
     }
     
-    
     // track expanded rows by ActivityId
-    private HashSet<Guid> ExpandedRows { get; } = new();
+    private HashSet<Guid> ExpandedRows { get; } = [];
 
     private void ToggleRow(Guid activityId)
     {
-        if (ExpandedRows.Contains(activityId))
+        if (!ExpandedRows.Remove(activityId))
         {
-            ExpandedRows.Remove(activityId);
-        }
-        else
-        {
-            // optional: collapse others — uncomment if you want single-open behavior
-            // ExpandedRows.Clear();
             ExpandedRows.Add(activityId);
         }
 
         StateHasChanged();
     }
-
-   
 }
