d4e4403e29a15a6fd9f49019f3fad7c663d9cd6b
Vinay Saini <vinay.saini@justice.gov.uk>
2025-12-10 17:31:10 +0000

    Adds chart and tabular data for enrolments accepted with advisories
    It displays enrolments which are accepted with advisory for provider to keep in mind for future,  service desk records the messages with prefixes 'Advisory' and 'Accepted by Exception'

---
+++ src/Server.UI/Components/Dashboard/QA/EnrolmentAdvisoriesToProviderDashboardComponent.razor.cs
@@ -0,0 +1,69 @@
+using ApexCharts;
+using Cfo.Cats.Application.Features.Dashboard.Queries;
+
+namespace Cfo.Cats.Server.UI.Components.Dashboard.QA;
+public partial class EnrolmentAdvisoriesToProviderDashboardComponent
+{ 
+    [EditorRequired, Parameter]
+    public DateRange? DateRange { get; set; }
+
+    [Parameter]
+    public string UserId { get; set; } = null!;    
+    
+    [Parameter]
+    public string TenantId { get; set; } = null!;    
+    
+    [EditorRequired, Parameter]
+    public bool VisualMode { get; set; }
+
+    [CascadingParameter(Name = "IsDarkMode")]
+    public bool IsDarkMode { get; set; }
+
+    protected override IRequest<Result<GetEnrolmentAdvisoriesToProvider.EnrolmentAdvisoriesToProviderDto>> CreateQuery()
+     => new GetEnrolmentAdvisoriesToProvider.Query()
+     {
+         CurrentUser = CurrentUser,
+         UserId = CurrentUser.UserId,
+         TenantId = TenantId,
+         StartDate = DateRange?.Start ?? throw new InvalidOperationException("DateRange not set"),
+         EndDate = DateRange?.End ?? throw new InvalidOperationException("DateRange not set")
+     };
+
+    private ApexCharts.ApexChartOptions<GetEnrolmentAdvisoriesToProvider.EnrolmentAdvisoriesChartData> Options => new()
+    {
+        Chart = new ApexCharts.Chart
+        {
+            Stacked = true,
+        },
+        PlotOptions = new ApexCharts.PlotOptions
+        {
+            Bar = new ApexCharts.PlotOptionsBar
+            {
+                DataLabels = new ApexCharts.PlotOptionsBarDataLabels
+                {
+                    Total = new ApexCharts.BarTotalDataLabels
+                    {
+                        Style = new ApexCharts.BarDataLabelsStyle
+                        {
+                            FontWeight = "800",
+                            Color = IsDarkMode ? "#FFFFFF" : "#000000",
+                        }
+                    }
+                },
+            },
+        },
+        Yaxis = new List<YAxis>
+        {
+            new YAxis
+            {
+                Min = 0,
+                ForceNiceScale = true
+            }
+        },
+        Theme = new ApexCharts.Theme
+        {
+            Mode = IsDarkMode ? ApexCharts.Mode.Dark : ApexCharts.Mode.Light
+        }
+    };
+
+}    
