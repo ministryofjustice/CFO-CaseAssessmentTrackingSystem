d801d54505fc4c00f8907906106a9517868149b1
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2025-12-19 14:38:49 +0000

    Adds validation to pathway plan review comments
    Ensures review comments are not null, adhere to length restrictions, and match the specified notes pattern.
    
    Also, reorders the list of review reasons and renames some reasons
    to align with updated requirements and improve clarity.

--- src/Domain/Common/Enums/PathwayPlanReviewReason.cs
+++ src/Domain/Common/Enums/PathwayPlanReviewReason.cs
@@ -6,11 +6,11 @@ public sealed class PathwayPlanReviewReason : SmartEnum<PathwayPlanReviewReason>
 {
     public static readonly PathwayPlanReviewReason Default = new("Please Select Reason", -1);
     public static readonly PathwayPlanReviewReason OriginalReview = new("Original review", 0);
-    public static readonly PathwayPlanReviewReason AssessmentPerformed = new("Assessment performed", 1);
-    public static readonly PathwayPlanReviewReason ParticipantChangedLocation = new("Participant changed location", 2);
-    public static readonly PathwayPlanReviewReason ChangeToCircumstances = new("Change of participant personal circumstances", 3);
-    public static readonly PathwayPlanReviewReason EndOfWingPhaseDelivery = new("End of wing phase delivery",4);
-    public static readonly PathwayPlanReviewReason NinetyDayReview = new("90 day review", 5);
+    public static readonly PathwayPlanReviewReason AssessmentPerformed = new("Assessment Changed", 1);
+    public static readonly PathwayPlanReviewReason ParticipantChangedLocation = new("Location Change", 2);
+    public static readonly PathwayPlanReviewReason ChangeToCircumstances = new("Change Of Personal Circumstances", 3);
+    public static readonly PathwayPlanReviewReason EndOfWingPhaseDelivery = new("End Of Wing Phase Delivery",4);
+    public static readonly PathwayPlanReviewReason NinetyDayReview = new("90 Day Review", 5);
     
     private PathwayPlanReviewReason(string name, int value) 
         : base(name, value) 
@@ -18,4 +18,14 @@ public sealed class PathwayPlanReviewReason : SmartEnum<PathwayPlanReviewReason>
     }
     
     public bool IsValidSelection() => this != Default;
+
+    public static PathwayPlanReviewReason[] OrderedList => new[]
+    {
+        Default,
+        NinetyDayReview,
+        AssessmentPerformed,
+        ChangeToCircumstances,
+        EndOfWingPhaseDelivery,
+        ParticipantChangedLocation
+    };
 }
