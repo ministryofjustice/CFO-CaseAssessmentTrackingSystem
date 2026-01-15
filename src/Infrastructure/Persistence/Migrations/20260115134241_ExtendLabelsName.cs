b7179c21e88919a0a55be2b0b43bf8b8ebe8a076
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2026-01-15 13:51:04 +0000

    Extends maximum length for label names
    Increases the maximum allowed length for label names from 15 to 25 characters.

---
+++ src/Infrastructure/Persistence/Migrations/20260115134241_ExtendLabelsName.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cfo.Cats.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExtendLabelsName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "Configuration",
+                table: "Label",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "Configuration",
+                table: "Label",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+        }
+    }
+}
