9ed348223364d3aa3ba23339856b471d94b30e4e
Paul Cooper <62419931+PaulCooperWorkJustice@users.noreply.github.com>
2026-01-15 09:19:53 +0000

    CFODEV-1434:  Add the database and entity for activity feedback

---
+++ src/Domain/Common/Enums/FeedbackStage.cs
@@ -0,0 +1,14 @@
+﻿using Ardalis.SmartEnum;
+
+namespace Cfo.Cats.Domain.Common.Enums;
+
+public sealed class FeedbackStage : SmartEnum<FeedbackStage>
+{
+    public static readonly FeedbackStage SecondPass = new("Second Pass", 0);
+    public static readonly FeedbackStage Escalation = new("Escalation", 1);
+   
+    private FeedbackStage(string name, int value)
+        : base(name, value)
+    {
+    }
+}
