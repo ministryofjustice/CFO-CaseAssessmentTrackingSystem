0327adfdeb7def53abe1115d700fef24673a99f7
Vinay Saini <vinay.saini@justice.gov.uk>
2026-01-20 11:31:21 +0000

    Migrations:Enrolments
    Adds FeedbackType field to Enrolment.EscalationQueueNote and Enrolment.Qa2QueueNote tables

--- src/Domain/Entities/Participants/EnrolmentQueueEntryNote.cs
+++ src/Domain/Entities/Participants/EnrolmentQueueEntryNote.cs
@@ -5,4 +5,5 @@ namespace Cfo.Cats.Domain.Entities.Participants;
 public class EnrolmentQueueEntryNote : Note
 {
     public bool IsExternal { get; set; }
+    public Cfo.Cats.Domain.Common.Enums.FeedbackType? FeedbackType { get; set; }
 }
