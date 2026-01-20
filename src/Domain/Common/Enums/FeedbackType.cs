88c0540ade3c9d54ab1618229237e12bf38baec1
Vinay Saini <vinay.saini@justice.gov.uk>
2026-01-20 10:36:55 +0000

    Activities: Add FeedbackType field to QA2 and Escalation queue responses
    - Add FeedbackType enum (Advisory, AcceptedByException) to Domain
    - Update QA2 and Escalation response commands to accept FeedbackType
    - Add FeedbackType radio buttons in QA2 and Escalation UI (shown only when Accept response is selected)
    - Clear FeedbackType when response changes from Accept to other options

--- src/Domain/Common/Enums/FeedbackType.cs
+++ src/Domain/Common/Enums/FeedbackType.cs
@@ -1,12 +1,7 @@
-using System.ComponentModel;
-
 namespace Cfo.Cats.Domain.Common.Enums;
 
 public enum FeedbackType
 {
-    [Description("Advisory")]
     Advisory = 0,
-    
-    [Description("Accepted by Exception")]
     AcceptedByException = 1
 }
