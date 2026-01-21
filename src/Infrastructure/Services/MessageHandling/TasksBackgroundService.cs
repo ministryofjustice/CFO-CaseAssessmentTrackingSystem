82c374aebda7376577e3e36f76925d01c19dfa4f
Paul Cooper <62419931+PaulCooperWorkJustice@users.noreply.github.com>
2026-01-21 15:25:06 +0000

    CFODEV-1418:  The database side and calls.  No migrations.

--- src/Infrastructure/Services/MessageHandling/TasksBackgroundService.cs
+++ src/Infrastructure/Services/MessageHandling/TasksBackgroundService.cs
@@ -21,7 +21,8 @@ internal class TasksBackgroundService(IServiceProvider provider, IConfiguration 
         _activator = new BuiltinHandlerActivator();
 
         _activator.Handle<PriTaskCompletedWatcherConsumer>(provider)
-            .Handle<RaisePaymentsAfterApprovalConsumer>(provider);
+            .Handle<RaisePaymentsAfterApprovalConsumer>(provider)
+            .Handle<RecordArchivedCaseConsumer>(provider);
 
         _bus = Configure.With(_activator)
             .Transport(t => t.UseRabbitMq(configuration.GetConnectionString("rabbit"), options.Value.TasksService)
