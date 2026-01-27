b39a76d02ad793c5375448c47616c24dfb7c4ef8
Vinay Saini <vinay.saini@justice.gov.uk>
2026-01-27 17:04:02 +0000

    Migrations - Add ProviderFeedbackEnrolment and  ProviderFeedbackEnrolment entities

---
+++ src/Domain/Entities/ManagementInformation/ProviderFeedbackEnrolment.cs
@@ -0,0 +1,196 @@
+namespace Cfo.Cats.Domain.Entities.ManagementInformation;
+
+public class ProviderFeedbackEnrolment
+{
+#pragma warning disable CS8618 
+    private ProviderFeedbackEnrolment(){}
+#pragma warning restore CS8618    
+
+    public Guid Id { get; set; } = Guid.CreateVersion7();
+    public required DateTime CreatedOn { get; set; }    
+
+    public required string SourceTable { get; set; }
+
+    public required string Queue { get; set; }
+
+    public required Guid QueueEntryId { get; set; }
+
+    public required int NoteId { get; set; }
+
+    public required string TenantId { get; set; }
+
+    public required string ContractId { get; set; }
+
+    public required string ParticipantId { get; set; }
+
+    public required string SupportWorkerId { get; set; }
+
+    public required string ProviderQaUserId { get; set; }
+
+    public required string CfoUserId { get; set; }
+
+    public required DateTime PqaSubmittedDate { get; set; }
+
+    public required DateTime ActionDate { get; set; }
+
+    public DateTime? NoteCreatedDate { get; set; }
+
+    public string? NoteCreatedBy { get; set; }
+
+    public string Message { get; set; }
+
+    public int? FeedbackType { get; set; }
+
+    public static ProviderFeedbackEnrolment CreateReturnedQA2Enrolment(
+        Guid queueEntryId,
+        int noteId,
+        string tenantId,
+        string contractId,
+        string participantId,
+        string supportWorkerId,
+        string providerQaUserId,
+        string cfoUserId,
+        DateTime pqaSubmittedDate,
+        DateTime actionDate,
+        DateTime? noteCreatedDate,
+        string? noteCreatedBy,
+        string message,
+        int? feedbackType)
+    {
+        return new ProviderFeedbackEnrolment()
+        {
+            Id = Guid.CreateVersion7(),
+            CreatedOn = DateTime.UtcNow,
+            Queue = "QA2",
+            SourceTable = "Enrolment.Qa2QueueNote",
+            QueueEntryId = queueEntryId,
+            NoteId = noteId,
+            TenantId = tenantId,
+            ContractId = contractId,
+            ParticipantId = participantId,
+            SupportWorkerId = supportWorkerId,
+            ProviderQaUserId = providerQaUserId,
+            CfoUserId = cfoUserId,
+            PqaSubmittedDate = pqaSubmittedDate,
+            ActionDate = actionDate,
+            NoteCreatedDate = noteCreatedDate,
+            NoteCreatedBy = noteCreatedBy,
+            Message = message,
+            FeedbackType = feedbackType
+        };
+    }
+    public static ProviderFeedbackEnrolment CreateReturnedEscalationEnrolment(
+        Guid queueEntryId,
+        int noteId,
+        string tenantId,
+        string contractId,
+        string participantId,
+        string supportWorkerId,
+        string providerQaUserId,
+        string cfoUserId,
+        DateTime pqaSubmittedDate,
+        DateTime actionDate,
+        DateTime? noteCreatedDate,
+        string? noteCreatedBy,
+        string message,
+        int? feedbackType)
+    {
+        return new ProviderFeedbackEnrolment()
+        {
+            Id = Guid.CreateVersion7(),
+            CreatedOn = DateTime.UtcNow,
+            Queue = "Escalation",
+            SourceTable = "Enrolment.EscalationNote",
+            QueueEntryId = queueEntryId,
+            NoteId = noteId,
+            TenantId = tenantId,
+            ContractId = contractId,
+            ParticipantId = participantId,
+            SupportWorkerId = supportWorkerId,
+            ProviderQaUserId = providerQaUserId,
+            CfoUserId = cfoUserId,
+            PqaSubmittedDate = pqaSubmittedDate,
+            ActionDate = actionDate,
+            NoteCreatedDate = noteCreatedDate,
+            NoteCreatedBy = noteCreatedBy,
+            Message = message,
+            FeedbackType = feedbackType
+        };
+    }    
+    public static ProviderFeedbackEnrolment CreateAdvisoryQA2Enrolment(
+        Guid queueEntryId,
+        int noteId,
+        string tenantId,
+        string contractId,
+        string participantId,
+        string supportWorkerId,
+        string providerQaUserId,
+        string cfoUserId,
+        DateTime pqaSubmittedDate,
+        DateTime actionDate,
+        DateTime? noteCreatedDate,
+        string? noteCreatedBy,
+        string message,
+        int? feedbackType)
+    {
+        return new ProviderFeedbackEnrolment()
+        {
+            Id = Guid.CreateVersion7(),
+            CreatedOn = DateTime.UtcNow,
+            Queue = "QA2",
+            SourceTable = "Enrolment.Qa2QueueNote",
+            QueueEntryId = queueEntryId,
+            NoteId = noteId,
+            TenantId = tenantId,
+            ContractId = contractId,
+            ParticipantId = participantId,
+            SupportWorkerId = supportWorkerId,
+            ProviderQaUserId = providerQaUserId,
+            CfoUserId = cfoUserId,
+            PqaSubmittedDate = pqaSubmittedDate,
+            ActionDate = actionDate,
+            NoteCreatedDate = noteCreatedDate,
+            NoteCreatedBy = noteCreatedBy,
+            Message = message,
+            FeedbackType = feedbackType
+        };
+    }
+    public static ProviderFeedbackEnrolment CreateAdvisoryEscalationEnrolment(
+        Guid queueEntryId,
+        int noteId,
+        string tenantId,
+        string contractId,
+        string participantId,
+        string supportWorkerId,
+        string providerQaUserId,
+        string cfoUserId,
+        DateTime pqaSubmittedDate,
+        DateTime actionDate,
+        DateTime? noteCreatedDate,
+        string? noteCreatedBy,
+        string message,
+        int? feedbackType)
+    {
+        return new ProviderFeedbackEnrolment()
+        {
+            Id = Guid.CreateVersion7(),
+            CreatedOn = DateTime.UtcNow,
+            Queue = "Escalation",
+            SourceTable = "Enrolment.EscalationNote",
+            QueueEntryId = queueEntryId,
+            NoteId = noteId,
+            TenantId = tenantId,
+            ContractId = contractId,
+            ParticipantId = participantId,
+            SupportWorkerId = supportWorkerId,
+            ProviderQaUserId = providerQaUserId,
+            CfoUserId = cfoUserId,
+            PqaSubmittedDate = pqaSubmittedDate,
+            ActionDate = actionDate,
+            NoteCreatedDate = noteCreatedDate,
+            NoteCreatedBy = noteCreatedBy,
+            Message = message,
+            FeedbackType = feedbackType
+        };
+    }    
+}
---
+++ src/Infrastructure/Persistence/Configurations/ManagementInformation/ProviderFeedbackActivityEntityTypeConfiguration.cs
@@ -0,0 +1,88 @@
+using Cfo.Cats.Domain.Entities.ManagementInformation;
+using Cfo.Cats.Infrastructure.Constants.Database;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Cfo.Cats.Infrastructure.Persistence.Configurations.ManagementInformation;
+
+public class ProviderFeedbackActivityEntityTypeConfiguration : IEntityTypeConfiguration<ProviderFeedbackActivity>
+{
+    public void Configure(EntityTypeBuilder<ProviderFeedbackActivity> builder)
+    {
+        builder.ToTable(nameof(ProviderFeedbackActivity), DatabaseConstants.Schemas.Mi);
+        
+        builder.HasKey(x => x.Id);
+        
+        builder.Property(x => x.CreatedOn)
+            .IsRequired();
+        
+        builder.Property(x => x.SourceTable)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.Queue)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(x => x.QueueEntryId)
+            .IsRequired();
+
+        builder.Property(x => x.NoteId)
+            .IsRequired()
+            .HasMaxLength(DatabaseConstants.FieldLengths.GuidId);
+
+        builder.Property(x => x.TenantId)
+            .IsRequired()
+            .HasMaxLength(DatabaseConstants.FieldLengths.TenantId);
+
+        builder.Property(x => x.ContractId)
+            .IsRequired()
+            .HasMaxLength(DatabaseConstants.FieldLengths.ContractId);
+
+        builder.Property(x => x.ParticipantId)
+            .IsRequired()
+            .HasMaxLength(DatabaseConstants.FieldLengths.ParticipantId);
+
+        builder.Property(x => x.SupportWorkerId)
+            .IsRequired()
+            .HasMaxLength(DatabaseConstants.FieldLengths.GuidId);
+
+        builder.Property(x => x.ProviderQaUserId)
+            .IsRequired()
+            .HasMaxLength(DatabaseConstants.FieldLengths.GuidId);
+
+        builder.Property(x => x.CfoUserId)
+            .IsRequired()
+            .HasMaxLength(DatabaseConstants.FieldLengths.GuidId);
+
+        builder.Property(x => x.PqaSubmittedDate)
+            .IsRequired();
+
+        builder.Property(x => x.ActionDate)
+            .IsRequired();
+
+        builder.Property(x => x.NoteCreatedDate);
+
+        builder.Property(x => x.NoteCreatedBy)
+            .HasMaxLength(DatabaseConstants.FieldLengths.GuidId);
+
+        builder.Property(x => x.Message)
+            .HasMaxLength(1000);
+
+        builder.Property(x => x.FeedbackType);
+
+        builder.HasIndex(x => new {
+            x.SourceTable, 
+            x.QueueEntryId,
+            x.NoteId
+        })
+        .IsUnique()
+        .HasDatabaseName("ix_ProviderFeedbackActivity_SourceTable_QueueEntryId_NoteId");
+
+        builder.HasIndex(x => new { 
+            x.SourceTable,
+            x.TenantId,
+            x.ActionDate
+        })
+        .HasDatabaseName("ix_ProviderFeedbackActivity_SourceTable_TenantId_ActionDate");
+    }
+}
