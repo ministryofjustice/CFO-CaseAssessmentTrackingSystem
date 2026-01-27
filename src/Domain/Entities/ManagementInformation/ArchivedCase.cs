70ac451cc0eace11278ceb3b473a813371ccae64
Paul Cooper <62419931+PaulCooperWorkJustice@users.noreply.github.com>
2026-01-27 11:06:59 +0000

    CFODEV-1418:  Following changes: Add First name, last name and To columns to entity but no migration.  Add handler to close To column.  Add to calls.  First try at the Dash boad.

--- src/Domain/Entities/ManagementInformation/ArchivedCase.cs
+++ src/Domain/Entities/ManagementInformation/ArchivedCase.cs
@@ -2,6 +2,7 @@
 
 public class ArchivedCase
 {
+
 #pragma warning disable CS8618
     private ArchivedCase()
     {
@@ -10,12 +11,15 @@ public class ArchivedCase
 
     public static ArchivedCase CreateArchivedCase(
         string participantId,
+        string firstName,
+        string lastName,
         int enrolmentHistoryId,
         DateTime created,
         string createdBy,
         string? additionalInfo,
         string? archiveReason,
         DateTime from,
+        DateTime? to,
         string contractId,
         int locationId,
         string locationType,
@@ -25,12 +29,15 @@ public class ArchivedCase
         {
             Id = Guid.CreateVersion7(),
             ParticipantId = participantId,
+            FirstName = firstName,
+            LastName = lastName,
             EnrolmentHistoryId = enrolmentHistoryId,
             Created = created,
             CreatedBy = createdBy,
             AdditionalInfo = additionalInfo,
             ArchiveReason = archiveReason,
             From = from,
+            To = to,
             ContractId = contractId,
             LocationId = locationId,
             LocationType = locationType,
@@ -38,14 +45,33 @@ public class ArchivedCase
         };
     }
 
+    public ArchivedCase SetTo(DateTime to)
+    {
+        if (To is not null)
+        {
+            throw new ApplicationException("Cannot set the to date more than once.");
+        }
+
+        if (to < From)
+        {
+            throw new ArgumentException("To cannot be earlier that From");
+        }
+
+        To = to;
+        return this;
+    }
+
     public required Guid Id { get; set; }
     public required string ParticipantId { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
     public required int EnrolmentHistoryId { get; set; }
     public required DateTime Created { get; set; }
     public required string CreatedBy { get; set; }
     public required string? AdditionalInfo { get; set; }
     public required string? ArchiveReason { get; set; }
     public required DateTime From { get; set; }
+    public required DateTime? To { get; set; }
     public required string ContractId { get; set; }
     public required int LocationId { get; set; }
     public required string LocationType { get; set; }
