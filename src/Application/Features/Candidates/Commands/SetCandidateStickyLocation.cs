f75f6a7d3fdbbce1f43d5bdc3f04fbdc488b3dc4
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2026-01-28 14:34:38 +0000

    Enables QA to set sticky location with justification (#933)
    Allows QA officers to set sticky locations for candidates,  requiring justification and call reference for audit purposes.
    Adds a dialog for setting the sticky location with input fields for justification and call reference, and displays the call reference
    in case notes.
    
    Updates authorization policies to allow QA access.
    
    Related to CFODEV-1532

--- src/Application/Features/Candidates/Commands/SetCandidateStickyLocation.cs
+++ src/Application/Features/Candidates/Commands/SetCandidateStickyLocation.cs
@@ -1,16 +1,47 @@
 ﻿using Cfo.Cats.Application.Common.Security;
 using Cfo.Cats.Application.Common.Validators;
 using Cfo.Cats.Application.SecurityConstants;
+using Cfo.Cats.Domain.ValueObjects;
 
 namespace Cfo.Cats.Application.Features.Candidates.Commands;
 
 public static class SetCandidateStickyLocation
 {
-    [RequestAuthorize(Policy = SecurityPolicies.SeniorInternal)]
+    [RequestAuthorize(Policy = SecurityPolicies.Qa1)]
     public class Command : IRequest<Result>
     {
         public string? ParticipantId { get; set; }
         public string? Region { get; set; }
+        
+        [Description("Justification")]
+        public string? Justification { get;set; }
+        
+        [Description("Call Reference")]
+        public string? CallReference { get; set; }
+    }
+
+    public class Handler(ICandidateService candidateService, IUnitOfWork unitOfWork) : IRequestHandler<Command, Result>
+    {
+        public async Task<Result> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var participant = unitOfWork.DbContext.Participants.FirstOrDefault(x => x.Id == request.ParticipantId!);
+            
+            if(participant is not null)
+            {
+                var callResult = await candidateService.SetStickyLocation(request.ParticipantId!, request.Region!);
+                if(callResult is { Succeeded: true, Data: true })
+                {
+                    participant.AddNote(new Note()
+                    {
+                        Message = request.Justification!,
+                        CallReference = request.CallReference!
+                    });
+                    return Result.Success();
+                }
+                return callResult;
+            }
+            return Result.Failure();
+        }
     }
 
     public class Validator : AbstractValidator<Command>
@@ -34,6 +65,16 @@ public static class SetCandidateStickyLocation
                 .MinimumLength(4)
                 .Matches(ValidationConstants.AlphaNumeric)
                 .WithMessage(string.Format(ValidationConstants.AlphaNumericMessage, "Region"));
+
+            RuleFor(x => x.Justification)
+                .NotEmpty()
+                .Matches(ValidationConstants.Notes)
+                .WithMessage(string.Format(ValidationConstants.NotesMessage, "Justification"));
+            
+            RuleFor(x => x.CallReference)
+                .NotEmpty()
+                .Matches(ValidationConstants.Numbers)
+                .WithMessage(string.Format(ValidationConstants.NumbersMessage, "Call Reference"));
 
             RuleSet(ValidationConstants.RuleSet.MediatR, () =>
             {
