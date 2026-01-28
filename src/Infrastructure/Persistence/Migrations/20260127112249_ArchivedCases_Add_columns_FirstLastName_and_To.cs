b36194a2370c2b868d6c67947bf161c0a4a0631c
Paul Cooper <62419931+PaulCooperWorkJustice@users.noreply.github.com>
2026-01-28 11:53:51 +0000

    CFODEV-1418:  Add Migration and the component to the dashboard

---
+++ src/Infrastructure/Persistence/Migrations/20260127112249_ArchivedCases_Add_columns_FirstLastName_and_To.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cfo.Cats.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ArchivedCases_Add_columns_FirstLastName_and_To : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                schema: "Mi",
+                table: "ArchivedCase",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                schema: "Mi",
+                table: "ArchivedCase",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "To",
+                schema: "Mi",
+                table: "ArchivedCase",
+                type: "datetime2",
+                nullable: true);
+            
+            migrationBuilder.Sql("""
+                                     UPDATE Mi.ArchivedCase
+                                     SET FirstName = p.FirstName,
+                                         LastName = p.LastName,
+                                         [To] = eh.[To]
+                                     FROM Mi.ArchivedCase ac
+                                     JOIN Participant.Participant p ON ac.ParticipantId = p.Id
+                                     JOIN Participant.EnrolmentHistory eh ON ac.EnrolmentHistoryId = eh.Id
+                                 """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                schema: "Mi",
+                table: "ArchivedCase");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                schema: "Mi",
+                table: "ArchivedCase");
+
+            migrationBuilder.DropColumn(
+                name: "To",
+                schema: "Mi",
+                table: "ArchivedCase");
+        }
+    }
+}
