1b5c56679c5452b2f5655000b3ae77a818353202
Paul Cooper <62419931+PaulCooperWorkJustice@users.noreply.github.com>
2026-01-28 11:53:51 +0000

    CFODEV-1418:  Add Migration and the component to the dashboard

--- src/Infrastructure/Persistence/Migrations/20260127112249_ArchivedCases_Add_columns_FirstLastName_and_To.Designer.cs
+++ src/Infrastructure/Persistence/Migrations/20260127112249_ArchivedCases_Add_columns_FirstLastName_and_To.Designer.cs
@@ -4,6 +4,7 @@ using Cfo.Cats.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cfo.Cats.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260127112249_ArchivedCases_Add_columns_FirstLastName_and_To")]
+    partial class ArchivedCases_Add_columns_FirstLastName_and_To
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -3610,9 +3613,6 @@ namespace Cfo.Cats.Infrastructure.Persistence.Migrations
                     b.Property<Guid>("Id")
                         .HasColumnType("uniqueidentifier");
 
-                    b.Property<int>("AppIcon")
-                        .HasColumnType("int");
-
                     b.Property<int>("Colour")
                         .HasColumnType("int");
 
@@ -3640,8 +3640,8 @@ namespace Cfo.Cats.Infrastructure.Persistence.Migrations
 
                     b.Property<string>("Name")
                         .IsRequired()
-                        .HasMaxLength(25)
-                        .HasColumnType("nvarchar(25)");
+                        .HasMaxLength(15)
+                        .HasColumnType("nvarchar(15)");
 
                     b.Property<int>("Variant")
                         .HasColumnType("int");
