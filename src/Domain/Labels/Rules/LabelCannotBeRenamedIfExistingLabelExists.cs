e4623d8740de9ad8801af1b2d05fc9f0ba5399a5
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2026-02-04 10:00:11 +0000

    Fix spelling typo and add missing tests for LabelCannotBeRenamedIfExistingLabelExists

--- src/Domain/Labels/Rules/LabelCannotBeRenamedIfExistingLabelExists.cs
+++ src/Domain/Labels/Rules/LabelCannotBeRenamedIfExistingLabelExists.cs
@@ -8,7 +8,7 @@ namespace Cfo.Cats.Domain.Labels.Rules;
 
 public class LabelCannotBeRenamedIfExistingLabelExists(string newName, string oldName, string? contractId,  ILabelCounter labelCounter) : IBusinessRule
 {
-    public string Message => $"Cannot rename label from {oldName} to {newName} as the label alread exists.";
+    public string Message => $"Cannot rename label from {oldName} to {newName} as the label already exists.";
 
     public bool IsBroken() => labelCounter.CountVisibleLabels(newName, contractId) > 0;
 }
