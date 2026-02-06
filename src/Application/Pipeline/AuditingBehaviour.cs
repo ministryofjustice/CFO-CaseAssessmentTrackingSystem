f4278f1d185862333d8b1cd1ea9ef97c6f08ecd3
Paul Cooper <62419931+PaulCooperWorkJustice@users.noreply.github.com>
2026-02-06 15:18:09 +0000

    CFODEV-1553:  Participant search by Id component. Also fixed a bug in auditing behaviour where if trying to save a participant id that doesnt exist fails with unhandled exception

--- src/Application/Pipeline/AuditingBehaviour.cs
+++ src/Application/Pipeline/AuditingBehaviour.cs
@@ -3,7 +3,6 @@ namespace Cfo.Cats.Application.Pipeline;
 public class AccessAuditingBehaviour<TRequest, TResponse>(IUnitOfWork unitOfWork, ICurrentUserService currentUserService) : IPipelineBehavior<TRequest, TResponse>
     where TRequest : IAuditableRequest<TResponse>
 {
-
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         var span = SentrySdk.GetSpan()?
@@ -12,18 +11,26 @@ public class AccessAuditingBehaviour<TRequest, TResponse>(IUnitOfWork unitOfWork
         var response = await next(cancellationToken);
         try
         {
-            string currentUser = currentUserService.UserId ?? Guid.Empty.ToString();
+            var currentUser = currentUserService.UserId ?? Guid.Empty.ToString();
             
-            ParticipantAccessAuditTrail t = ParticipantAccessAuditTrail.Create(
-                typeof(TRequest).FullName!.Split('.').Last(), 
-                request.Identifier(), 
-                currentUser);
-            
-            unitOfWork.DbContext.AccessAuditTrails.Add(t);
+            // Check if the participant exists before auditing access to avoid unhandled exceptions for non-existent participants as forign keys in the audit trail on particpant table
+            var exists = await unitOfWork.DbContext.Participants
+                .AnyAsync(p => p.Id == request.Identifier(), cancellationToken);
+
+            if (exists)
+            {
+                var auditEntry = ParticipantAccessAuditTrail.Create(
+                    typeof(TRequest).FullName!.Split('.').Last(),
+                    request.Identifier(),
+                    currentUser);
+                
+                unitOfWork.DbContext.AccessAuditTrails.Add(auditEntry);
+            }
         }
-        catch
+        catch (Exception ex)
         {
-            // do nothing. We do not want audit to break the system.
+            // Ensure that audit exceptions do not break normal flow
+            span?.SetExtra("AuditError", ex.Message);
         }
         finally
         {
@@ -31,6 +38,5 @@ public class AccessAuditingBehaviour<TRequest, TResponse>(IUnitOfWork unitOfWork
         }
 
         return response;
-
     }
 }
