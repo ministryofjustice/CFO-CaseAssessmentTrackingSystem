53d26c21fdcc9d59690e5b13da7c26ba9eb2acbe
Paul Cooper <62419931+PaulCooperWorkJustice@users.noreply.github.com>
2026-02-13 12:40:21 +0000

    CFODEV-1418: Make contract Id nullable on archive case table.  Tidied up code.

---
+++ src/Infrastructure/Persistence/Migrations/20260213123038_ArchivedCase_Change_contractID_To_Nullable.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cfo.Cats.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ArchivedCase_Change_contractID_To_Nullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ContractId",
+                schema: "Mi",
+                table: "ArchivedCase",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldMaxLength: 12);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ContractId",
+                schema: "Mi",
+                table: "ArchivedCase",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldMaxLength: 12,
+                oldNullable: true);
+        }
+    }
+}
