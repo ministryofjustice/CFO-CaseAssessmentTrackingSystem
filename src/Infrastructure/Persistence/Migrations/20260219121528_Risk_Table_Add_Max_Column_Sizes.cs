ca1fea2292043d20de6af530e363de575240fc68
Paul Cooper <62419931+PaulCooperWorkJustice@users.noreply.github.com>
2026-02-23 14:10:48 +0000

    CFODEV-980: the migration for column widths

---
+++ src/Infrastructure/Persistence/Migrations/20260219121528_Risk_Table_Add_Max_Column_Sizes.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cfo.Cats.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Risk_Table_Add_Max_Column_Sizes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ReferrerName",
+                schema: "Participant",
+                table: "Risk",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ReferrerEmail",
+                schema: "Participant",
+                table: "Risk",
+                type: "nvarchar(320)",
+                maxLength: 320,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ReferrerName",
+                schema: "Participant",
+                table: "Risk",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ReferrerEmail",
+                schema: "Participant",
+                table: "Risk",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(320)",
+                oldMaxLength: 320,
+                oldNullable: true);
+        }
+    }
+}
