f8a2bbd033d08a27edeb3fb5e24edf6cdb3dbf28
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2026-03-10 14:13:06 +0000

    Participants V2 optional from menu

--- src/Application/Features/Participants/Specifications/ParticipantListView.cs
+++ src/Application/Features/Participants/Specifications/ParticipantListView.cs
@@ -14,3 +14,21 @@ public enum ParticipantListView
     [Description("All")] All
 }
 
+public static class ParticipantListViewExtensions
+{
+    public static string GetDescription(this ParticipantListView e)
+        => e switch
+        {
+            ParticipantListView.Default => "Default",
+            ParticipantListView.Identified => "Identified",
+            ParticipantListView.Enrolling => "Enrolling",
+            ParticipantListView.SubmittedToProvider => "PQA",
+            ParticipantListView.SubmittedToQa => "CFO QA",
+            ParticipantListView.SubmittedToAny => "QA (any)",
+            ParticipantListView.Approved => "Approved",
+            ParticipantListView.Dormant => "Dormant",
+            ParticipantListView.Archived => "Archived",
+            ParticipantListView.All => "All",
+            _ => throw new ArgumentOutOfRangeException("Unknown list view type")
+        };
+}
