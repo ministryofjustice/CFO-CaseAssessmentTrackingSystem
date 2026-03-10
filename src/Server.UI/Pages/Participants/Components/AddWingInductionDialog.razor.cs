fb1ad1c743b44804019a3536909674f5b770083c
Vinay Saini <vinay.saini@justice.gov.uk>
2026-03-10 12:56:41 +0000

    Replaces location search with location dropdown list
    - Replaced MudAutocomplete with MudSelect + @foreach loop over MudSelectItems
    - Made it Clearable="true" for consistency
    - Removed the SearchLocations method since it's no longer needed with the dropdown

---
+++ src/Server.UI/Pages/Participants/Components/AddWingInductionDialog.razor.cs
@@ -0,0 +1,76 @@
+using Cfo.Cats.Application.Features.Inductions.Commands;
+using Cfo.Cats.Application.Features.Locations.DTOs;
+using Cfo.Cats.Application.Features.Participants.Queries;
+using Cfo.Cats.Infrastructure.Constants;
+
+namespace Cfo.Cats.Server.UI.Pages.Participants.Components;
+
+public partial class AddWingInductionDialog
+{
+    private MudForm? form;
+
+    [CascadingParameter]
+    private IMudDialogInstance MudDialog { get; set; } = default!;
+
+    [EditorRequired]
+    [Parameter]
+    public LocationDto[]? Locations { get; set; }
+
+    [EditorRequired]
+    [Parameter]
+    public AddWingInduction.Command? Model { get; set; }
+
+    private bool saving;
+
+    private bool hasParticpantBeenAtThisLocationOnThisDate;
+
+    private void Cancel()
+    {
+        MudDialog.Cancel();
+    }
+
+    private async Task Submit()
+    {
+        try
+        {
+            saving = true;
+
+            await form!.Validate();
+
+            if (form!.IsValid == false)
+            {
+                return;
+            }
+
+            var result = await GetNewMediator().Send(Model!);
+            if (result.Succeeded)
+            {
+                MudDialog.Close(DialogResult.Ok(true));
+                Snackbar.Add(ConstantString.SaveSuccess, Severity.Info);
+            }
+            else
+            {
+                Snackbar.Add(result.ErrorMessage, Severity.Error);
+            }
+        }
+        finally
+        {
+            saving = false;
+        }
+    }
+
+    private async Task CheckParticipantBeenAtThisLocationOnDate()
+    {
+        if (Model?.InductionDate == null || Model?.Location?.Id == null)
+        {
+            return;
+        }
+
+        hasParticpantBeenAtThisLocationOnThisDate = await GetNewMediator().Send(new GetParticipantWasAtThisLocationCheck.Query()
+        {
+            ParticipantId = Model!.ParticipantId,
+            LocationId = Model.Location!.Id,
+            DateAtLocation = Model.InductionDate
+        });
+    }
+}
