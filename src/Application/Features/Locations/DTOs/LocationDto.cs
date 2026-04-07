7fec78598d7d61e6e003c56166d77f70d40805d5
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2026-04-07 11:20:46 +0100

    hotfix/location mapping (#1003)
    * Re-add mapping for now, other mappers rely on it
    
    * Re-add mapping for now, other mappers rely on it

--- src/Application/Features/Locations/DTOs/LocationDto.cs
+++ src/Application/Features/Locations/DTOs/LocationDto.cs
@@ -1,4 +1,6 @@
-﻿namespace Cfo.Cats.Application.Features.Locations.DTOs;
+﻿using Cfo.Cats.Domain.Entities.Administration;
+
+namespace Cfo.Cats.Application.Features.Locations.DTOs;
 
 public record LocationDto
 {
@@ -18,5 +20,17 @@ public record LocationDto
     public int? ParentLocationId { get; init; }
     
     public string? ParentLocationName { get; set; }
+    
+    private class Mapping : Profile
+    {
+        public Mapping() =>
+            CreateMap<Location, LocationDto>()
+                .ForMember(t => t.Id, o => o.MapFrom(s => s.Id))
+                .ForMember(t => t.GenderProvision, o => o.MapFrom(s => s.GenderProvision))
+                .ForMember(t => t.LocationType, o => o.MapFrom(s => s.LocationType))
+                .ForMember(t => t.ParentLocationId, o => o.MapFrom(s => s.ParentLocation!.Id))
+                .ForMember(t => t.ParentLocationName, o => o.MapFrom(s => s.ParentLocation!.Name))
+                .ForMember(t => t.Name, o => o.MapFrom(s => s.Name));
+    }
    
 }
