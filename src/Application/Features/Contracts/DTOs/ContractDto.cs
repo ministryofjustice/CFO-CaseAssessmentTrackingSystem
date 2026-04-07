d116e0abc050c34bd580990bd9d3de98a8d3a8b0
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2026-04-07 10:50:44 +0100

    Re-add mapping for now, other mappers rely on it

--- src/Application/Features/Contracts/DTOs/ContractDto.cs
+++ src/Application/Features/Contracts/DTOs/ContractDto.cs
@@ -1,8 +1,19 @@
-﻿namespace Cfo.Cats.Application.Features.Contracts.DTOs;
+﻿using Cfo.Cats.Domain.Entities.Administration;
+
+namespace Cfo.Cats.Application.Features.Contracts.DTOs;
 
 public class ContractDto
 {
     public required string Id { get; set; }
     public required string Name { get; set; }
     public required string TenantId { get; set; }
+
+    private class Mapper : Profile
+    {
+        public Mapper() =>
+            CreateMap<Contract, ContractDto>()
+                .ForMember(t => t.Id, o => o.MapFrom(s => s.Id))
+                .ForMember(t => t.Name, o => o.MapFrom(s => s.Description))
+                .ForMember(t => t.TenantId, o => o.MapFrom(s => s.Tenant!.Id));
+    }
 }
