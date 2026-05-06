b3fada4e0daa7d2e5f664588df953090b356ba36
Sam Gibson <Sam.Gibson@justice.gov.uk>
2026-05-06 14:13:33 +0100

    Only seed initiative objectives if there are none

--- src/DatabaseSeeding/Scripts/0059_InitiativeObjectives.sql
+++ src/DatabaseSeeding/Scripts/0059_InitiativeObjectives.sql
@@ -1,25 +1,26 @@
-
 -- ============================================================
 -- Seed: Initiative Objectives
 -- Links objectives to initiatives where the objective description
 -- starts with the initiative code,
 -- e.g. 'IF-25-15 RR Barista Training: 17/09/2025' -> IF-25-15.
 -- ============================================================
-
-INSERT INTO [Participant].[InitiativeObjective]
-    ([Id], [ObjectiveId], [InitiativeId], [ParticipantId], [Created], [CreatedBy])
-SELECT
-    NEWID(),
-    o.[Id],
-    i.[Id],
-    pp.[ParticipantId],
-    GETUTCDATE(),
-    N'2a9b3450-1feb-4be3-ab94-24e64cd34829'
-FROM        [Participant].[Objective]   o
-JOIN        [Participant].[PathwayPlan] pp ON pp.[Id]   = o.[PathwayPlanId]
-JOIN        [Configuration].[Initiative] i ON o.[Description] LIKE i.[Code] + '%'
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM [Participant].[InitiativeObjective] io
-    WHERE io.[ObjectiveId] = o.[Id]
-);
+IF NOT EXISTS(SELECT 1 FROM [Participant].[InitiativeObjective])
+BEGIN
+    INSERT INTO [Participant].[InitiativeObjective]
+        ([Id], [ObjectiveId], [InitiativeId], [ParticipantId], [Created], [CreatedBy])
+    SELECT
+        NEWID(),
+        o.[Id],
+        i.[Id],
+        pp.[ParticipantId],
+        GETUTCDATE(),
+        N'2a9b3450-1feb-4be3-ab94-24e64cd34829'
+    FROM        [Participant].[Objective]   o
+    JOIN        [Participant].[PathwayPlan] pp ON pp.[Id]   = o.[PathwayPlanId]
+    JOIN        [Configuration].[Initiative] i ON o.[Description] LIKE i.[Code] + '%'
+    WHERE NOT EXISTS (
+        SELECT 1
+        FROM [Participant].[InitiativeObjective] io
+        WHERE io.[ObjectiveId] = o.[Id]
+    );
+END;
