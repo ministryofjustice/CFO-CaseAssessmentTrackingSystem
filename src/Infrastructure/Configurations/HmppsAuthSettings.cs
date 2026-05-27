c64897444dc93dd1f29bfa82fab1a0443160c4d4
Sam Gibson <Sam.Gibson@justice.gov.uk>
2026-05-27 09:15:34 +0100

    Adds HMPPS Auth support

---
+++ src/Infrastructure/Configurations/HmppsAuthSettings.cs
@@ -0,0 +1,14 @@
+namespace Cfo.Cats.Infrastructure.Configurations;
+
+public class HmppsAuthSettings
+{
+    public const string Key = "HmppsAuth";
+
+    public string Instance { get; set; } = string.Empty;
+    public string ClientId { get; set; } = string.Empty;
+    public string ClientSecret { get; set; } = string.Empty;
+    public string CallbackPath { get; set; } = "/signin-hmpps";
+    public string AuthorizationEndpoint { get; set; } = string.Empty;
+    public string TokenEndpoint { get; set; } = string.Empty;
+    public string UserInformationEndpoint { get; set; } = string.Empty;
+}
