38a9aaf1c8a59f4566734b3bf35384d3001ff7e4
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2026-06-19 08:36:19 +0100

    rename ICommand -> IExternalCommand as this now conflicts with the Mediator package

--- src/Application/Features/Identity/MessageBus/NotifyInactiveUserCommand.cs
+++ src/Application/Features/Identity/MessageBus/NotifyInactiveUserCommand.cs
@@ -2,4 +2,4 @@ using Cfo.Cats.Application.Common.MessageBus;
 
 namespace Cfo.Cats.Application.Features.Identity.MessageBus;
 
-public record NotifyInactiveUserCommand(string Email) : ICommand;
+public record NotifyInactiveUserCommand(string Email) : IExternalCommand;
