9d7aaf209d078914deca542e0c0a8e68eade789a
Sam Gibson <140488216+samgibsonmoj@users.noreply.github.com>
2026-06-25 11:20:55 +0100

    CFODEV-1735: Activities "all" screen (#1161)
    * Add "Activities" screen to participants workspace
    
    * Add Activities document export
    
    * Refactoring
    
    * Include all activities (not just qa-ables)
    
    * Replace MudChipSet type filter with singular dropdown
    
    * Include participant name / ID in activity/ete edit menu
    
    * Add ability to show document on activity list
    
    * Don't load the notes until they are used
    
    * Add descriptive titles for dialogs
    
    * Move into components
    
    * Cleanup RenderFragment
    
    * Ignore QA1 returns
    
    * Remove navigation on row click
    
    * Use Filled instead of Outlined buttons
    
    * Add activities to menu

---
+++ src/Application/Features/Activities/Specifications/AllActivitiesAdvancedSpecification.cs
@@ -0,0 +1,24 @@
+﻿using Cfo.Cats.Domain.Entities.Activities;
+
+namespace Cfo.Cats.Application.Features.Activities.Specifications;
+
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+public class AllActivitiesAdvancedSpecification : Specification<Activity>
+{
+    public AllActivitiesAdvancedSpecification(AllActivitiesAdvancedFilter filter)
+    {
+        var typeFilter = filter.TypeFilter.HasValue ? ActivityType.FromValue(filter.TypeFilter.Value) : null;
+
+        Query.Where(a => a.Participant.EnrolmentStatus != EnrolmentStatus.ArchivedStatus.Value)
+            .Where(a => a.TenantId.StartsWith(filter.UserProfile.TenantId!))
+            .Where(a => a.TenantId.StartsWith(filter.TenantId!), string.IsNullOrEmpty(filter.TenantId) == false)
+            .Where(a => a.OwnerId == filter.OwnerId, string.IsNullOrEmpty(filter.OwnerId) == false)
+            .Where(a => a.TookPlaceAtLocation.Id == filter.LocationId, filter.LocationId is not null)
+            .Where(a => a.Type == typeFilter, typeFilter is not null)
+            .Where(a => a.Status == ActivityStatus.FromValue(filter.Status!.Value), filter.Status is not null)
+            .Where(a => string.IsNullOrWhiteSpace(filter.Keyword)
+                        || a.Participant.FirstName.Contains(filter.Keyword)
+                        || a.Participant.LastName.Contains(filter.Keyword)
+                        || a.ParticipantId.Contains(filter.Keyword));
+    }
+}
