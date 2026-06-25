f3413c488d2558dd444d3e417185ef30d9766eb6
Sam Gibson <140488216+samgibsonmoj@users.noreply.github.com>
2026-06-25 11:20:55 +0100

    CFODEV-1735: Activities "all" screen (#1161)
    * Add "Activities" screen to participants workspace
    
    * Add Activities document export
    
    * Refactoring
    
    * Include all activities (not just qa-ables)
    
    * Replace MudChipSet type filter with singular dropdown
    
    * Include participant name / ID in activity/ete edit menu
    
    * Add ability to show document on activity list
    
    * Don't load the notes until they are used
    
    * Add descriptive titles for dialogs
    
    * Move into components
    
    * Cleanup RenderFragment
    
    * Ignore QA1 returns
    
    * Remove navigation on row click
    
    * Use Filled instead of Outlined buttons
    
    * Add activities to menu

---
+++ src/Server.UI/Pages/Workspaces/Participants/Services/ActivitiesSessionStorage.cs
@@ -0,0 +1,47 @@
+using Cfo.Cats.Application.Features.Activities.Queries;
+using Cfo.Cats.Server.UI.Services;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace Cfo.Cats.Server.UI.Pages.Workspaces.Participants.Services;
+
+public class ActivitiesSessionStorage(ProtectedSessionStorage protectedSessionStorage, ICurrentUserService currentUserService)
+    : CatsSessionStorage<ActivitiesSessionData>(protectedSessionStorage, currentUserService)
+{
+}
+
+public record ActivitiesSessionData
+{
+    public ActivitiesSessionData()
+    {
+    }
+
+    internal static ActivitiesSessionData FromQuery(AllActivitiesWithPagination.Query query, bool tabular)
+        => new()
+        {
+            TenantId = query.TenantId,
+            OwnerId = query.OwnerId,
+            LocationId = query.LocationId,
+            LocationName = query.LocationName,
+            Status = query.Status,
+            TypeFilter = query.TypeFilter,
+            ReturnedWithinDays = query.ReturnedWithinDays,
+            Keyword = query.Keyword,
+            OrderBy = query.OrderBy,
+            SortDirection = query.SortDirection,
+            PageNumber = query.PageNumber,
+            Tabular = tabular
+        };
+
+    public string? TenantId { get; init; }
+    public string? OwnerId { get; init; }
+    public int? LocationId { get; init; }
+    public string? LocationName { get; init; }
+    public int? Status { get; init; }
+    public int? TypeFilter { get; init; }
+    public int? ReturnedWithinDays { get; init; }
+    public string? Keyword { get; init; }
+    public string? OrderBy { get; init; }
+    public string? SortDirection { get; init; }
+    public int PageNumber { get; init; } = 1;
+    public bool Tabular { get; init; }
+}
