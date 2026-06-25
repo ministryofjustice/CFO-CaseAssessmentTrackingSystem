480a007ce206bef9ea99ff49d75ac422a77e144e
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2026-06-25 09:39:33 +0100

    Replace sidebar with menu items (#1164)
    * Moves menu and search to topbar
    
    * Update src/Server.UI/Services/Navigation/AsyncMenuService.cs
    
    Co-authored-by: Sam Gibson <140488216+samgibsonmoj@users.noreply.github.com>
    
    * Update src/Server.UI/Services/Navigation/AsyncMenuService.cs
    
    Co-authored-by: Sam Gibson <140488216+samgibsonmoj@users.noreply.github.com>
    
    * Addresses issues raised in PR
    
    - Moves the login to the menu
    
    ---------
    
    Co-authored-by: Sam Gibson <140488216+samgibsonmoj@users.noreply.github.com>

--- src/Server.UI/Models/NavigationMenu/MenuSectionModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Cfo.Cats.Server.UI.Models.NavigationMenu;
-
-public class MenuSectionModel
-{
-    public string Title { get; set; } = string.Empty;
-    public List<MenuSectionItemModel> SectionItems { get; init; } = [];
-}
--- src/Server.UI/Models/NavigationMenu/MenuSectionSubItemModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Cfo.Cats.Server.UI.Models.NavigationMenu;
-
-public class MenuSectionSubItemModel
-{
-    public string Title { get; set; } = string.Empty;
-    public string? Href { get; set; }
-    public PageStatus PageStatus { get; set; } = PageStatus.Completed;
-    public string? Target { get; set; }
-}
