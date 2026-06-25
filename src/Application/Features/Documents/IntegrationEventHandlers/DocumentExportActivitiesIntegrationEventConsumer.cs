01a3e87d395302b4ee900686cbf033a1e7ac250d
Sam Gibson <140488216+samgibsonmoj@users.noreply.github.com>
2026-06-25 11:20:55 +0100

    CFODEV-1735: Activities "all" screen (#1161)
    * Add "Activities" screen to participants workspace
    
    * Add Activities document export
    
    * Refactoring
    
    * Include all activities (not just qa-ables)
    
    * Replace MudChipSet type filter with singular dropdown
    
    * Include participant name / ID in activity/ete edit menu
    
    * Add ability to show document on activity list
    
    * Don't load the notes until they are used
    
    * Add descriptive titles for dialogs
    
    * Move into components
    
    * Cleanup RenderFragment
    
    * Ignore QA1 returns
    
    * Remove navigation on row click
    
    * Use Filled instead of Outlined buttons
    
    * Add activities to menu

---
+++ src/Application/Features/Documents/IntegrationEventHandlers/DocumentExportActivitiesIntegrationEventConsumer.cs
@@ -0,0 +1,87 @@
+using Cfo.Cats.Application.Features.Activities.DTOs;
+using Cfo.Cats.Application.Features.Activities.Queries;
+using Cfo.Cats.Application.Features.Documents.IntegrationEvents;
+using Cfo.Cats.Domain.Entities.Documents;
+using Newtonsoft.Json;
+using Rebus.Handlers;
+
+namespace Cfo.Cats.Application.Features.Documents.IntegrationEventHandlers;
+
+public class DocumentExportActivitiesIntegrationEventConsumer(
+    IUnitOfWork unitOfWork,
+    IExcelService excelService,
+    IUploadService uploadService,
+    IDomainEventDispatcher domainEventDispatcher,
+    ILogger<DocumentExportActivitiesIntegrationEventConsumer> logger) : IHandleMessages<ExportDocumentIntegrationEvent>
+{
+    public async Task Handle(ExportDocumentIntegrationEvent context)
+    {
+        if (context.Key != DocumentTemplate.Activities.Name)
+        {
+            logger.LogDebug("Export document not supported by this handler");
+            return;
+        }
+
+        var document = await unitOfWork.DbContext.GeneratedDocuments.FindAsync(context.DocumentId);
+
+        if (document is null)
+        {
+            logger.LogError("Export activities document event raised for a document that does not exist. ({DocumentId})", context.DocumentId);
+            return;
+        }
+
+        try
+        {
+            var request = JsonConvert.DeserializeObject<AllActivitiesWithPagination.Query>(context.SearchCriteria!)
+                          ?? throw new Exception();
+
+            request.PageSize = int.MaxValue;
+
+            // Hack: call handler directly (skips Authorization pipeline, as we're outside of the HttpContext).
+            var data = await new AllActivitiesWithPagination.Handler(unitOfWork).Handle(request!, CancellationToken.None);
+
+            var results = await excelService.ExportAsync(data.Data!.Items!,
+                new Dictionary<string, Func<ActivityPaginationDto, object?>>
+                {
+                    { "Participant Id", item => item.ParticipantId },
+                    { "Participant Name", item => item.Participant },
+                    { "Activity", item => item.Definition.Category.Name },
+                    { "Activity Type", item => item.Definition.Type.Name },
+                    { "Status", item => item.Status.Name },
+                    { "Submitted By", item => item.SubmittedBy },
+                    { "Took Place At", item => item.TookPlaceAtLocationName },
+                    { "Commenced on", item => item.CommencedOn },
+                    { "Submitted", item => item.Created },
+                    { "Approved on", item => item.ApprovedOn },
+                    { "Last Modified", item => item.LastModified },
+                    { "Additional Information", item => item.AdditionalInformation },
+                }
+            );
+
+            var uploadRequest = new UploadRequest(document.Title!, UploadType.Document, results);
+
+            var result = await uploadService.UploadAsync($"MyDocuments/{context.UserId}", uploadRequest);
+
+            if (result.Succeeded)
+            {
+                document
+                    .WithStatus(DocumentStatus.Available)
+                    .SetURL(result);
+            }
+            else
+            {
+                logger.LogError("Failed to upload activities document {DocumentId}: {Errors}", context.DocumentId, string.Join(", ", result.Errors));
+                document.WithStatus(DocumentStatus.Error);
+            }
+
+            await domainEventDispatcher.DispatchEventsAsync(unitOfWork.DbContext, CancellationToken.None);
+            await unitOfWork.CommitTransactionAsync();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error exporting activities document {DocumentId}: {ErrorMessage}", context.DocumentId, ex.Message);
+            document.WithStatus(DocumentStatus.Error);
+            await unitOfWork.CommitTransactionAsync();
+        }
+    }
+}
