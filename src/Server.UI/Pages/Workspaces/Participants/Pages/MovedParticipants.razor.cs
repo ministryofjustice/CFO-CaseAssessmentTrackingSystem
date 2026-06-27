6714853edea7302f47b56e934d938f8d7342c9ce
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2026-06-27 15:48:28 +0100

    Organisation and reformatting of Participants workspace (#1175)

--- src/Server.UI/Pages/Workspaces/Participants/Pages/MovedParticipants.razor.cs
+++ src/Server.UI/Pages/Workspaces/Participants/Pages/MovedParticipants.razor.cs
@@ -1,10 +1,17 @@
 using Cfo.Cats.Application.Features.Participants.Queries;
+using Cfo.Cats.Server.UI.Pages.Workspaces.Participants.Services;
 
-namespace Cfo.Cats.Server.UI.Pages.Participants;
+namespace Cfo.Cats.Server.UI.Pages.Workspaces.Participants.Pages;
 
 public partial class MovedParticipants
 {
     private IEnumerable<GetParticipantsWithAccessEndingSoon.ParticipantWithAccessEndingSoonDto> participantAccess = [];
+
+    private IReadOnlyList<BreadcrumbItem> _breadCrumbs = [
+        new BreadcrumbItem(ParticipantLinks.Home.Title, ParticipantLinks.Home.Url, false),
+        new BreadcrumbItem(ParticipantLinks.MovedParticipants.Title, ParticipantLinks.MovedParticipants.Url, true)
+    ];
+
     protected override async Task OnInitializedAsync()
     {
         participantAccess = await GetNewMediator().Send(new GetParticipantsWithAccessEndingSoon.Query());
