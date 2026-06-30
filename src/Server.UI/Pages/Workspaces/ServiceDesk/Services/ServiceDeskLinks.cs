cb9534d198d0b2b525cbe4ddddec922eda1b434f
Vinay Saini <vinay.saini@justice.gov.uk>
2026-06-30 16:54:56 +0100

    Home page clickable list items
    -Removes View Queue button
    -Enrolments and Activities First Pass and Second Pass shows tooltip 'Click to Get Next' and assigns next available item to the user, if available else displayserror message.
    -adds description to the links on home page (needs more thought)

--- src/Server.UI/Pages/Workspaces/ServiceDesk/Services/ServiceDeskLinks.cs
+++ src/Server.UI/Pages/Workspaces/ServiceDesk/Services/ServiceDeskLinks.cs
@@ -13,10 +13,10 @@ namespace Cfo.Cats.Server.UI.Pages.Workspaces.ServiceDesk.Services;
 public static class ServiceDeskLinks
 {
     public static BreadcrumbLinkModel Home => new("Service Desk", string.Empty, "/pages/workspace/servicedesk");
-    public static BreadcrumbLinkModel ActivitiesQueue = new("Activities Queue", "Activities Queue", $"{Home.Href}/activities/queue");
-    public static BreadcrumbLinkModel ActivitiesFeedback = new("Activities Feedback", "Activities Feedback", $"{Home.Href}/activities/feedback");
-    public static BreadcrumbLinkModel EnrolmentsQueue = new("Enrolments Queue", "Enrolments Queue", $"{Home.Href}/enrolments/queue");
-    public static BreadcrumbLinkModel EnrolmentsFeedback = new("Enrolments Feedback", "Enrolments Feedback", $"{Home.Href}/enrolments/feedback");
+    public static BreadcrumbLinkModel ActivitiesQueue = new("Activities Queue", "Access relevant Activities queue(s)", $"{Home.Href}/activities/queue");
+    public static BreadcrumbLinkModel ActivitiesFeedback = new("Activities Feedback", "Access Activities feedback", $"{Home.Href}/activities/feedback");
+    public static BreadcrumbLinkModel EnrolmentsQueue = new("Enrolments Queue", "Access relevant Enrolments queue(s)", $"{Home.Href}/enrolments/queue");
+    public static BreadcrumbLinkModel EnrolmentsFeedback = new("Enrolments Feedback", "Access Enrolments feedback", $"{Home.Href}/enrolments/feedback");
 
 }
 
