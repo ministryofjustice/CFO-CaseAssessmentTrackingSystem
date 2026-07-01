14a5ed122d8de2460b45815b1b8838095dfa78df
Paul Cooper <62419931+PaulCooperWorkJustice@users.noreply.github.com>
2026-07-01 11:39:32 +0100

    CFODEV-1741:  Move jobs and system management to Administration workspace (#1181)
    * Initial work so far.  Moved Jobmanagement to new admin workspace
    
    Signed-off-by: Paul Cooper <Paul.Cooper5@Justice.gov.uk>
    
    * Added jobs and cache management components to Administration workspace.  Changed the index so it matches the breadcrumbs pattern.
    
    Signed-off-by: Paul Cooper <Paul.Cooper5@Justice.gov.uk>
    
    * Fixed breadcrumb bug for cache management
    
    Signed-off-by: Paul Cooper <Paul.Cooper5@Justice.gov.uk>
    
    * Moved sync component into administration.  Also added security to the pages and only create the links if they have access.
    
    Signed-off-by: Paul Cooper <Paul.Cooper5@Justice.gov.uk>
    
    * Moved Synchcomponents back to original place.  Added security to Admin page.
    
    Signed-off-by: Paul Cooper <Paul.Cooper5@Justice.gov.uk>
    
    ---------
    
    Signed-off-by: Paul Cooper <Paul.Cooper5@Justice.gov.uk>

---
+++ src/Server.UI/Pages/Workspaces/Administration/Pages/Index.razor.cs
@@ -0,0 +1,37 @@
+using Cfo.Cats.Server.UI.Models.Breadcrumb;
+using Cfo.Cats.Server.UI.Pages.Workspaces.Administration.Services;
+using Cfo.Cats.Application.SecurityConstants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Cfo.Cats.Server.UI.Pages.Workspaces.Administration.Pages;
+
+[Authorize(Policy = SecurityPolicies.Internal)]
+public partial class Index
+{
+    [CascadingParameter] public Task<AuthenticationState> AuthState { get; set; } = null!;
+
+    private BreadcrumbLinkModel[] Links { get; set; } = [];
+
+    private bool _showJobManagement;
+
+    protected override async Task OnInitializedAsync()
+    {
+        var state = await AuthState;
+
+        var canAccessSystemSupport =
+            (await AuthService.AuthorizeAsync(state.User, SecurityPolicies.SystemSupportFunctions)).Succeeded;
+
+        _showJobManagement = canAccessSystemSupport;
+
+        List<BreadcrumbLinkModel> links = [];
+
+        if (_showJobManagement)
+        {
+            links.Add(AdministrationLinks.Jobs);
+            links.Add(AdministrationLinks.CacheManagement);
+        }
+
+        Links = links.ToArray();
+    }
+}
---
+++ src/Server.UI/Pages/Workspaces/Administration/Services/AdministrationLinks.cs
@@ -0,0 +1,11 @@
+using Cfo.Cats.Server.UI.Models.Breadcrumb;
+
+namespace Cfo.Cats.Server.UI.Pages.Workspaces.Administration.Services;
+
+public static class AdministrationLinks
+{
+    public static BreadcrumbLinkModel Home => new("Administration", "", "/pages/workspace/Administration");
+    public static BreadcrumbLinkModel Jobs = new("Jobs", "Job Scheduler", $"{Home.Href}/Jobs");
+    public static BreadcrumbLinkModel CacheManagement = new("Cache Management", "Cache Management", $"{Home.Href}/CacheManagement");
+ }
+ 
