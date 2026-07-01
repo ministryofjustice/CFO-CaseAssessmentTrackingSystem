4f9f6b1aea6cc25bdbee630242828001348f1fef
Paul Cooper <62419931+PaulCooperWorkJustice@users.noreply.github.com>
2026-07-01 11:39:32 +0100

    CFODEV-1741:  Move jobs and system management to Administration workspace (#1181)
    * Initial work so far.  Moved Jobmanagement to new admin workspace
    
    Signed-off-by: Paul Cooper <Paul.Cooper5@Justice.gov.uk>
    
    * Added jobs and cache management components to Administration workspace.  Changed the index so it matches the breadcrumbs pattern.
    
    Signed-off-by: Paul Cooper <Paul.Cooper5@Justice.gov.uk>
    
    * Fixed breadcrumb bug for cache management
    
    Signed-off-by: Paul Cooper <Paul.Cooper5@Justice.gov.uk>
    
    * Moved sync component into administration.  Also added security to the pages and only create the links if they have access.
    
    Signed-off-by: Paul Cooper <Paul.Cooper5@Justice.gov.uk>
    
    * Moved Synchcomponents back to original place.  Added security to Admin page.
    
    Signed-off-by: Paul Cooper <Paul.Cooper5@Justice.gov.uk>
    
    ---------
    
    Signed-off-by: Paul Cooper <Paul.Cooper5@Justice.gov.uk>

---
+++ src/Server.UI/Pages/Workspaces/Administration/Components/CacheControl.razor.cs
@@ -0,0 +1,18 @@
+﻿using Cfo.Cats.Application.SecurityConstants;
+using Microsoft.AspNetCore.Authorization;
+using ZiggyCreatures.Caching.Fusion;
+
+namespace Cfo.Cats.Server.UI.Pages.Workspaces.Administration.Components;
+
+[Authorize(Policy = SecurityPolicies.SystemSupportFunctions)]
+public partial class CacheControl
+{
+    [Inject] 
+    private IFusionCache FusionCache { get; set; } = null!;
+
+    private async Task ClearCache()
+    {
+        await FusionCache.ClearAsync();
+        Snackbar.Add($"Cache cleared", Severity.Info);
+    }
+}
