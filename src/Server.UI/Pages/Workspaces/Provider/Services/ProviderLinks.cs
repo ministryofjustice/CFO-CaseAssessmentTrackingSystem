ccf0166c8b3aa235d92d30a53a871feb67decbae
Sam Gibson <140488216+samgibsonmoj@users.noreply.github.com>
2026-07-03 07:53:14 +0100

    CFODEV-1468: Move 'Support Worker' and 'Tenant' Dashboards to 'Provider' workspace (#1189)
    * Move 'Support Worker' and 'Tenant' Dashboards to 'Provider' workspace
    
    * Make components more uniform in style

--- src/Server.UI/Pages/Workspaces/Provider/Services/ProviderLinks.cs
+++ src/Server.UI/Pages/Workspaces/Provider/Services/ProviderLinks.cs
@@ -6,4 +6,6 @@ public static class ProviderLinks
 {
     public static BreadcrumbLinkModel Home => new ( "Provider", "" ,"/pages/workspace/provider");
     public static BreadcrumbLinkModel CaseWorkload => new ( "Case Workload", "An overview of how cases are spread across your team" , $"{Home.Href}/case-workload");
+    public static BreadcrumbLinkModel CaseManagement => new ( "Case Management", "Location, pathway plan and initiative breakdowns for your cases" , $"{Home.Href}/case-management");
+    public static BreadcrumbLinkModel Performance => new ( "Performance", "Enrolment, activity and outcome performance for your cases" , $"{Home.Href}/performance");
 }
