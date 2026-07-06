261475d0549f7ed4acdf06c567fa09ea0db38717
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2026-07-06 10:48:35 +0100

    update the label messages

--- src/Server.UI/Pages/Workspaces/Administration/Services/AdministrationLinks.cs
+++ src/Server.UI/Pages/Workspaces/Administration/Services/AdministrationLinks.cs
@@ -5,13 +5,13 @@ namespace Cfo.Cats.Server.UI.Pages.Workspaces.Administration.Services;
 public static class AdministrationLinks
 {
     public static BreadcrumbLinkModel Home => new("Administration", "", "/pages/workspace/administration");
-    public static readonly BreadcrumbLinkModel Jobs = new("Jobs", "Job Scheduler", $"{Home.Href}/Jobs");
-    public static readonly BreadcrumbLinkModel CacheManagement = new("Cache Management", "Cache Management", $"{Home.Href}/CacheManagement");
-    public static readonly BreadcrumbLinkModel AuditTrails = new("Audit Trails", "Audit Trails", $"{Home.Href}/AuditTrails");
-    public static readonly BreadcrumbLinkModel PickList = new("Picklist", "Picklist", $"{Home.Href}/picklist");
-    public static readonly BreadcrumbLinkModel Outbox = new("Outbox Messages", "Outbox Messages", $"{Home.Href}/Outbox");
-    public static readonly BreadcrumbLinkModel Labels = new("Labels", "Labels", $"{Home.Href}/Labels");
-    public static readonly BreadcrumbLinkModel Initiatives = new("Initiatives", "Initiatives", $"{Home.Href}/Initiatives");
+    public static readonly BreadcrumbLinkModel Jobs = new("Jobs", "View and manage quarz jobs.", $"{Home.Href}/Jobs");
+    public static readonly BreadcrumbLinkModel CacheManagement = new("Cache Management", "Manage the in memory cache for CATS", $"{Home.Href}/CacheManagement");
+    public static readonly BreadcrumbLinkModel AuditTrails = new("Audit Trails", "View the system audit of edits, updates and inserts", $"{Home.Href}/AuditTrails");
+    public static readonly BreadcrumbLinkModel PickList = new("Picklist", "Manage the simple lookup lists.", $"{Home.Href}/picklist");
+    public static readonly BreadcrumbLinkModel Outbox = new("Outbox Messages", "View and reschedule outbox messages.", $"{Home.Href}/Outbox");
+    public static readonly BreadcrumbLinkModel Labels = new("Labels", "Manage labels that can be added to participants", $"{Home.Href}/Labels");
+    public static readonly BreadcrumbLinkModel Initiatives = new("Initiatives", "Manage initiatives (inovation funds etc)", $"{Home.Href}/Initiatives");
     
 }
  
