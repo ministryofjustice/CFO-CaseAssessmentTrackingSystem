bce2108ac131fa50430e382a1b2429da20b8659b
Paul Cooper <62419931+PaulCooperWorkJustice@users.noreply.github.com>
2026-07-07 20:10:43 +0100

    CFODEV-1761 move users to administration workspace (#1196)
    * Move Users to Administration workspace
    
    Relocates user management pages and components from Identity to Administration workspace. Fixed DI for OnlineUserTracker and NetworkIpProvider services.
    
    Addresses CFODEV-1468
    
    Signed-off-by: Paul Cooper <Paul.Cooper5@Justice.gov.uk>
    
    * Add the users to Administration Links
    
    Fix edit profile link in async menu
    Updated the edit profile navigation link in AsyncMenuService to point to the correct route after moving the profile page to the Administration workspace.
    
    Changed route from /pages/workspace/users/profile to /pages/workspace/administration/users/profile to match the @page directive in Profile.razor.
    
    ---------
    
    Signed-off-by: Paul Cooper <Paul.Cooper5@Justice.gov.uk>

--- src/Server.UI/Pages/Workspaces/Administration/Pages/Users/LoginAudit.razor.cs
+++ src/Server.UI/Pages/Workspaces/Administration/Pages/Users/LoginAudit.razor.cs
@@ -2,44 +2,17 @@
 using Cfo.Cats.Application.Features.Identity.Queries.PaginationQuery;
 using Cfo.Cats.Application.Features.Identity.Specifications;
 
-namespace Cfo.Cats.Server.UI.Pages.Identity.Users.Components;
+namespace Cfo.Cats.Server.UI.Pages.Workspaces.Administration.Pages.Users;
 
-public partial class UserAuditDialog
+public partial class LoginAudit : CatsComponentBase
 {
-    [Parameter, EditorRequired] public string UserName { get; set; } = default!;
-
-    private IdentityAuditTrailsWithPagination.Query Query { get; } = new();
     private MudDataGrid<IdentityAuditTrailDto> _table = null!;
-    private readonly IdentityAuditTrailDto _currentDto = new();
     private bool _loading;
+    private readonly IdentityAuditTrailDto _currentDto = new();
     private int _defaultPageSize = 15;
 
-    private async Task<GridData<IdentityAuditTrailDto>> ServerReload(GridState<IdentityAuditTrailDto> state, CancellationToken cancellationToken)
-    {
-        try
-        {
-            _loading = true;
-            Query.UserName = UserName;
-            Query.OrderBy = state.SortDefinitions.FirstOrDefault()?.SortBy ?? "Id";
-            Query.SortDirection = state.SortDefinitions.FirstOrDefault()?.Descending ?? true
-                ? SortDirection.Descending.ToString()
-                : SortDirection.Ascending.ToString();
-            Query.PageNumber = state.Page + 1;
-            Query.PageSize = state.PageSize;
-
-            var result = await GetNewMediator().Send(Query);
-            if (result is { Succeeded: true, Data: not null })
-            {
-                return new GridData<IdentityAuditTrailDto> { TotalItems = result.Data.TotalItems, Items = result.Data.Items };    
-            }
-            Snackbar.Add(result.ErrorMessage, Severity.Error);
-            return new GridData<IdentityAuditTrailDto>() { TotalItems = 0, Items = [] };
-        }
-        finally
-        {
-            _loading = false;
-        }
-    }
+    private string Title { get; set; } = "User Audit";
+    private IdentityAuditTrailsWithPagination.Query Query { get; } = new();
 
     private async Task OnChangedListView(IdentityAuditTrailListView listview)
     {
@@ -47,11 +20,37 @@ public partial class UserAuditDialog
         await _table.ReloadServerData();
     }
 
-    private async Task OnRefresh() => await _table.ReloadServerData();
-
     private async Task OnSearch(IdentityActionType? identityActionType)
     {
         Query.IdentityActionType = identityActionType;
         await _table.ReloadServerData();
+    }
+    
+    private async Task<GridData<IdentityAuditTrailDto>> ServerReload(GridState<IdentityAuditTrailDto> state, CancellationToken cancellationToken)
+    {
+        try
+        {
+            _loading = true;
+            Query.UserName = null;
+            Query.OrderBy = state.SortDefinitions.FirstOrDefault()?.SortBy ?? "Id";
+            Query.SortDirection = state.SortDefinitions.FirstOrDefault()?.Descending ?? true
+                ? nameof(SortDirection.Descending)
+                : nameof(SortDirection.Ascending);
+            Query.PageNumber = state.Page + 1;
+            Query.PageSize = state.PageSize;
+
+            var result = await GetNewMediator().Send(Query, cancellationToken: cancellationToken);
+            if (result is { Succeeded: true, Data: not null })
+            {
+                return new GridData<IdentityAuditTrailDto> { TotalItems = result.Data.TotalItems, Items = result.Data.Items };    
+            }
+
+            Snackbar.Add(result.ErrorMessage, Severity.Error);
+            return new GridData<IdentityAuditTrailDto> { TotalItems = 0, Items = [] };
+        }
+        finally
+        {
+            _loading = false;
+        }
     }
 }
