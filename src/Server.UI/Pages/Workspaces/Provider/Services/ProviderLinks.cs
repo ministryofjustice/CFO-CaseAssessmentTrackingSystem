f38208be8c9e4badacaaf2818bddef2875ad2970
Sam Gibson <140488216+samgibsonmoj@users.noreply.github.com>
2026-07-08 15:01:05 +0100

    CFODEV-1669: Move enrolments/activities PQA to Provider workspace (#1205)
    Move enrolments/activities PQA to Provider workspace

--- src/Server.UI/Pages/Workspaces/Provider/Services/ProviderLinks.cs
+++ src/Server.UI/Pages/Workspaces/Provider/Services/ProviderLinks.cs
@@ -9,5 +9,7 @@ public static class ProviderLinks
     public static BreadcrumbLinkModel CaseManagement => new ( "Case Management", "Location, pathway plan and initiative breakdowns for your cases" , $"{Home.Href}/case-management");
     public static BreadcrumbLinkModel Performance => new ( "Performance", "Enrolment, activity and outcome performance for your cases" , $"{Home.Href}/performance");
     public static BreadcrumbLinkModel Cumulatives => new ( "Cumulatives", "Cumulative performance figures against contract targets" , $"{Home.Href}/cumulatives");
+    public static BreadcrumbLinkModel EnrolmentsPqa => new ( "Enrolments PQA", "Review enrolments submitted for provider quality assurance" , $"{Home.Href}/enrolments/pqa");
+    public static BreadcrumbLinkModel ActivitiesPqa => new ( "Activities PQA", "Review activities submitted for provider quality assurance" , $"{Home.Href}/activities/pqa");
     public static BreadcrumbLinkModel Payments => new ( "Payments", "Contract payment auditing and management" , $"{Home.Href}/payments");
 }
