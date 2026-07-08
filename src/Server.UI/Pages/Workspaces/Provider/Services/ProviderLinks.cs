57d9b5aa5f769c1c660c4c15f2e88d86b4e46856
Sam Gibson <140488216+samgibsonmoj@users.noreply.github.com>
2026-07-08 12:17:44 +0100

    CFODEV-1764: Move 'Cumulatives' to Provider workspace (#1202)
    Move 'Cumulatives' to Provider workspace

--- src/Server.UI/Pages/Workspaces/Provider/Services/ProviderLinks.cs
+++ src/Server.UI/Pages/Workspaces/Provider/Services/ProviderLinks.cs
@@ -8,4 +8,5 @@ public static class ProviderLinks
     public static BreadcrumbLinkModel CaseWorkload => new ( "Case Workload", "An overview of how cases are spread across your team" , $"{Home.Href}/case-workload");
     public static BreadcrumbLinkModel CaseManagement => new ( "Case Management", "Location, pathway plan and initiative breakdowns for your cases" , $"{Home.Href}/case-management");
     public static BreadcrumbLinkModel Performance => new ( "Performance", "Enrolment, activity and outcome performance for your cases" , $"{Home.Href}/performance");
+    public static BreadcrumbLinkModel Cumulatives => new ( "Cumulatives", "Cumulative performance figures against contract targets" , $"{Home.Href}/cumulatives");
 }
