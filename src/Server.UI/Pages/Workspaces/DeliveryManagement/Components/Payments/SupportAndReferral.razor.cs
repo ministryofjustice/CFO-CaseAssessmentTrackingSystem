b8fdf81c30b1ed3cf7f7acb04a9aff1d4ff171b5
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2026-07-17 11:26:19 +0100

    CFODEV-1804: Rename 'Provider' workspace to 'Delivery Management' (#1251)
    Refactor the entire 'Provider' workspace to 'Delivery Management'.
    
    This includes:
    - Renaming all associated UI components, pages, routes, and namespaces.
    - Updating navigation links and descriptions across the application.
    - Introducing link grouping within the Delivery Management workspace for better organization.
    
    This update aligns the workspace terminology with current business language and improves navigability.

---
+++ src/Server.UI/Pages/Workspaces/DeliveryManagement/Components/Payments/SupportAndReferral.razor.cs
@@ -0,0 +1,118 @@
+﻿using ApexCharts;
+using Cfo.Cats.Application.Common.Security;
+using Cfo.Cats.Application.Features.Contracts.DTOs;
+using Cfo.Cats.Application.Features.Payments.Commands;
+using Cfo.Cats.Application.Features.Payments.DTOs;
+using Cfo.Cats.Application.Features.Payments.Queries;
+using Cfo.Cats.Infrastructure.Constants;
+
+namespace Cfo.Cats.Server.UI.Pages.Workspaces.DeliveryManagement.Components.Payments;
+
+public partial class SupportAndReferral
+{
+    private readonly ApexChartOptions<SupportAndReferralPaymentSummaryDto> _options = new();
+    private bool _loading = true;
+    private bool _downloading;
+
+    [Parameter, EditorRequired] public bool DataView { get; set; }
+
+    [Parameter, EditorRequired] public int Month { get; set; }
+
+    [Parameter, EditorRequired] public int Year { get; set; }
+
+    [Parameter] public ContractDto? Contract { get; set; }
+
+    [CascadingParameter] public UserProfile CurrentUser { get; set; } = default!;
+
+    private SupportAndReferralPaymentDto[] Payments { get; set; } = [];
+    private List<SupportAndReferralPaymentSummaryDto> SummaryData = [];
+
+    private GetSupportAndReferralPayments.Query? Query;
+
+    [CascadingParameter(Name = "IsDarkMode")]
+    public bool IsDarkMode { get; set; }
+
+    public ApexChartOptions<SupportAndReferralPaymentSummaryDto> Options => new()
+    {
+        Theme = new Theme
+        {
+            Mode = IsDarkMode ? Mode.Dark : Mode.Light
+        }
+    };
+
+    private async Task OnRefresh()
+    {
+        try
+        {
+            _loading = true;
+
+            var mediator = GetNewMediator();
+
+            var result = await mediator.Send(Query!);
+
+            if (result is not { Succeeded: true })
+            {
+                throw new Exception(result.ErrorMessage);
+            }
+
+            Payments = result.Data?.Items ?? [];
+            SummaryData = result.Data?.ContractSummary ?? [];
+
+        }
+        catch (Exception ex)
+        {
+            Snackbar.Add(ex.Message, Severity.Error);
+        }
+        finally { _loading = false; }
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        Query = new()
+        {
+            ContractId = Contract?.Id,
+            Month = Month,
+            Year = Year,
+            TenantId = CurrentUser!.TenantId!
+        };
+
+        await OnRefresh();
+    }
+
+    private string _searchString = "";
+
+    private async Task OnSearch()
+    {
+        Query!.Keyword = _searchString;
+        await OnRefresh();
+    }
+
+    private async Task OnExport()
+    {
+        try
+        {
+            _downloading = true;
+            var result = await GetNewMediator().Send(new ExportSupportAndReferralPayments.Command()
+            {
+                Query = Query!
+            });
+
+            if (result.Succeeded)
+            {
+                Snackbar.Add($"{ConstantString.ExportSuccess}", Severity.Info);
+                return;
+            }
+
+            Snackbar.Add(result.ErrorMessage, Severity.Error);
+
+        }
+        catch
+        {
+            Snackbar.Add($"An error occurred while generating your document.", Severity.Error);
+        }
+        finally
+        {
+            _downloading = false;
+        }
+    }
+}
