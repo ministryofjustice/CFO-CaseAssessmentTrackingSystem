f528665e4f209ef19e3fc6993682ce82f0de27c8
Vinay Saini <153621591+vks333@users.noreply.github.com>
2026-07-17 06:42:09 +0100

    Fix Service Desk Access rights (#1250)
    -Restricts QA Officer access to view queues
    -when navigating from Queue summary, breadcrumbs to return to current processing item

--- src/Server.UI/Pages/Workspaces/ServiceDesk/Pages/Index.razor.cs
+++ src/Server.UI/Pages/Workspaces/ServiceDesk/Pages/Index.razor.cs
@@ -34,12 +34,14 @@ public partial class Index
             .Succeeded;
         var isQa1User = (await AuthorizationService.AuthorizeAsync(authState.User, SecurityPolicies.Qa1))
             .Succeeded;
+        var canViewQueueLinks = isQa1User && CurrentUser.AssignedRoles.Any(role =>
+            role is RoleNames.QASupportManager or RoleNames.QAManager or RoleNames.SMT or RoleNames.SystemSupport);
 
         _showQaPots = isInternalUser;
         _showSyncInfo = isInternalUser;
-        _showActivitiesQueue = isQa1User;
+        _showActivitiesQueue = canViewQueueLinks;
         _showActivitiesFeedback = isQa1User;
-        _showEnrolmentsQueue = isQa1User;
+        _showEnrolmentsQueue = canViewQueueLinks;
         _showEnrolmentsFeedback = isQa1User;
         
         List<BreadcrumbLinkModel> links = [];
