b44cd0308c1d49b30f9b058548ca2839dd58cc2d
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2026-07-20 09:46:47 +0100

    Feature/cfodev 1770 - activities in QA pots (#1267)
    * Initial move
    
    * add graphs

---
+++ src/Server.UI/Pages/Workspaces/DeliveryManagement/Pages/ActivitiesInQaPots.razor.cs
@@ -0,0 +1,10 @@
+using Cfo.Cats.Application.SecurityConstants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Cfo.Cats.Server.UI.Pages.Workspaces.DeliveryManagement.Pages;
+
+public partial class ActivitiesInQaPots
+{
+
+}
