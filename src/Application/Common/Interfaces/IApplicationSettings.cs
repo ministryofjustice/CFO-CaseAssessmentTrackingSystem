04991ff1610aacb999ef0293c6e1130f16afa172
Ryan Kearsley <49034987+ryankearsley-moj@users.noreply.github.com>
2026-07-21 14:31:11 +0100

    Cfodev 1818 fix dark mode banner and chart colours (#1285)
    * Fix chart styling to load correctly when system mode is toggled
    
    * Add support for dark mode colors in application settings and themes

--- src/Application/Common/Interfaces/IApplicationSettings.cs
+++ src/Application/Common/Interfaces/IApplicationSettings.cs
@@ -6,7 +6,15 @@ public interface IApplicationSettings
     string Copyright { get; set; }
     string Version { get; set; }
     string PrimaryColour { get; set; }
+    ThemeDarkColours PrimaryColourDark { get; set; }
     string PreLoginMessage { get; set; }
 
     int IdleTimeOutMinutes { get; set; }
+}
+
+public class ThemeDarkColours
+{
+    public required string Primary { get; set; }
+    public required string TableLines { get; set; }
+    public required string AppbarBackground { get; set; }
 }
