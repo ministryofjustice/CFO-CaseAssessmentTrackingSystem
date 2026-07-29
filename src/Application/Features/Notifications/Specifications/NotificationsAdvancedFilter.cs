5d5a28911265ba444dcb418384a08ebbbcc2996f
Paul Cooper <62419931+PaulCooperWorkJustice@users.noreply.github.com>
2026-07-29 12:55:06 +0100

    CFODEV-1819:  Enhance notifications page with filtering and improved UI (#1293)
    * Enhance notifications page with filtering and improved UI
    
    - Add notification type filter (Activities, Enrolments, Assigned, Unassigned, PRI)
    - Add keyword search across heading and details
    - Improve UI layout with cards, pagination, and action buttons
    - Fix typo in participant dialog ("assingee" → "assignee")
    
    Signed-off-by: Paul Cooper <Paul.Cooper5@Justice.gov.uk>
    
    * Changes recommended on Peer review.
    
    Signed-off-by: Paul Cooper <Paul.Cooper5@Justice.gov.uk>
    
    ---------
    
    Signed-off-by: Paul Cooper <Paul.Cooper5@Justice.gov.uk>

--- src/Application/Features/Notifications/Specifications/NotificationsAdvancedFilter.cs
+++ src/Application/Features/Notifications/Specifications/NotificationsAdvancedFilter.cs
@@ -9,10 +9,21 @@ public class NotificationsAdvancedFilter : PaginationFilter
     public bool ShowReadNotifications { get; set; } = false;
 
     public NotificationsListView ListView { get; set; } = NotificationsListView.Default;
-
+    
+    public NotificationType? Type { get; set; }
 }
 
 public enum NotificationsListView
 {
     [Description("Default")] Default = 0,
+}
+
+public enum NotificationType
+{
+    [Description("All")] All = 0,
+    [Description("Activities")] Activities = 1,
+    [Description("Enrolments")] Enrolments = 2,
+    [Description("Assigned")] Assigned = 3,
+    [Description("Unassigned")] Unassigned = 4,
+    [Description("Pri")] Pri = 5
 }
