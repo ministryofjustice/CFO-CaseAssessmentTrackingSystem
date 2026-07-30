b42c64f19921678907b428682cf4052d1ea61efb
Paul Cooper <62419931+PaulCooperWorkJustice@users.noreply.github.com>
2026-07-30 10:18:58 +0100

    Cfodev 1551 ws export delivery management (#1313)
    CFODEV-1551:  Add export button to each tabs on the delivery management, performance work space.  Also refactored the code so it cleaner.
    
    Signed-off-by: Paul Cooper <Paul.Cooper5@Justice.gov.uk>

--- src/Server.UI/Components/Dashboard/PaidActivityComponent.razor.cs
+++ src/Server.UI/Components/Dashboard/PaidActivityComponent.razor.cs
@@ -1,6 +1,8 @@
 using Cfo.Cats.Application.Features.Dashboard.Queries;
 using ApexCharts;
+using Cfo.Cats.Application.Features.Workspaces.Performance.Commands;
 using Cfo.Cats.Domain.Common.Enums;
+using Cfo.Cats.Infrastructure.Constants;
 
 namespace Cfo.Cats.Server.UI.Components.Dashboard;
 
@@ -21,17 +23,20 @@ public partial class PaidActivityComponent
     [CascadingParameter(Name = "IsDarkMode")]
     public bool IsDarkMode { get; set; }
 
-    private List<ActivityType> desiredOrder = new List<ActivityType>
-    {
+    private readonly List<ActivityType> _desiredOrder =
+    [
         ActivityType.SupportWork,
         ActivityType.HumanCitizenship,
         ActivityType.CommunityAndSocial,
         ActivityType.InterventionsAndServicesWraparoundSupport,
         ActivityType.EducationAndTraining,
         ActivityType.Employment
-    };
-    private ApexChartOptions<LocationActivityCount> chartOptions = new();
+    ];
+    
+    private ApexChartOptions<LocationActivityCount> _chartOptions = new();
 
+    private bool _downloading;
+    
     protected override IQuery<Result<GetPaidActivities.PaidActivitiesDto>> CreateQuery()
      => new GetPaidActivities.Query()
      {
@@ -52,7 +57,7 @@ public partial class PaidActivityComponent
         }
 
         // Configure chart options
-        chartOptions = new ApexChartOptions<LocationActivityCount>
+        _chartOptions = new ApexChartOptions<LocationActivityCount>
         {
             Chart = new Chart
             {
@@ -66,12 +71,12 @@ public partial class PaidActivityComponent
                 Bar = new PlotOptionsBar
                 {
                     Horizontal = false,
-                    DataLabels = new ApexCharts.PlotOptionsBarDataLabels
+                    DataLabels = new PlotOptionsBarDataLabels
                     {
-                        Total = new ApexCharts.BarTotalDataLabels
+                        Total = new BarTotalDataLabels
                         {
                             Enabled = true,
-                            Style = new ApexCharts.BarDataLabelsStyle
+                            Style = new BarDataLabelsStyle
                             {
                                 FontWeight = "800",
                                 Color = IsDarkMode ? "#FFFFFF" : "#000000",
@@ -88,14 +93,14 @@ public partial class PaidActivityComponent
             {
                 Title = new AxisTitle { Text = "Payable" }
             },
-            Yaxis = new List<YAxis>
-            {
+            Yaxis =
+            [
                 new YAxis
                 {
                     Min = 0,
                     ForceNiceScale = true
                 }
-            },
+            ],
             Legend = new Legend
             {
                 Position = LegendPosition.Bottom,
@@ -144,5 +149,39 @@ public partial class PaidActivityComponent
     {
         public string Location { get; set; } = string.Empty;
         public int Count { get; set; }
+    }
+    
+    private async Task OnExport()
+    {
+        try
+        {
+            _downloading = true;
+            var result = await Service.Send(new ExportActivities.Command()
+            {
+                Request = new ExportActivities.ActivitiesExportRequest
+                {
+                    StartDate = DateRange?.Start ?? throw new InvalidOperationException("DateRange not set"),
+                    EndDate = DateRange?.End ?? throw new InvalidOperationException("DateRange not set"),
+                    TenantId = TenantId,
+                    UserId = UserId
+                }
+            });
+
+            if (result.Succeeded)
+            {
+                Snackbar.Add(ConstantString.ExportSuccess, Severity.Info);
+                return;
+            }
+
+            Snackbar.Add(result.ErrorMessage, Severity.Error);
+        }
+        catch
+        {
+            Snackbar.Add("An error occurred while generating your document.", Severity.Error);
+        }
+        finally
+        {
+            _downloading = false;
+        }
     }
 }
