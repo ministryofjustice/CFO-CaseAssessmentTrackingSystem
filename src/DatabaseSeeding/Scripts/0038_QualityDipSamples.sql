52d687b6af3cca085f6261dc49200609c2237f8e
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2026-08-03 07:18:59 +0100

    Test records obviousness

--- src/DatabaseSeeding/Scripts/0038_QualityDipSamples.sql
+++ src/DatabaseSeeding/Scripts/0038_QualityDipSamples.sql
@@ -16,13 +16,13 @@ BEGIN
 	
 	INSERT INTO [Mi].[OutcomeQualityDipSampleParticipant] ([Id], [ParticipantId], [DipSampleId], [LocationType], [HasClearParticipantJourney], [ShowsTaskProgression], [TTGDemonstratesGoodPRIProcess], [SupportsJourney], [CsoComments], [LastModified], [FinalReviewedBy], [CpmComments], [CpmIsCompliant], [CpmReviewedBy], [CpmReviewedOn], [Created], [CreatedBy], [CsoIsCompliant], [CsoReviewedBy], [CsoReviewedOn], [FinalComments], [FinalIsCompliant], [FinalReviewedOn], [LastModifiedBy], [AlignsWithDoS]) 
 	VALUES 
-	(31, N'1CFG1553F', N'01990585-d759-7a75-bc0b-b09567674a50', N'Wider Custody', 0, 0, 0, 0, NULL, NULL, NULL, NULL, 0, NULL, NULL, @FirstOfTheMonth, NULL, 0, NULL, NULL, NULL, 0, NULL, NULL, 0),
-	(32, N'1CFG5469L', N'01990585-d759-7a75-bc0b-b09567674a50', N'Wider Custody', 0, 0, 0, 0, NULL, NULL, NULL, NULL, 0, NULL, NULL, @FirstOfTheMonth, NULL, 0, NULL, NULL, NULL, 0, NULL, NULL, 0),
-	(33, N'1CFG4261Y', N'01990585-d759-717c-94b9-867b3fde1e4f', N'Wider Custody', 0, 0, 0, 0, NULL, NULL, NULL, NULL, 0, NULL, NULL, @FirstOfTheMonth, NULL, 0, NULL, NULL, NULL, 0, NULL, NULL, 0),
-	(34, N'1CFG2457U', N'01990585-d759-717c-94b9-867b3fde1e4f', N'Wider Custody', 0, 0, 0, 0, NULL, NULL, NULL, NULL, 0, NULL, NULL, @FirstOfTheMonth, NULL, 0, NULL, NULL, NULL, 0, NULL, NULL, 0),
-	(35, N'1CFG2622V', N'01990585-d759-717c-94b9-867b3fde1e4f', N'Wider Custody', 0, 0, 0, 0, NULL, NULL, NULL, NULL, 0, NULL, NULL, @FirstOfTheMonth, NULL, 0, NULL, NULL, NULL, 0, NULL, NULL, 0),
-	(36, N'1CFG8432I', N'01990585-d759-717c-94b9-867b3fde1e4f', N'Wider Custody', 0, 0, 0, 0, NULL, NULL, NULL, NULL, 0, NULL, NULL, @FirstOfTheMonth, NULL, 0, NULL, NULL, NULL, 0, NULL, NULL, 0),
-	(37, N'1CFG9960M', N'01990585-d759-717c-94b9-867b3fde1e4f', N'Wider Custody', 0, 0, 0, 0, NULL, NULL, NULL, NULL, 0, NULL, NULL, @FirstOfTheMonth, NULL, 0, NULL, NULL, NULL, 0, NULL, NULL, 0);
+	(31, N'TEST00003', N'01990585-d759-7a75-bc0b-b09567674a50', N'Wider Custody', 0, 0, 0, 0, NULL, NULL, NULL, NULL, 0, NULL, NULL, @FirstOfTheMonth, NULL, 0, NULL, NULL, NULL, 0, NULL, NULL, 0),
+	(32, N'TEST00015', N'01990585-d759-7a75-bc0b-b09567674a50', N'Wider Custody', 0, 0, 0, 0, NULL, NULL, NULL, NULL, 0, NULL, NULL, @FirstOfTheMonth, NULL, 0, NULL, NULL, NULL, 0, NULL, NULL, 0),
+	(33, N'TEST00008', N'01990585-d759-717c-94b9-867b3fde1e4f', N'Wider Custody', 0, 0, 0, 0, NULL, NULL, NULL, NULL, 0, NULL, NULL, @FirstOfTheMonth, NULL, 0, NULL, NULL, NULL, 0, NULL, NULL, 0),
+	(34, N'TEST00005', N'01990585-d759-717c-94b9-867b3fde1e4f', N'Wider Custody', 0, 0, 0, 0, NULL, NULL, NULL, NULL, 0, NULL, NULL, @FirstOfTheMonth, NULL, 0, NULL, NULL, NULL, 0, NULL, NULL, 0),
+	(35, N'TEST00006', N'01990585-d759-717c-94b9-867b3fde1e4f', N'Wider Custody', 0, 0, 0, 0, NULL, NULL, NULL, NULL, 0, NULL, NULL, @FirstOfTheMonth, NULL, 0, NULL, NULL, NULL, 0, NULL, NULL, 0),
+	(36, N'TEST00023', N'01990585-d759-717c-94b9-867b3fde1e4f', N'Wider Custody', 0, 0, 0, 0, NULL, NULL, NULL, NULL, 0, NULL, NULL, @FirstOfTheMonth, NULL, 0, NULL, NULL, NULL, 0, NULL, NULL, 0),
+	(37, N'TEST00028', N'01990585-d759-717c-94b9-867b3fde1e4f', N'Wider Custody', 0, 0, 0, 0, NULL, NULL, NULL, NULL, 0, NULL, NULL, @FirstOfTheMonth, NULL, 0, NULL, NULL, NULL, 0, NULL, NULL, 0);
 	
 	SET IDENTITY_INSERT [Mi].[OutcomeQualityDipSampleParticipant] OFF
 
