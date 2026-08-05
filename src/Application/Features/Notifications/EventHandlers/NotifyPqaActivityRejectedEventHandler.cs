7d06f688730d8ba7cb349d587ab75aa6d6cb6ab2
Paul Cooper <Paul.Cooper5@Justice.gov.uk>
2026-08-05 12:21:25 +0100

    CFODEV-1858: Fix the notification location for PQA items.
    Signed-off-by: Paul Cooper <Paul.Cooper5@Justice.gov.uk>

--- src/Application/Features/Notifications/EventHandlers/NotifyPqaActivityRejectedEventHandler.cs
+++ src/Application/Features/Notifications/EventHandlers/NotifyPqaActivityRejectedEventHandler.cs
@@ -31,7 +31,7 @@ public class NotifyPqaActivityRejectedEventHandler(IUnitOfWork unitOfWork) : INo
             if (previous == null)
             {
                 var n = Notification.Create(heading, details, qaUser!);
-                n.SetLink($"pages/workspace/provider/activities/pqa/");
+                n.SetLink($"pages/workspace/deliverymanagement/activities/pqa/");
                 await unitOfWork.DbContext.Notifications.AddAsync(n, cancellationToken);
             }
         }
