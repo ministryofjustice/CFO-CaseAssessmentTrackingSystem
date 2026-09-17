1792e7aeae5951e0d4574c3d8d49200308f6533b
Vinay Saini <vinay.saini@justice.gov.uk>
2026-09-17 14:50:17 +0100

    Excludes hub locations from the Incoming/Outgoing Transfers Location dialog

--- src/Server.UI/Pages/Workspaces/Participants/Pages/IncomingTransfers.razor.cs
+++ src/Server.UI/Pages/Workspaces/Participants/Pages/IncomingTransfers.razor.cs
@@ -87,7 +87,7 @@ public partial class IncomingTransfers
 
     private async Task ShowSelectLocationDialog()
     {
-        var location = await ParticipantDialogService.PromptForLocationAsync(UserProfile);
+        var location = await ParticipantDialogService.PromptForLocationAsync(UserProfile, l => l.LocationType.IsHub == false);
 
         if (location is not null)
         {
