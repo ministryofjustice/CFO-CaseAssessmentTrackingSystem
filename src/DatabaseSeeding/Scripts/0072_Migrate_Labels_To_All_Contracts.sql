f32c905c36ece4ffa51142b670bd8034ca068ff6
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2026-09-24 11:35:08 +0100

    Exclude ISC from labels to disable it.

--- src/DatabaseSeeding/Scripts/0072_Migrate_Labels_To_All_Contracts.sql
+++ src/DatabaseSeeding/Scripts/0072_Migrate_Labels_To_All_Contracts.sql
@@ -6,5 +6,6 @@ BEGIN
 
   INSERT INTO [Configuration].[LabelContract] ( [ContractId], [LabelId] )
   SELECT [Contract].[Id], [Label].[Id] FROM [Configuration].[Contract], [Configuration].[Label]
+  WHERE Label.Id <> '1f611df9-1fb2-4059-b1ff-dcb4db6e761b' -- NO ISC generic
  
 END
